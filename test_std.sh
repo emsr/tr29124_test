1bc32c3c007bac84964a34e5cf1f9185536f45e4
Ed Smith-Rowland <3dw4rd@verizon.net>
2015-11-13 14:16:08 -0500

    This script runs the tests in std...

---
+++ test_std.sh
@@ -0,0 +1,4 @@
+tool="$HOME/bin_specfun/bin/g++ -I/home/ed/gcc_specfun/libstdc++-v3/testsuite/util -D__STDCPP_WANT_MATH_SPEC_FUNCS__"
+srcs=`find /home/ed/gcc_specfun/libstdc++-v3/testsuite/special_functions -name \*.cc`
+
+$tool $srcs 2> test_std.txt
