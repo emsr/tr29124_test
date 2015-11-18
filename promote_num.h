76ded62675511c485558f9cff207b8cbaf1c50a9
U-ALIONSCIENCE\ESmith-rowland <ESmith-rowland@SEG_SMTHRWLNDLT.alionscience.com>
2015-11-18 17:11:23 -0500

    Decay refs and cv in promote_num...

--- promote_num.h
+++ promote_num.h
@@ -3,7 +3,7 @@
   // For complex and cmath
   template<typename _Tp, bool = std::is_integral<_Tp>::value>
     struct __promote_help
-    { typedef double __type; };
+    { using __type = double; };
 
   // No nested __type member for non-integer non-floating point types,
   // allows this type to be used for SFINAE to constrain overloads in
@@ -14,19 +14,30 @@
 
   template<>
     struct __promote_help<long double>
-    { typedef long double __type; };
+    { using __type = long double; };
 
   template<>
     struct __promote_help<double>
-    { typedef double __type; };
+    { using __type = double; };
 
   template<>
     struct __promote_help<float>
-    { typedef float __type; };
+    { using __type = float; };
 
+  template<typename... _Tps>
+    using __promote_help_t = typename __promote_help<_Tps...>::__type;
+
+  // Decay refs and cv...
+  // Alternatively we could decay refs and propagate cv to promoted type.
   template<typename _Tp, typename... _Tps>
     struct __promote_num
-    { using __type = decltype(_Tp() + typename __promote_help<_Tps...>::__type()); };
+    { using __type = decltype(__promote_help_t<std::decay_t<_Tp>>()
+		   + typename __promote_num<_Tps...>::__type()); };
+
+  template<>
+    template<typename _Tp>
+      struct __promote_num<_Tp>
+      { using __type = decltype(__promote_help_t<std::decay_t<_Tp>>()); };
 
   template<typename... _Tps>
     using __promote_num_t = typename __promote_num<_Tps...>::__type;
--- test_promote_num.cpp
+++ test_promote_num.cpp
@@ -1,5 +1,6 @@
 // $HOME/bin/bin/g++ -o test_promote_num test_promote_num.cpp
 
+#include <type_traits>
 #include "promote_num.h"
 
 int
@@ -13,4 +14,26 @@ main()
 
   __promote_num_t<int, short> x;
   __promote_num_t<int, long double> y;
+  __promote_num_t<float, float, int, float> z;
+  static_assert(std::is_same<decltype(x), double>::value, "");
+  static_assert(std::is_same<decltype(y), long double>::value, "");
+  static_assert(std::is_same<decltype(z), double>::value, "");
+
+  static_assert(std::is_same<decltype(d), double>::value, "");
+
+  double & rd = d;
+  //static_assert(std::is_same<decltype(rd), double>::value, "");
+  static_assert(std::is_same<__promote_num_t<decltype(rd)>, double>::value, "");
+
+  const double cd = d;
+  //static_assert(std::is_same<decltype(cd), double>::value, "");
+  static_assert(std::is_same<__promote_num_t<decltype(cd)>, double>::value, "");
+
+  volatile double vd = d;
+  //static_assert(std::is_same<decltype(vd), double>::value, "");
+  static_assert(std::is_same<__promote_num_t<decltype(vd)>, double>::value, "");
+
+  const volatile double cvd = d;
+  //static_assert(std::is_same<decltype(cvd), double>::value, "");
+  static_assert(std::is_same<__promote_num_t<decltype(cvd)>, double>::value, "");
 }
