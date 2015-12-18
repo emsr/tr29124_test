9af0d853f4daea6e619e84e076409c6d5a0ab4c3
Ed Smith-Rowland <3dw4rd@verizon.net>
2015-12-18 10:21:43 -0500

    Got shit to work and converge OK. The series to asymptotic is still rough. I extended series to 5. I really must abstract a tool to accumulate and watch the asymptotic series.

--- airy_toy.cpp
+++ airy_toy.cpp
@@ -24,7 +24,7 @@ template<typename _Tp>
     auto __denom = _Tp{1};
     for (int __s = 1; __s <= 200; ++__s)
       {
-	//  This also works actually.
+	//  This also works actually - it's recursion on the products.
 	//_S_cn.push_back(_S_cn.back()
 	//	       * (6 * __s - 5) * (6 * __s - 3) * (6 * __s - 1)
 	//	       / (216 * __s * (2 * __s - 1)));
@@ -54,19 +54,19 @@ template<typename _Tp>
     auto __F_denom = _Tp{1};
     auto __G_numer = _Tp{1};
     auto __G_denom = _Tp{1};
-    for (unsigned long long __k = 1ULL; __k <= 12ULL; ++__k)
+    for (unsigned long long __k = 1ULL; __k <= 15ULL; ++__k)
       {
 	std::cout << '\n' << ' ' << std::setw(40) << __F_numer << '/' << std::setw(40) << __F_denom;
 	__F_denom *= (3ULL * __k - 2ULL) * (3ULL * __k - 1ULL) * (3ULL * __k - 0ULL);
 	__F_numer *= 1ULL + 3ULL * (__k - 1ULL);
 	_F_k.push_back(__F_numer / __F_denom);
-	_Fp_k.push_back(3 * (__k + 1) * _F_k.back());
+	_Fp_k.push_back(3 * __k * _F_k.back());
 
 	std::cout << '\t' << ' ' << std::setw(40) << __G_numer << '/' << std::setw(40) << __G_denom;
-	__G_denom *= (3ULL * (__k + 1) - 4ULL) * (3ULL * (__k + 1) - 3ULL) * (3ULL * (__k + 1) - 2ULL);
+	__G_denom *= (3ULL * __k - 1ULL) * (3ULL * __k) * (3ULL * __k + 1ULL);
 	__G_numer *= 2ULL + 3ULL * (__k - 1ULL);
 	_G_k.push_back(__G_numer / __G_denom);
-	_Gp_k.push_back((3 * (__k + 1) + 1) * _G_k.back());
+	_Gp_k.push_back((3 * __k + 1) * _G_k.back());
       }
     std::cout << "\nF\n";
     for (const auto& c : _F_k)
