d1f790ed0920bf1e97bd6212eb738452af2c0702
Ed Smith-Rowland <3dw4rd@verizon.net>
2016-01-11 14:02:58 -0500

    Make test_dawson write arrays.

--- test_dawson.cpp
+++ test_dawson.cpp
@@ -15,17 +15,18 @@ main()
   using _Tp = __float128;
   std::cout.precision(std::numeric_limits<_Tp>::digits10);
   constexpr auto _S_1_sqrtpi{0.5641895835477562869480794515607726L};
-  constexpr auto _S_H(0.2L);
+  constexpr auto _S_H{0.2L};
   /// @todo this needs some compile-time construction!
-  constexpr auto __n_max = 100;
-  static _Tp __c[__n_max + 1];
+  constexpr auto _S_n_max = 100;
+  static _Tp _S_c[_S_n_max];
   static auto __init = false;
-  __c[0] = _Tp{1};
-  for (unsigned int __i = 1; __i <= __n_max; ++__i)
+  _S_c[0] = _Tp{1};
+  for (unsigned int __i = 0; __i < _S_n_max; ++__i)
    {
-     _Tp __y = _Tp(2 * __i - 1) * _S_H;
-     __c[__i] = std::exp(-__y * __y);
+     _Tp __y = _Tp(2 * __i + 1) * _S_H;
+     _S_c[__i] = std::exp(-__y * __y);
    }
-  for (auto __CC : __c)
-    std::cout << ' ' << __CC << ',' << '\n';
+  std::cout << std::scientific;
+  for (auto __CC : _S_c)
+    std::cout << '\t' << __CC << "L,\n";
 }
