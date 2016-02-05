7a6282f55741a1cb1a984f1271601ed93f24239c
U-ALIONSCIENCE\ESmith-rowland <ESmith-rowland@SEG_SMTHRWLNDLT.alionscience.com>
2016-02-05 18:40:52 -0500

    Put in Florian Goths complex polylog! Mofe bernoulli* to test_bernoulli. You should offer this. Florian needs zetam1 too!

---
+++ test_polylog.cpp
@@ -0,0 +1,16 @@
+// $HOME/bin/bin/g++ -std=c++14 -o test_polylog test_polylog.cpp
+
+// ./test_polylog > test_polylog.txt
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <complex>
+
+#include "sf_polylog.tcc"
+
+int
+main()
+{
+  ;
+}
