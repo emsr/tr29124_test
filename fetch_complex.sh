c023b167b96668e30db47c9cc8a069ad25bf0585
Ed Smith-Rowland <3dw4rd@verizon.net>
2016-02-08 11:10:06 -0500

    Tracking down testsuite failures.

--- fetch_complex.sh
+++ fetch_complex.sh
@@ -1,7 +1,7 @@
 #!  /bin/bash
 
-cp ../gcc_specfun/libstdc++-v3/testsuite/ext/special_functions/ellint_rc/complex.cc complex_ellint_rc 
-cp ../gcc_specfun/libstdc++-v3/testsuite/ext/special_functions/ellint_rd/complex.cc complex_ellint_rd 
-cp ../gcc_specfun/libstdc++-v3/testsuite/ext/special_functions/ellint_rf/complex.cc complex_ellint_rf 
-cp ../gcc_specfun/libstdc++-v3/testsuite/ext/special_functions/ellint_rg/complex.cc complex_ellint_rg 
-cp ../gcc_specfun/libstdc++-v3/testsuite/ext/special_functions/ellint_rj/complex.cc complex_ellint_rj 
+cp ../gcc_specfun/libstdc++-v3/testsuite/ext/special_functions/ellint_rc/complex.cc complex_ellint_rc.cc 
+cp ../gcc_specfun/libstdc++-v3/testsuite/ext/special_functions/ellint_rd/complex.cc complex_ellint_rd.cc 
+cp ../gcc_specfun/libstdc++-v3/testsuite/ext/special_functions/ellint_rf/complex.cc complex_ellint_rf.cc 
+cp ../gcc_specfun/libstdc++-v3/testsuite/ext/special_functions/ellint_rg/complex.cc complex_ellint_rg.cc 
+cp ../gcc_specfun/libstdc++-v3/testsuite/ext/special_functions/ellint_rj/complex.cc complex_ellint_rj.cc 
