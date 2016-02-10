dfd905f55389ed1405b010d009e7a2029c6f2f7f
Ed Smith-Rowland <3dw4rd@verizon.net>
2016-02-10 07:48:59 -0500

    The inverse erf series works swimmingly. The Atlas was a bit wrong on the series! I guess I'll implement experfc and do the CF for |X| > 0.85 or 0.9 or so.

--- test_inv_erf.cpp
+++ test_inv_erf.cpp
@@ -1,53 +1,117 @@
-// g++ -std=c++14 -o test_inv_erf test_inv_erf.cpp
+// $HOME/bin_specfun/bin/g++ -D__STDCPP_WANT_MATH_SPEC_FUNCS__ -o test_inv_erf test_inv_erf.cpp -lquadmath
 
-// ./test_inv_erf
+// LD_LIBRARY_PATH=$HOME/bin_specfun/lib64:$LD_LIBRARY_PATH ./test_inv_erf > test_inv_erf.txt
 
 // AAOF pp. 408-409.
 
-#include<vector>
-#include<iostream>
-#include<limits>
-#include<cmath>
+#include <vector>
+#include <iostream>
+#include <limits>
+#include <cmath>
+#include "float128.h"
+
+template<typename _Tp>
+  void
+  test_inv_erf()
+  {
+    //  Build the series coefficients.
+    const auto _S_eps = std::numeric_limits<_Tp>::epsilon();
+    const auto _S_pi =  _Tp{3.1415926535897932384626433832795029Q};
+    const auto _S_sqrt_pi = _Tp{1.772453850905516027298167483341145182797Q};
+
+    std::cout.precision(std::numeric_limits<_Tp>::digits10);
+    auto width = 6 + std::cout.precision();
+
+    const int n_max = 250;
+    std::vector<_Tp> a;
+    a.push_back(1);
+    for (int n = 1; n < n_max; ++n)
+      {
+	auto atemp = _Tp{0};
+	for (int k = 1; k <= n; ++k)
+          atemp += _Tp(2 * (k - 1) + 1) * a[k - 1]
+		 * _Tp(2 * (n - k) + 1) * a[n - k]
+		 / _Tp(k * (2 * k - 1));
+	atemp /= _Tp(2 * n + 1);
+	a.push_back(atemp);
+      }
+    for (auto aa : a)
+      std::cout << ' ' << aa << '\n';
+
+    std::cout << "\n\n"
+	      << std::setw(width) << "x"
+	      << std::setw(width) << "inv_erf(x)"
+	      << std::setw(width) << "erf(inv_erf(x))"
+	      << std::setw(width) << "erf(inv_erf(x)) - x"
+	      << '\n';
+    for (int i = -200; i <= 200; ++i)
+      {
+	auto x = i * _Tp{0.01Q};
+	auto chi = _S_sqrt_pi * x / _Tp{2};
+	auto chi2 = chi * chi;
+	auto chip = chi;
+	auto inverf = _Tp{0};
+	for (int k = 0; k < n_max; ++k)
+	  {
+	    auto term = a[k] * chip;
+	    inverf += term;
+	    if (std::abs(term) / std::abs(inverf) < _S_eps)
+	      break;
+	    chip *= chi2;
+	  }
+	std::cout << ' ' << std::setw(width) << x
+		  << ' ' << std::setw(width) << inverf
+		  << ' ' << std::setw(width) << std::erf(inverf)
+		  << ' ' << std::setw(width) << std::erf(inverf) - x
+		  << '\n';
+      }
+
+    std::cout << "\n\n"
+	      << std::setw(width) << "x"
+	      << std::setw(width) << "erf(x)"
+	      << std::setw(width) << "inv_erf(erf(x))"
+	      << std::setw(width) << "inv_erf(erf(x)) - x"
+	      << '\n';
+    for (int i = -200; i <= 200; ++i)
+      {
+	auto x = i * _Tp{0.01Q};
+	auto erf = std::erf(x);
+	auto chi = _S_sqrt_pi * erf / _Tp{2};
+	auto chi2 = chi * chi;
+	auto chip = chi;
+	auto inverf = _Tp{0};
+	for (int k = 0; k < n_max; ++k)
+	  {
+	    auto term = a[k] * chip;
+	    inverf += term;
+	    if (std::abs(term) / std::abs(inverf) < _S_eps)
+	      break;
+	    chip *= chi2;
+	  }
+	std::cout << ' ' << std::setw(width) << x
+		  << ' ' << std::setw(width) << erf
+		  << ' ' << inverf
+		  << ' ' << inverf - x
+		  << '\n';
+      }
+  }
 
 int
 main()
 {
-  //  Build the series coefficients.
-  using _Tp = double;
-  const auto _S_eps = std::numeric_limits<_Tp>::epsilon();
-  const auto _S_pi =  _Tp{3.1415926535897932384626433832795029L};
-  const auto _S_sqrt_pi = _Tp{1.772453850905516027298167483341145182797L};
+  std::cout << "\n\n  float\n";
+  std::cout << "  =====\n";
+  test_inv_erf<float>();
 
-  std::cout.precision(std::numeric_limits<_Tp>::digits10);
+  std::cout << "\n\n  double\n";
+  std::cout << "  ======\n";
+  test_inv_erf<double>();
 
-  std::vector<_Tp> a;
-  a.push_back(1);
-  for (int j = 1; j < 100; ++j)
-    {
-      auto atemp = _Tp{0};
-      for (int k = 1; k <= j; ++k)
-        atemp += a[k - 1] * a[j - k] / _Tp(k) / _Tp(2 * k - 1);
-      atemp /= _Tp(2 * j + 1);
-      a.push_back(atemp);
-    }
-  for (auto aa : a)
-    std::cout << ' ' << aa << '\n';
+  std::cout << "\n\n  long double\n";
+  std::cout << "  ===========\n";
+  test_inv_erf<long double>();
 
-  for (int i = -200; i <= 200; ++i)
-    {
-      auto x = i * 0.01;
-      auto chi = _S_sqrt_pi * x / _Tp{2};
-      auto chi2 = chi * chi;
-      auto inverf = _Tp{0};
-      auto chip = chi;
-      for (int k = 0; k < 100; ++k)
-	{
-	  auto term = a[k] * chip;
-	  inverf += term;
-	  if (std::abs(term) < _S_eps)
-	    break;
-	  chip *= chi2;
-	}
-      std::cout << ' ' << x << ' ' << inverf << ' ' << erf(inverf) << '\n';
-    }
+  std::cout << "\n\n  __float128\n";
+  std::cout << "  ==========\n";
+  test_inv_erf<__float128>();
 }
