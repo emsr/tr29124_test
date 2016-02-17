5d3520a542627e5dbfffb01acd590739638a78a3
U-ALIONSCIENCE\ESmith-rowland <ESmith-rowland@SEG_SMTHRWLNDLT.alionscience.com>
2016-02-17 16:25:17 -0500

    Add __float128. Added a series for the nome to help near 0 where K takes many iterations. K could use a similar series. I should also just fit K.

--- test_comp_ellint_1.cpp
+++ test_comp_ellint_1.cpp
@@ -1,4 +1,4 @@
-// $HOME/bin/bin/g++ -std=c++1z -o test_comp_ellint_1 test_comp_ellint_1.cpp
+// $HOME/bin/bin/g++ -std=gnu++1z -o test_comp_ellint_1 test_comp_ellint_1.cpp -lquadmath
 
 // LD_LIBRARY_PATH=$HOME/bin/lib64:$LD_LIBRARY_PATH ./test_comp_ellint_1 > test_comp_ellint_1.txt
 
@@ -7,6 +7,7 @@
 #include <iomanip>
 #include <limits>
 #include <experimental/utility>
+#include "float128.h"
 
 auto IMAX = 0;
 
@@ -16,16 +17,29 @@ template<typename _Tp>
   __ellint_K(_Tp __k)
   {
     constexpr auto _S_max_iter = 100;
-    auto __am = _Tp{0.5} * (_Tp{1} + __k);
+    auto __am = _Tp{0.5Q} * (_Tp{1} + __k);
     auto __gm = std::sqrt(__k);
     for (int __i = 0; __i < _S_max_iter; ++__i)
       {
 IMAX = __i;
-	__gm = std::sqrt(__gm * std::exchange(__am, _Tp{0.5} * (__am + __gm)));
+	__gm = std::sqrt(__gm * std::exchange(__am, _Tp{0.5Q} * (__am + __gm)));
 	if (std::abs(__am - __gm) < std::numeric_limits<_Tp>::epsilon())
 	  break;
       }
     return __gm;
+  }
+
+//  Use MacLaurin series to calculate the elliptic nome.
+template<typename _Tp>
+  _Tp
+  __ellint_nome_series(_Tp __k)
+  {
+    auto __m = __k * __k; 
+    return __m * ((_Tp{1} / _Tp{16})
+	 + __m * ((_Tp{1} / _Tp{32})
+	 + __m * ((_Tp{21} / _Tp{1024})
+	 + __m * ((_Tp{31} / _Tp{2048})
+	 + __m * (_Tp{6257} / _Tp{524288})))));
   }
 
 //  Use AGM to do an ab initio calculation of the elliptic nome.
@@ -33,11 +47,17 @@ template<typename _Tp>
   _Tp
   __ellint_nome(_Tp __k)
   {
-    constexpr auto _S_pi = _Tp{3.1415926535897932384626433832795029L};
-    auto __kp = std::sqrt((_Tp{1} - __k) * (_Tp{1} + __k));
-    auto __K = __ellint_K(__k);
-    auto __Kp = __ellint_K(__kp);
-    return std::exp(-_S_pi * __Kp / __K);
+    constexpr auto _S_eps = std::numeric_limits<_Tp>::epsilon();
+    if (__k < std::pow(_Tp{67} * _S_eps, _Tp{0.125Q}))
+      return __ellint_nome_series(__k);
+    else
+      {
+	constexpr auto _S_pi = _Tp{3.1415926535897932384626433832795029Q};
+	auto __kp = std::sqrt((_Tp{1} - __k) * (_Tp{1} + __k));
+	auto __K = __ellint_K(__k);
+	auto __Kp = __ellint_K(__kp);
+	return std::exp(-_S_pi * __Kp / __K);
+      }
   }
 
 template<typename _Tp>
@@ -49,7 +69,7 @@ template<typename _Tp>
     auto width = 6 + std::cout.precision();
     for (int i = 0; i <= 1000; ++i)
       {
-	auto k = _Tp(i * 0.001L);
+	auto k = _Tp(i * 0.001Q);
 	std::cout << ' ' << IMAX
 		  << ' ' << std::setw(width) << k
 		  << ' ' << std::setw(width) << __ellint_K(k)
@@ -68,4 +88,7 @@ main()
 
   std::cout << "\n\nlong double\n";
   test_K<long double>();
+
+  std::cout << "\n\n__float128\n";
+  test_K<__float128>();
 }
