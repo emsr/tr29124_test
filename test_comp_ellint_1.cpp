a50726a0110d19aabf826d0a3fbc1f7fb8b166c4
Ed Smith-Rowland <3dw4rd@verizon.net>
2016-02-17 23:56:54 -0500

    Factored ellint nome some. I should test against shipped comp_elling_1 - especially since I went to comp_rf from rf. Removes loop count. Reenerated output.

--- test_comp_ellint_1.cpp
+++ test_comp_ellint_1.cpp
@@ -1,6 +1,6 @@
-// $HOME/bin/bin/g++ -std=gnu++1z -o test_comp_ellint_1 test_comp_ellint_1.cpp -lquadmath
+// $HOME/bin_specfun/bin/g++ -std=gnu++1z -o test_comp_ellint_1 test_comp_ellint_1.cpp -lquadmath
 
-// LD_LIBRARY_PATH=$HOME/bin/lib64:$LD_LIBRARY_PATH ./test_comp_ellint_1 > test_comp_ellint_1.txt
+// LD_LIBRARY_PATH=$HOME/bin_specfun/lib64:$LD_LIBRARY_PATH ./test_comp_ellint_1 > test_comp_ellint_1.txt
 
 #include <cmath>
 #include <iostream>
@@ -9,19 +9,16 @@
 #include <experimental/utility>
 #include "float128.h"
 
-auto IMAX = 0;
-
 //  Use AGM to do an ab initio calculation of K(k).
 template<typename _Tp>
   _Tp
-  __ellint_K(_Tp __k)
+  __comp_ellint_1_agm(_Tp __k)
   {
     constexpr auto _S_max_iter = 100;
     auto __am = _Tp{0.5Q} * (_Tp{1} + __k);
     auto __gm = std::sqrt(__k);
     for (int __i = 0; __i < _S_max_iter; ++__i)
       {
-IMAX = __i;
 	__gm = std::sqrt(__gm * std::exchange(__am, _Tp{0.5Q} * (__am + __gm)));
 	if (std::abs(__am - __gm) < std::numeric_limits<_Tp>::epsilon())
 	  break;
@@ -29,7 +26,10 @@ IMAX = __i;
     return __gm;
   }
 
-//  Use MacLaurin series to calculate the elliptic nome.
+/**
+ *  Use MacLaurin series to calculate the elliptic nome
+ *  given the , k.
+ */
 template<typename _Tp>
   _Tp
   __ellint_nome_series(_Tp __k)
@@ -42,7 +42,24 @@ template<typename _Tp>
 	 + __m * (_Tp{6257} / _Tp{524288})))));
   }
 
-//  Use AGM to do an ab initio calculation of the elliptic nome.
+/**
+ *  Use the arithmetic-geometric mean to calculate the elliptic nome
+ *  given the , k.
+ */
+template<typename _Tp>
+  _Tp
+  __ellint_nome_agm(_Tp __k)
+  {
+    constexpr auto _S_pi = _Tp{3.1415926535897932384626433832795029Q};
+    auto __kp = std::sqrt((_Tp{1} - __k) * (_Tp{1} + __k));
+    auto __K = __comp_ellint_1_agm(__k);
+    auto __Kp = __comp_ellint_1_agm(__kp);
+    return std::exp(-_S_pi * __Kp / __K);
+  }
+
+/**
+ *  Return the elliptic nome given the , k.
+ */
 template<typename _Tp>
   _Tp
   __ellint_nome(_Tp __k)
@@ -51,13 +68,7 @@ template<typename _Tp>
     if (__k < std::pow(_Tp{67} * _S_eps, _Tp{0.125Q}))
       return __ellint_nome_series(__k);
     else
-      {
-	constexpr auto _S_pi = _Tp{3.1415926535897932384626433832795029Q};
-	auto __kp = std::sqrt((_Tp{1} - __k) * (_Tp{1} + __k));
-	auto __K = __ellint_K(__k);
-	auto __Kp = __ellint_K(__kp);
-	return std::exp(-_S_pi * __Kp / __K);
-      }
+      return __ellint_nome_agm(__k);
   }
 
 template<typename _Tp>
@@ -70,9 +81,8 @@ template<typename _Tp>
     for (int i = 0; i <= 1000; ++i)
       {
 	auto k = _Tp(i * 0.001Q);
-	std::cout << ' ' << IMAX
-		  << ' ' << std::setw(width) << k
-		  << ' ' << std::setw(width) << __ellint_K(k)
+	std::cout << ' ' << std::setw(width) << k
+		  << ' ' << std::setw(width) << __comp_ellint_1_agm(k)
 		  << ' ' << std::setw(width) << __ellint_nome(k) << '\n';
       }
   }
