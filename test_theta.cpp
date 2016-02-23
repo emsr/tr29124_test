0457e8af53762c7657aa9e8c47935eb5677f6b9e
Ed Smith-Rowland <3dw4rd@verizon.net>
2016-02-22 20:49:06 -0500

    Fix up test_theta... Of couse we have issues.

--- test_theta.cpp
+++ test_theta.cpp
@@ -2,6 +2,8 @@
 
 // LD_LIBRARY_PATH=$HOME/bin_specfun/lib64:$LD_LIBRARY_PATH ./test_theta > test_theta.txt
 
+#include <iostream>
+#include <iomanip>
 #include <cmath>
 
 template<typename _Tp>
@@ -9,24 +11,30 @@ template<typename _Tp>
   test_theta()
   {
     std::cout.precision(std::numeric_limits<_Tp>::digits10);
+    auto width = 8 + std::cout.precision();
     std::cout << std::showpoint << std::scientific;
 
     std::cout << "\n\n Theta function values\n";
     std::cout << " =====================\n";
-    std::cout << ' ' << std::setw(width) << "x"
-	      << ' ' << std::setw(width) << "theta_1"
-	      << ' ' << std::setw(width) << "theta_2"
-	      << ' ' << std::setw(width) << "theta_3"
-	      << ' ' << std::setw(width) << "theta_4"
-	      << '\n';
     for (int i = 0; i <= 20; ++i)
       {
-	auto nu = 0.1 * i;
+	auto nu = _Tp(i * 0.1Q);
 	std::cout << '\n' << ' ' << std::setw(width) << nu << '\n';
-	for (int j = 0; j < 100; ++j)
+	std::cout << ' ' << std::setw(width) << "x"
+		  << ' ' << std::setw(width) << "theta_1"
+		  << ' ' << std::setw(width) << "theta_2"
+		  << ' ' << std::setw(width) << "theta_3"
+		  << ' ' << std::setw(width) << "theta_4"
+		  << '\n';
+	std::cout << ' ' << std::setw(width) << "------------------------"
+		  << ' ' << std::setw(width) << "------------------------"
+		  << ' ' << std::setw(width) << "------------------------"
+		  << ' ' << std::setw(width) << "------------------------"
+		  << ' ' << std::setw(width) << "------------------------"
+		  << '\n';
+	for (int j = 0; j <= 100; ++j)
 	  {
-	    auto k = j * 0.01;
-	    auto q = __gnu_cxx::ellnome(k);
+	    auto x = _Tp(j * 0.01Q);
 	    std::cout << ' ' << std::setw(width) << x
 		      << ' ' << std::setw(width) << __gnu_cxx::theta_1(nu, x)
 		      << ' ' << std::setw(width) << __gnu_cxx::theta_2(nu, x)
@@ -38,36 +46,45 @@ template<typename _Tp>
 
     std::cout << "\n\n Theta function values and compares with Jacobi elliptic functions\n";
     std::cout << " =================================================================\n";
-    std::cout << ' ' << std::setw(width) << "x"
-	      << ' ' << std::setw(width) << "theta_s"
-	      << ' ' << std::setw(width) << "theta_c"
-	      << ' ' << std::setw(width) << "theta_n"
-	      << ' ' << std::setw(width) << "theta_d"
-	      << ' ' << std::setw(width) << "th_s/th_n - sn"
-	      << ' ' << std::setw(width) << "th_c/th_n - cn"
-	      << ' ' << std::setw(width) << "th_d/th_n - dn"
-	      << '\n';
-    for (int i = 0; i <= 20; ++i)
+    for (int i = -10; i <= 10; ++i)
       {
-	auto k = 0.1 * i;
-	std::cout << '\n' << ' ' << std::setw(width) << k << '\n';
-	for (int j = 0; j < 100; ++j)
+	auto k = _Tp(i * 0.1Q);
+	std::cout << '\n' << ' ' << std::setw(width) << k;
+	std::cout << '\n' << ' ' << std::setw(width) << __gnu_cxx::ellnome(k) << '\n';
+	std::cout << ' ' << std::setw(width) << "x"
+		  << ' ' << std::setw(width) << "theta_s"
+		  << ' ' << std::setw(width) << "theta_c"
+		  << ' ' << std::setw(width) << "theta_n"
+		  << ' ' << std::setw(width) << "theta_d"
+		  << ' ' << std::setw(width) << "th_s/th_n - sn"
+		  << ' ' << std::setw(width) << "th_c/th_n - cn"
+		  << ' ' << std::setw(width) << "th_d/th_n - dn"
+		  << '\n';
+	std::cout << ' ' << std::setw(width) << "------------------------"
+		  << ' ' << std::setw(width) << "------------------------"
+		  << ' ' << std::setw(width) << "------------------------"
+		  << ' ' << std::setw(width) << "------------------------"
+		  << ' ' << std::setw(width) << "------------------------"
+		  << ' ' << std::setw(width) << "------------------------"
+		  << ' ' << std::setw(width) << "------------------------"
+		  << ' ' << std::setw(width) << "------------------------"
+		  << '\n';
+	for (int j = 0; j <= 100; ++j)
 	  {
-	    auto x = j * 0.01;
+	    auto x = _Tp(j * 0.01Q);
 	    auto s = __gnu_cxx::theta_s(k, x);
 	    auto c = __gnu_cxx::theta_c(k, x);
 	    auto d = __gnu_cxx::theta_d(k, x);
 	    auto n = __gnu_cxx::theta_n(k, x);
 	    auto sncndn = std::__detail::__jacobi_sncndn(k, x);
-	    std::cout << ' ' << std::setw() << k
-		      << ' ' << x
-		      << ' ' << s
-		      << ' ' << c
-		      << ' ' << d
-		      << ' ' << n
-		      << ' ' << s / n - std::get<0>(sncndn)
-		      << ' ' << c / n - std::get<1>(sncndn)
-		      << ' ' << d / n - std::get<2>(sncndn)
+	    std::cout << ' ' << std::setw(width) << x
+		      << ' ' << std::setw(width) << s
+		      << ' ' << std::setw(width) << c
+		      << ' ' << std::setw(width) << d
+		      << ' ' << std::setw(width) << n
+		      << ' ' << std::setw(width) << s / n - std::get<0>(sncndn)
+		      << ' ' << std::setw(width) << c / n - std::get<1>(sncndn)
+		      << ' ' << std::setw(width) << d / n - std::get<2>(sncndn)
 		      << '\n';
 	  }
       }
