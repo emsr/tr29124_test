edeed943a7d249f194c239b1eeb70806ddf3c782
Ed Smith-Rowland <3dw4rd@verizon.net>
2016-02-27 20:27:11 -0500

    Start getting CEPHES up and running.

---
+++ cephes/bessel/cephes_bessel.h
@@ -0,0 +1,33 @@
+
+double jv (double nu, double x);
+double jn(int n, double x);
+double j0(double x);
+double j1(double x);
+
+double yv (double nu, double x);
+double yn(int n, double x);
+double y0(double x);
+double y1(double x);
+
+double onef2 (double a, double b, double c, double x, double* err);
+
+double threef0 (double a, double b, double c, double x, double* err);
+
+double struve(double nu, double x);
+
+double psi(double x);
+
+double iv(double nu, double x);
+double in(int n, double x);
+double i0(double x);
+double i1(double x);
+
+double kn(int n, double x);
+double k0(double x);
+double k1(double x);
+
+double hyperg(double a, double b, double x);
+double hyp2f1(double a, double b, double c, double x);
+
+int airy(double x, double* ai, double* aip, double* bi, double* bip);
+
