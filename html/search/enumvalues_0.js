80b7ed132d2a5dba6d4074abe5d1931bd083fee3
Ed Smith-Rowland <3dw4rd@verizon.net>
2016-04-01 08:46:59 -0400

    Add specfun.h to Doxyfile.

--- html/search/enumvalues_0.js
+++ html/search/enumvalues_0.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['cosint',['COSINT',['../namespacestd_1_1____detail.html#a09a7f71de3b59ea17555c75cab41685ba4b5ea0190e37e477a2874e231190489e',1,'std::__detail']]]
+  ['_5fglibcxx_5fjacobi_5fcn',['_GLIBCXX_JACOBI_CN',['../namespace____gnu__cxx.html#ad6c62dd86a596716cece6ac2d4cfd4b3a86d36c2efbbbfddcfb1e552853d72d65',1,'__gnu_cxx']]],
+  ['_5fglibcxx_5fjacobi_5fdn',['_GLIBCXX_JACOBI_DN',['../namespace____gnu__cxx.html#ad6c62dd86a596716cece6ac2d4cfd4b3a4576182edcbe93595def76dd1e61e0f7',1,'__gnu_cxx']]],
+  ['_5fglibcxx_5fjacobi_5fsn',['_GLIBCXX_JACOBI_SN',['../namespace____gnu__cxx.html#ad6c62dd86a596716cece6ac2d4cfd4b3a3f3a4942031777493cbc33f592c941c7',1,'__gnu_cxx']]]
 ];
