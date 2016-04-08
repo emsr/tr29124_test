e42984308eacbdf720831a8e2f4311738fcf96c3
Ed Smith-Rowland <3dw4rd@verizon.net>
2016-04-08 12:37:14 -0400

    I added stats output to both hankel testers.

--- test_cyl_hankel.cpp
+++ test_cyl_hankel.cpp
@@ -22,8 +22,10 @@ template<typename _Tp>
     std::cout.flags(std::ios::showpoint);
     auto width = 8 + std::cout.precision();
 
+    auto max_max_abs_frac = _Tp{-1};
     for (int inu = 0; inu <= +500; ++inu)
       {
+	auto max_abs_frac = _Tp{-1};
 	auto nu = inu * 0.1;
 	std::cout << '\n';
 	for (int iz = 1; iz <= +1000; ++iz)
@@ -31,14 +33,21 @@ template<typename _Tp>
 	    auto z = iz * 0.01;
 	    auto h1s = __gnu_cxx::cyl_hankel_1(nu, z);
 	    auto h1b = beast::cyl_hankel_1(nu, z);
+	    auto abs_frac = std::abs((h1s - h1b) / h1b);
 	    std::cout << ' ' << std::setw(width) << nu
 		      << ' ' << std::setw(width) << z
 		      << ' ' << std::setw(2 * width) << h1s
 		      << ' ' << std::setw(2 * width) << h1b
 		      << ' ' << std::setw(width) << std::abs((h1s - h1b) / h1b)
 		      << std::endl;
+	    if (abs_frac > max_abs_frac)
+	      max_abs_frac = abs_frac;
 	  }
+	std::cout << " max_abs_frac = " << std::setw(width) << max_abs_frac << '\n';
+	if (max_abs_frac > max_max_abs_frac)
+	  max_max_abs_frac = max_abs_frac;
       }
+    std::cout << " max_max_abs_frac = " << std::setw(width) << max_max_abs_frac << '\n';
   }
 
 int
