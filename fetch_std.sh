dba6d90447bc20360cd8f5c19de2f909d72f22d7
emsr <3dw4rd@verizon.net>
2016-04-12 18:00:19 -0400

    Rework the scripts to take variables. I'm trying to make it easier for other people to hack this thing. There is junk in the directories currently - see comments in scripts. I really should dump the local inplementation and other files in preference of the subdirs.

--- fetch_std.sh
+++ fetch_std.sh
@@ -1,39 +1,55 @@
 #!/bin/bash
 
 tool="cp -f"
-gcc_dir="$HOME/gcc_specfun/libstdc++-v3/include/bits"
-ext_dir="$HOME/gcc_specfun/libstdc++-v3/include/ext"
 
-${tool} ${gcc_dir}/specfun.h         ./specfun.h
-${tool} ${gcc_dir}/specfun_util.h    ./specfun_util.h
-${tool} ${gcc_dir}/complex_util.h    ./complex_util.h
-${tool} ${gcc_dir}/complex_util.tcc  ./complex_util.tcc
-${tool} ${gcc_dir}/float128.h        ./float128.h
-${tool} ${gcc_dir}/float128.tcc      ./float128.tcc
-${tool} ${gcc_dir}/numeric_limits.h  ./numeric_limits.h
-${tool} ${ext_dir}/math_const.h      ./math_const.h
-${tool} ${gcc_dir}/sf_bessel.tcc     ./sf_bessel.tcc
-${tool} ${gcc_dir}/sf_beta.tcc       ./sf_beta.tcc
-${tool} ${gcc_dir}/sf_cardinal.tcc   ./sf_cardinal.tcc
-${tool} ${gcc_dir}/sf_chebyshev.tcc  ./sf_chebyshev.tcc
-${tool} ${gcc_dir}/sf_dawson.tcc     ./sf_dawson.tcc
-${tool} ${gcc_dir}/sf_ellint.tcc     ./sf_ellint.tcc
-${tool} ${gcc_dir}/sf_expint.tcc     ./sf_expint.tcc
-${tool} ${gcc_dir}/sf_fresnel.tcc    ./sf_fresnel.tcc
-${tool} ${gcc_dir}/sf_gamma.tcc      ./sf_gamma.tcc
-${tool} ${gcc_dir}/sf_gegenbauer.tcc ./sf_gegenbauer.tcc
-${tool} ${gcc_dir}/sf_hermite.tcc    ./sf_hermite.tcc
-${tool} ${gcc_dir}/sf_hydrogen.tcc   ./sf_hydrogen.tcc
-${tool} ${gcc_dir}/sf_hyperg.tcc     ./sf_hyperg.tcc
-${tool} ${gcc_dir}/sf_hypint.tcc     ./sf_hypint.tcc
-${tool} ${gcc_dir}/sf_jacobi.tcc     ./sf_jacobi.tcc
-${tool} ${gcc_dir}/sf_laguerre.tcc   ./sf_laguerre.tcc
-${tool} ${gcc_dir}/sf_legendre.tcc   ./sf_legendre.tcc
-${tool} ${gcc_dir}/sf_mod_bessel.tcc ./sf_mod_bessel.tcc
-${tool} ${gcc_dir}/sf_theta.tcc      ./sf_theta.tcc
-${tool} ${gcc_dir}/sf_trigint.tcc    ./sf_trigint.tcc
-${tool} ${gcc_dir}/sf_zeta.tcc       ./sf_zeta.tcc
-${tool} ${gcc_dir}/sf_owens_t.tcc    ./sf_owens_t.tcc
-${tool} ${gcc_dir}/sf_polylog.tcc    ./sf_polylog.tcc
-${tool} ${gcc_dir}/sf_airy.tcc       ./sf_airy.tcc
-${tool} ${gcc_dir}/sf_hankel.tcc     ./sf_hankel.tcc
+src_dir="$HOME/gcc_specfun/libstdc++-v3/include"
+if [ -d "$1" ]; then 
+  src_dir="$1"
+fi
+src_impl_dir="${src_dir}/bits"
+src_ext_dir="${src_dir}/ext"
+
+dst_dir="."
+if [ -d "$2" ]; then 
+  dst_dir="$2"
+fi
+dst_impl_dir="${dst_dir}"
+dst_ext_dir="${dst_dir}"
+
+${tool} "${src_dir}/cmath"                  "${dst_dir}/cmath"
+
+${tool} "${src_ext_dir}/math_const.h"       "${dst_ext_dir}/math_const.h"
+#${tool} "${src_ext_dir}/cmath"              "${dst_ext_dir}/cmath_ext"
+
+${tool} "${src_impl_dir}/specfun.h"         "${dst_impl_dir}/specfun.h"
+${tool} "${src_impl_dir}/specfun_util.h"    "${dst_impl_dir}/specfun_util.h"
+${tool} "${src_impl_dir}/complex_util.h"    "${dst_impl_dir}/complex_util.h"
+${tool} "${src_impl_dir}/complex_util.tcc"  "${dst_impl_dir}/complex_util.tcc"
+${tool} "${src_impl_dir}/float128.h"        "${dst_impl_dir}/float128.h"
+${tool} "${src_impl_dir}/float128.tcc"      "${dst_impl_dir}/float128.tcc"
+${tool} "${src_impl_dir}/numeric_limits.h"  "${dst_impl_dir}/numeric_limits.h"
+${tool} "${src_impl_dir}/sf_bessel.tcc"     "${dst_impl_dir}/sf_bessel.tcc"
+${tool} "${src_impl_dir}/sf_beta.tcc"       "${dst_impl_dir}/sf_beta.tcc"
+${tool} "${src_impl_dir}/sf_cardinal.tcc"   "${dst_impl_dir}/sf_cardinal.tcc"
+${tool} "${src_impl_dir}/sf_chebyshev.tcc"  "${dst_impl_dir}/sf_chebyshev.tcc"
+${tool} "${src_impl_dir}/sf_dawson.tcc"     "${dst_impl_dir}/sf_dawson.tcc"
+${tool} "${src_impl_dir}/sf_ellint.tcc"     "${dst_impl_dir}/sf_ellint.tcc"
+${tool} "${src_impl_dir}/sf_expint.tcc"     "${dst_impl_dir}/sf_expint.tcc"
+${tool} "${src_impl_dir}/sf_fresnel.tcc"    "${dst_impl_dir}/sf_fresnel.tcc"
+${tool} "${src_impl_dir}/sf_gamma.tcc"      "${dst_impl_dir}/sf_gamma.tcc"
+${tool} "${src_impl_dir}/sf_gegenbauer.tcc" "${dst_impl_dir}/sf_gegenbauer.tcc"
+${tool} "${src_impl_dir}/sf_hermite.tcc"    "${dst_impl_dir}/sf_hermite.tcc"
+${tool} "${src_impl_dir}/sf_hydrogen.tcc"   "${dst_impl_dir}/sf_hydrogen.tcc"
+${tool} "${src_impl_dir}/sf_hyperg.tcc"     "${dst_impl_dir}/sf_hyperg.tcc"
+${tool} "${src_impl_dir}/sf_hypint.tcc"     "${dst_impl_dir}/sf_hypint.tcc"
+${tool} "${src_impl_dir}/sf_jacobi.tcc"     "${dst_impl_dir}/sf_jacobi.tcc"
+${tool} "${src_impl_dir}/sf_laguerre.tcc"   "${dst_impl_dir}/sf_laguerre.tcc"
+${tool} "${src_impl_dir}/sf_legendre.tcc"   "${dst_impl_dir}/sf_legendre.tcc"
+${tool} "${src_impl_dir}/sf_mod_bessel.tcc" "${dst_impl_dir}/sf_mod_bessel.tcc"
+${tool} "${src_impl_dir}/sf_theta.tcc"      "${dst_impl_dir}/sf_theta.tcc"
+${tool} "${src_impl_dir}/sf_trigint.tcc"    "${dst_impl_dir}/sf_trigint.tcc"
+${tool} "${src_impl_dir}/sf_zeta.tcc"       "${dst_impl_dir}/sf_zeta.tcc"
+${tool} "${src_impl_dir}/sf_owens_t.tcc"    "${dst_impl_dir}/sf_owens_t.tcc"
+${tool} "${src_impl_dir}/sf_polylog.tcc"    "${dst_impl_dir}/sf_polylog.tcc"
+${tool} "${src_impl_dir}/sf_airy.tcc"       "${dst_impl_dir}/sf_airy.tcc"
+${tool} "${src_impl_dir}/sf_hankel.tcc"     "${dst_impl_dir}/sf_hankel.tcc"
