b53769a3a1acc7a4a27e1f7f44174e0cbc3a658a
Ed Smith-Rowland <3dw4rd@verizon.net>
2016-04-28 17:04:58 -0400

    Regenerate Doxy.

--- html/search/functions_d.js
+++ html/search/functions_d.js
@@ -6,9 +6,9 @@ var searchData=
   ['pochhammer_5fu',['pochhammer_u',['../group__gnu__math__spec__func.html#ga4cadb141501737e85c8d6511fe3d3b94',1,'__gnu_cxx']]],
   ['pochhammer_5fuf',['pochhammer_uf',['../group__gnu__math__spec__func.html#ga520a93cbd1963b22679e865f29f36331',1,'__gnu_cxx']]],
   ['pochhammer_5ful',['pochhammer_ul',['../group__gnu__math__spec__func.html#ga169c9e8f9d84e51187a0fe4ecaba0947',1,'__gnu_cxx']]],
-  ['polylog',['polylog',['../group__gnu__math__spec__func.html#gafbe8ee0e3edd53cd5cd421e9b913f6b9',1,'__gnu_cxx']]],
-  ['polylogf',['polylogf',['../group__gnu__math__spec__func.html#ga5376edb72358b777035a78b929deb49f',1,'__gnu_cxx']]],
-  ['polylogl',['polylogl',['../group__gnu__math__spec__func.html#ga9eb79e506eda210610bc59c1912b4d0f',1,'__gnu_cxx']]],
+  ['polylog',['polylog',['../group__gnu__math__spec__func.html#ga49da0ff03584a0bbc64f925a550537d3',1,'__gnu_cxx::polylog(_Tp __s, _Wp __w)'],['../group__gnu__math__spec__func.html#ga2789ccca20a89af26d1bcdf4f9a2c400',1,'__gnu_cxx::polylog(_Tp __s, std::complex&lt; _Tp &gt; __w)']]],
+  ['polylogf',['polylogf',['../group__gnu__math__spec__func.html#ga5bcdd35473144a6d8efc258a79bc82d8',1,'__gnu_cxx::polylogf(float __s, float __w)'],['../group__gnu__math__spec__func.html#ga5376edb72358b777035a78b929deb49f',1,'__gnu_cxx::polylogf(float __s, std::complex&lt; float &gt; __w)']]],
+  ['polylogl',['polylogl',['../group__gnu__math__spec__func.html#ga3aa007b4b4e345c30be015ab145d5598',1,'__gnu_cxx::polylogl(long double __s, long double __w)'],['../group__gnu__math__spec__func.html#ga9eb79e506eda210610bc59c1912b4d0f',1,'__gnu_cxx::polylogl(long double __s, std::complex&lt; long double &gt; __w)']]],
   ['psi',['psi',['../group__gnu__math__spec__func.html#ga850cdad2e428a553e5adf2474a6a385a',1,'__gnu_cxx']]],
   ['psif',['psif',['../group__gnu__math__spec__func.html#ga47ae74abfaa3f549eed4a87b1b63449d',1,'__gnu_cxx']]],
   ['psil',['psil',['../group__gnu__math__spec__func.html#gaaf230aedcb20a1c5a43fc71132bb0dc1',1,'__gnu_cxx']]]
