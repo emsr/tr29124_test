e378f55a4fd8c7cb55d4c0fdd79f9011a637c71d
Ed Smith-Rowland <3dw4rd@verizon.net>
2016-05-10 12:53:13 -0400

    Update airy_toy with a get_zeta from hankel_toy that mimics the real version (we could probably fold these). I got the right series for C and D in airy_toy. There is still a 3/2 wronski factor in some regions.  I may need to write a full solution. I should do that anyway.
    Spiffed up the test_jacobi_inv with *f and *l overloads.
    I should make a proper impl that does NaN and range testing though.

--- test_jacobi_inv.cpp
+++ test_jacobi_inv.cpp
@@ -1,6 +1,6 @@
 // $HOME/bin_specfun/bin/g++ -std=gnu++17 -g -Wall -Wextra -o test_jacobi_inv test_jacobi_inv.cpp -L$HOME/bin/lib64 -lquadmath
 
-// LD_LIBRARY_PATH=$HOME/bin_specfun/lib64:$LD_LIBRARY_PATH ./test_jacobi_inv > test_jacobi_inv.new
+// LD_LIBRARY_PATH=$HOME/bin_specfun/lib64:$LD_LIBRARY_PATH ./test_jacobi_inv > test_jacobi_inv.txt
 
 // g++ -std=gnu++17 -g -Wall -Wextra -DNO_LOGBQ -I. -o test_jacobi_inv test_jacobi_inv.cpp -lquadmath
 
@@ -11,20 +11,88 @@
 #include <iomanip>
 #include <limits>
 
-template<typename _Tp>
-  _Tp
-  jacobi_acn(_Tp __k, _Tp __v)
+  // Jacobi elliptic cosine amplitude inverse functions.
+
+  /**
+   * 
+   */
+  float
+  jacobi_acnf(float __k, float __v)
   { return std::ellint_1(__k, std::acos(__v)); }
 
-template<typename _Tp>
-  _Tp
-  jacobi_asn(_Tp __k, _Tp __v)
+  /**
+   * 
+   */
+  long double
+  jacobi_acnl(long double __k, long double __v)
+  { return std::ellint_1(__k, std::acos(__v)); }
+
+  /**
+   * 
+   */
+  template<typename _Tk, typename _Tv>
+    __gnu_cxx::__promote_num_t<_Tk, _Tv>
+    jacobi_acn(_Tk __k, _Tv __v)
+    {
+      using __type = __gnu_cxx::__promote_num_t<_Tk, _Tv>;
+      return std::ellint_1<__type>(__k, std::acos(__v));
+    }
+
+  // Jacobi elliptic sine amplitude inverse functions.
+
+  /**
+   * 
+   */
+  float
+  jacobi_asnf(float __k, float __v)
   { return std::ellint_1(__k, std::asin(__v)); }
 
-template<typename _Tp>
-  _Tp
-  jacobi_adn(_Tp __k, _Tp __v)
-  { return std::ellint_1(__k, std::asin(std::sqrt(_Tp{1} - __v * __v) / __k)); }
+  /**
+   * 
+   */
+  long double
+  jacobi_asnl(long double __k, long double __v)
+  { return std::ellint_1(__k, std::asin(__v)); }
+
+  /**
+   * 
+   */
+  template<typename _Tk, typename _Tv>
+    __gnu_cxx::__promote_num_t<_Tk, _Tv>
+    jacobi_asn(_Tk __k, _Tv __v)
+    {
+      using __type = __gnu_cxx::__promote_num_t<_Tk, _Tv>;
+      return std::ellint_1<__type>(__k, std::asin(__v));
+    }
+
+  // Jacobi elliptic delta amplitude inverse functions.
+
+  /**
+   * 
+   */
+  float
+  jacobi_adnf(float __k, float __v)
+  { return std::ellint_1(__k, std::asin(std::sqrt(1.0F - __v * __v) / __k)); }
+
+  /**
+   * 
+   */
+  long double
+  jacobi_adnl(long double __k, long double __v)
+  { return std::ellint_1(__k, std::asin(std::sqrt(1.0L - __v * __v) / __k)); }
+
+  /**
+   * 
+   */
+  template<typename _Tk, typename _Tv>
+    __gnu_cxx::__promote_num_t<_Tk, _Tv>
+    jacobi_adn(_Tk __k, _Tv __v)
+    {
+      using __type = __gnu_cxx::__promote_num_t<_Tk, _Tv>;
+      auto __root = std::sqrt(__type{1} - __v * __v);
+      return std::ellint_1<__type>(__k, std::asin(__root / __k));
+    }
+
 
 int
 main()
@@ -38,9 +106,9 @@ main()
 
   std::cout << "\n\nJacobi cn inverse\n";
   std::cout << "++++++++++++++++++++++++++++++\n";
-  std::cout << std::setw(width) << "y"
-	    << std::setw(width) << "cn(k,u)"
-	    << std::setw(width) << "acn(k,cn(k,u))"
+  std::cout << std::setw(width) << "u"
+	    << std::setw(width) << "v = cn(k,u)"
+	    << std::setw(width) << "|acn(k,v) - u|"
 	      << '\n';
   std::cout << std::setw(width) << "=============="
 	    << std::setw(width) << "=============="
@@ -59,9 +127,9 @@ main()
 
   std::cout << "\n\nJacobi sn inverse\n";
   std::cout << "++++++++++++++++++++++++++++++\n";
-  std::cout << std::setw(width) << "y"
-	    << std::setw(width) << "sn(k,u)"
-	    << std::setw(width) << "asn(k,sn(k,u))"
+  std::cout << std::setw(width) << "u"
+	    << std::setw(width) << "v = sn(k,u)"
+	    << std::setw(width) << "|asn(k,v) - u|"
 	      << '\n';
   std::cout << std::setw(width) << "=============="
 	    << std::setw(width) << "=============="
@@ -80,9 +148,9 @@ main()
 
   std::cout << "\n\nJacobi dn inverse\n";
   std::cout << "++++++++++++++++++++++++++++++\n";
-  std::cout << std::setw(width) << "y"
-	    << std::setw(width) << "dn(k,u)"
-	    << std::setw(width) << "adn(k,dn(k,u))"
+  std::cout << std::setw(width) << "u"
+	    << std::setw(width) << "v = dn(k,u)"
+	    << std::setw(width) << "|adn(k,v) - u|"
 	      << '\n';
   std::cout << std::setw(width) << "=============="
 	    << std::setw(width) << "=============="
