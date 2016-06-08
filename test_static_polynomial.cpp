1caa8c2738c542b034fc735a8b44fdc2e0f74a1d
emsr <3dw4rd@verizon.net>
2016-06-08 16:55:23 -0400

    Fix the static polynomial and start a tester. Add even and odd-term evaluation to the polynomials. This last will be used in some Airy asymptotics and elsewhere. Started an impl of array_ref because I'm a nut. I started _StaticPolynomial with std::array because it was easy. It might well be better as an array_ref or a span because all I really care about with _StaticPolynomial is a nice way to envapsulate hard-coded polynomial literals where all you really care about is evaluation.

--- test_static_polynomial.cpp
+++ test_static_polynomial.cpp
@@ -1,10 +1,20 @@
-// $HOME/bin_specfun/bin/g++ -std=gnu++1z -o test_static_polynomial test_static_polynomial.cpp
+// $HOME/bin/bin/g++ -g -std=gnu++1z -o test_static_polynomial test_static_polynomial.cpp
 
-// 
+// LD_LIBRARY_PATH=$HOME/bin/lib64:$LD_LIBRARY_PATH ./test_static_polynomial
+
+//  Get past a bug....
+// $HOME/bin/bin/g++ -D__STDCPP_WANT_MATH_SPEC_FUNCS__=0 -o test_static_polynomial test_static_polynomial.cpp
 
 #include "static_polynomial.h"
+#include <iostream>
+#include <complex>
+#include <sstream>
 
 int
 main()
 {
+  float aa[5]{1.0f, 2.0f, -1.5f, 0.2f, -0.1f};
+
+  __gnu_cxx::_StaticPolynomial<float, 5> p(aa);
+  //__gnu_cxx::_StaticPolynomial<float, 5> a(1.0f, 2.0f, -1.5f, 0.2f, -0.1f);
 }
