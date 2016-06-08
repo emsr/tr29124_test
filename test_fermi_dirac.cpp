dd751e69de4dc8f11c23187078cccd6e805f8403
Ed Smith-Rowland <3dw4rd@verizon.net>
2016-06-07 23:49:43 -0400

    Another round of Airy cleanup.

---
+++ test_fermi_dirac.cpp
@@ -0,0 +1,32 @@
+// $HOME/bin_specfun/bin/g++ -std=gnu++1z -o test_fermi_dirac test_fermi_dirac.cpp gsl_wrap.cpp -lgsl -lgslcblas
+
+// LD_LIBRARY_PATH=$HOME/bin_specfun/lib64:$LD_LIBRARY_PATH ./test_fermi_dirac
+
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <ext/cmath>
+
+
+template<typename _Tp>
+  void
+  run_fermi_dirac()
+  {
+  }
+
+
+int
+main()
+{
+  std::cout << "\nfloat\n=====\n\n";
+  run_fermi_dirac<float>();
+
+  std::cout << "\ndouble\n======\n";
+  run_fermi_dirac<double>();
+
+  std::cout << "\nlong double\n===========\n";
+  run_fermi_dirac<long double>();
+
+  std::cout << "\n__float128\n==========\n";
+  //run_fermi_dirac<__float128>();
+}
