94866785941eaff4d75e98fe0d696d544f073d4f
emsr <3dw4rd@verizon.net>
2016-06-15 14:18:49 -0400

    Prepare the scripts to point to the new branch.

--- fetch_complex.sh
+++ fetch_complex.sh
@@ -1,7 +1,10 @@
 #!  /bin/bash
 
-cp ../gcc_specfun/libstdc++-v3/testsuite/ext/special_functions/ellint_rc/complex.cc complex_ellint_rc.cc 
-cp ../gcc_specfun/libstdc++-v3/testsuite/ext/special_functions/ellint_rd/complex.cc complex_ellint_rd.cc 
-cp ../gcc_specfun/libstdc++-v3/testsuite/ext/special_functions/ellint_rf/complex.cc complex_ellint_rf.cc 
-cp ../gcc_specfun/libstdc++-v3/testsuite/ext/special_functions/ellint_rg/complex.cc complex_ellint_rg.cc 
-cp ../gcc_specfun/libstdc++-v3/testsuite/ext/special_functions/ellint_rj/complex.cc complex_ellint_rj.cc 
+tool="cp -f"
+suffix="_specfun"
+
+${tool} ../gcc${suffix}/libstdc++-v3/testsuite/ext/special_functions/ellint_rc/complex.cc complex_ellint_rc.cc 
+${tool} ../gcc${suffix}/libstdc++-v3/testsuite/ext/special_functions/ellint_rd/complex.cc complex_ellint_rd.cc 
+${tool} ../gcc${suffix}/libstdc++-v3/testsuite/ext/special_functions/ellint_rf/complex.cc complex_ellint_rf.cc 
+${tool} ../gcc${suffix}/libstdc++-v3/testsuite/ext/special_functions/ellint_rg/complex.cc complex_ellint_rg.cc 
+${tool} ../gcc${suffix}/libstdc++-v3/testsuite/ext/special_functions/ellint_rj/complex.cc complex_ellint_rj.cc 
