a90d784236201de21fb91e4637e947857ddcd2e3
Ed Smith-Rowland <3dw4rd@verizon.net>
2016-06-16 12:09:08 -0400

    Clean up the main directory. We don't need copies of all the impl files in bits and in the top. They are far more usable in the directories with a -I. flag. The scripts now just pull to the bits directories. I'm adding a tr1 directory with the old specfun contents is case we need to hack them.

---
+++ pull_tr1.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+tool="cp -f"
+suffix="_tr29124"
+
+src_dir="$HOME/gcc${suffix}/libstdc++-v3/include"
+if [ -d "$1" ]; then 
+  src_dir="$1"
+fi
+src_tr1_dir="${src_dir}/tr1"
+
+dst_dir="."
+if [ -d "$2" ]; then 
+  dst_dir="$2"
+fi
+dst_tr1_dir="${dst_dir}/tr1"
+
+${tool} "${src_tr1_dir}/cmath"                    "${dst_tr1_dir}"
+${tool} "${src_tr1_dir}/bessel_function.tcc"      "${dst_tr1_dir}"
+${tool} "${src_tr1_dir}/beta_function.tcc"        "${dst_tr1_dir}"
+${tool} "${src_tr1_dir}/ell_integral.tcc"         "${dst_tr1_dir}"
+${tool} "${src_tr1_dir}/exp_integral.tcc"         "${dst_tr1_dir}"
+${tool} "${src_tr1_dir}/gamma.tcc"                "${dst_tr1_dir}"
+${tool} "${src_tr1_dir}/hypergeometric.tcc"       "${dst_tr1_dir}"
+${tool} "${src_tr1_dir}/legendre_function.tcc"    "${dst_tr1_dir}"
+${tool} "${src_tr1_dir}/modified_bessel_func.tcc" "${dst_tr1_dir}"
+${tool} "${src_tr1_dir}/poly_hermite.tcc"         "${dst_tr1_dir}"
+${tool} "${src_tr1_dir}/poly_laguerre.tcc"        "${dst_tr1_dir}"
+${tool} "${src_tr1_dir}/riemann_zeta.tcc"         "${dst_tr1_dir}"
+${tool} "${src_tr1_dir}/special_function_util.h"  "${dst_tr1_dir}"
