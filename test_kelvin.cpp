eade05b22c4eea66c163645255b86872fcd923d6
emsr <3dw4rd@verizon.net>
2016-06-20 14:00:13 -0400

    Add a new way of summing the Kelvin functions.

--- test_kelvin.cpp
+++ test_kelvin.cpp
@@ -36,6 +36,37 @@ g++ -std=gnu++14 -g -I. -o test_kelvin test_kelvin.cpp
     }
 
   template<typename _Tp>
+    std::pair<_Tp, _Tp>
+    kelvin_series(_Tp __x)
+    {
+      constexpr auto _S_1d2 = _Tp{1} / _Tp{2};
+      constexpr auto _S_eps = std::numeric_limits<_Tp>::epsilon();
+      constexpr auto _S_maxiter = 1000;
+      const auto __xd2 = __x / _Tp{2};
+      const auto __xxd4 = __xd2 * __xd2;
+      const auto __y = __xxd4 * __xxd4;
+      auto __factr = _Tp{1};
+      auto __termr = _Tp{1};
+      auto __facti = _Tp{1};
+      auto __termi = _Tp{1};
+      //__gnu_cxx::_WenigerDeltaSum<__gnu_cxx::_VanWijngaardenSum<_Tp>> __bex;
+      __gnu_cxx::_BasicSum<_Tp> __ber, __bei;
+      __ber += __termr;
+      __bei += __termi;
+      for (auto __k = 1; __k < _S_maxiter; ++__k)
+	{
+	  __factr *= _Tp{2} * __k * (_Tp{2} * __k - 1);
+	  __termr *= -__y / __factr / __factr;
+	  __ber += __termr;
+	  __facti = __factr * (_Tp{2} * __k + 1);
+	  __termi *= -__y / __facti / __facti;
+	  if (std::abs(__termr) < _S_eps * std::abs(__ber()))
+	    break;
+	}
+      return std::make_pair(__ber(), __xxd4 * __bei());
+    }
+
+  template<typename _Tp>
     _Tp
     kelvin_ber_series(_Tp __x)
     { return kelvin_bex_series(__x, -1); }
@@ -83,8 +114,45 @@ template<typename _Tp>
     std::cout << std::endl;
   }
 
+
+template<typename _Tp>
+  void
+  run_kelvin2()
+  {
+    std::cout.precision(std::numeric_limits<_Tp>::digits10);
+    std::cout << std::showpoint << std::scientific;
+    auto width = 8 + std::cout.precision();
+
+    std::cout << "\n\nPrint Kelvin functions computed by series expansions\n";
+    std::cout << "++++++++++++++++++++++++++++++++++++++++++++++++++++\n";
+
+    std::cout << "\n\n";
+    std::cout << std::setw(width) << "x"
+	      << std::setw(width) << "ber"
+	      << std::setw(width) << "bei"
+	      << std::setw(width) << "ker"
+	      << std::setw(width) << "kei"
+	      << '\n';
+    std::cout << std::setw(width) << "========="
+	      << std::setw(width) << "========="
+	      << std::setw(width) << "========="
+	      << std::setw(width) << "========="
+	      << std::setw(width) << "========="
+	      << '\n';
+    for (int i = -100; i <= 200; ++i)
+      {
+	auto x = _Tp(0.1) * i;
+	auto be = kelvin_series(x);
+	std::cout << std::setw(width) << x
+		  << std::setw(width) << be.first
+		  << std::setw(width) << be.second
+		  << '\n';
+      }
+    std::cout << std::endl;
+  }
+
 int
 main()
 {
-  run_kelvin<double>();
+  run_kelvin2<double>();
 }
