32ae538e694a574db903a0443b3b63c757e1f89c
Ed Smith-Rowland <3dw4rd@verizon.net>
2016-06-21 11:02:53 -0400

    Add a plain raw sum to Kelvin - spot on with BasicSum.

--- test_kelvin.cpp
+++ test_kelvin.cpp
@@ -11,6 +11,10 @@ g++ -std=gnu++14 -g -I. -o test_kelvin test_kelvin.cpp
 
 bool WRITE_TERM = false;
 
+
+  /*
+   *
+   */
   template<typename _Tp>
     _Tp
     kelvin_bex_series(_Tp __x, int __sign)
@@ -41,6 +45,28 @@ if (WRITE_TERM)
       return __bex();
     }
 
+
+  /*
+   *
+   */
+  template<typename _Tp>
+    _Tp
+    kelvin_ber_series(_Tp __x)
+    { return kelvin_bex_series(__x, -1); }
+
+
+  /*
+   *
+   */
+  template<typename _Tp>
+    _Tp
+    kelvin_bei_series(_Tp __x)
+    { return __x * __x * kelvin_bex_series(__x, +1) / _Tp{4}; }
+
+
+  /*
+   *
+   */
   template<typename _Tp>
     std::pair<_Tp, _Tp>
     kelvin_series(_Tp __x)
@@ -58,9 +84,10 @@ if (WRITE_TERM)
       auto __termi = _Tp{1};
 if (WRITE_TERM)
   std::cout << __termr << '\t' << __termi << '\n';
-      __gnu_cxx::_WenigerDeltaSum<__gnu_cxx::_VanWijngaardenSum<_Tp>>
-        __ber, __bei, __ker, __kei;
-      //__gnu_cxx::_BasicSum<_Tp> __ber, __bei, __ker, __kei;
+      //__gnu_cxx::_WenigerDeltaSum<__gnu_cxx::_VanWijngaardenSum<_Tp>>
+      //  __ber, __bei, __ker, __kei;
+      //__gnu_cxx::_WenigerDeltaSum<__gnu_cxx::_BasicSum<_Tp>> _H_n;
+      __gnu_cxx::_BasicSum<_Tp> __ber, __bei, __ker, __kei;
       __ber += __termr;
       __bei += __termi;
       for (auto __k = 1; __k < _S_maxiter; ++__k)
@@ -68,9 +95,11 @@ if (WRITE_TERM)
 	  __factr *= _Tp{2} * __k * (_Tp{2} * __k - 1);
 	  __termr *= -__y / __factr / __factr;
 	  __ber += __termr;
+
 	  __facti = __factr * (_Tp{2} * __k + 1) / (_Tp{2} * __k - 1);
 	  __termi *= -__y / __facti / __facti;
 	  __bei += __termi;
+
 if (WRITE_TERM)
   std::cout << __termr << '\t' << __termi << '\n';
 	  if (std::abs(__termr) < _S_eps * std::abs(__ber()))
@@ -79,17 +108,53 @@ if (WRITE_TERM)
       return std::make_pair(__ber(), __xxd4 * __bei());
     }
 
+
+  /*
+   *
+   */
   template<typename _Tp>
-    _Tp
-    kelvin_ber_series(_Tp __x)
-    { return kelvin_bex_series(__x, -1); }
+    std::pair<_Tp, _Tp>
+    kelvin_series2(_Tp __x)
+    {
+      constexpr auto _S_gamma_e = __gnu_cxx::__math_constants<_Tp>::__gamma_e;
+      constexpr auto _S_1d2 = _Tp{1} / _Tp{2};
+      constexpr auto _S_eps = std::numeric_limits<_Tp>::epsilon();
+      constexpr auto _S_maxiter = 1000;
+      const auto __xd2 = __x / _Tp{2};
+      const auto __xxd4 = __xd2 * __xd2;
+      const auto __y = __xxd4 * __xxd4;
+      auto __factr = _Tp{1};
+      auto __termr = _Tp{1};
+      auto __ber = __termr;
+      auto __facti = _Tp{1};
+      auto __termi = _Tp{1};
+      auto __bei = __termi;
+if (WRITE_TERM)
+  std::cout << __termr << '\t' << __termi << '\n';
+      __ber += __termr;
+      __bei += __termi;
+      for (auto __k = 1; __k < _S_maxiter; ++__k)
+	{
+	  __factr *= _Tp{2} * __k * (_Tp{2} * __k - 1);
+	  __termr *= -__y / __factr / __factr;
+	  __ber += __termr;
 
-  template<typename _Tp>
-    _Tp
-    kelvin_bei_series(_Tp __x)
-    { return __x * __x * kelvin_bex_series(__x, +1) / _Tp{4}; }
+	  __facti = __factr * (_Tp{2} * __k + 1) / (_Tp{2} * __k - 1);
+	  __termi *= -__y / __facti / __facti;
+	  __bei += __termi;
+
+if (WRITE_TERM)
+  std::cout << __termr << '\t' << __termi << '\n';
+	  if (std::abs(__termr) < _S_eps * std::abs(__ber))
+	    break;
+	}
+      return std::make_pair(__ber, __xxd4 * __bei);
+    }
 
 
+/*
+ *
+ */
 template<typename _Tp>
   void
   run_kelvin()
@@ -134,6 +199,9 @@ WRITE_TERM=false;
   }
 
 
+/*
+ *
+ */
 template<typename _Tp>
   void
   run_kelvin2()
@@ -142,10 +210,10 @@ template<typename _Tp>
     std::cout << std::showpoint << std::scientific;
     auto width = 8 + std::cout.precision();
 
-    auto ber0 = kelvin_series(_Tp{});
+    auto ber0 = kelvin_series2(_Tp{});
 
 WRITE_TERM=true;
-    auto ber1 = kelvin_series(_Tp{1});
+    auto ber1 = kelvin_series2(_Tp{1});
 WRITE_TERM=false;
 
     std::cout << "\n\nPrint Kelvin functions computed by series expansions\n";
@@ -167,7 +235,7 @@ WRITE_TERM=false;
     for (int i = -100; i <= 200; ++i)
       {
 	auto x = _Tp(0.1) * i;
-	auto be = kelvin_series(x);
+	auto be = kelvin_series2(x);
 	std::cout << std::setw(width) << x
 		  << std::setw(width) << be.first
 		  << std::setw(width) << be.second
@@ -175,6 +243,7 @@ WRITE_TERM=false;
       }
     std::cout << std::endl;
   }
+
 
 int
 main()
