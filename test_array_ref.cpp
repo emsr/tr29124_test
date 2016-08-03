e3f5ebc9e860220bac06503a54d581285d51bcbd
emsr <3dw4rd@verizon.net>
2016-08-03 14:05:46 -0400

    Add stuff from P0331 to the test_array_ref.cpp

--- test_array_ref.cpp
+++ test_array_ref.cpp
@@ -2,6 +2,25 @@
 
 #include <array_ref>
 
+void foo(int A[] , size_t N); // Traditional API
+void foo(const int A[], size_t N); // Traditional API
+
+void foo(array_ref<int[]> A); // Reference API
+void foo(array_ref<const int[]> A); // Reference API
+
+void
+bar()
+{
+  enum { L = ... };
+  int buffer[L];
+  array_ref<int[]> A(buffer, L);
+
+  assert(L == A.size());
+  assert(& A[0] == buffer);
+
+  foo(array);
+}
+
 int
 main()
 {
