419eafd95cd6cc50515198295eb726e621e4d2fb
emsr <3dw4rd@verizon.net>
2016-08-22 22:49:29 -0400

    Clean up plot_airy_new.sh

--- plot_airy_new.sh
+++ plot_airy_new.sh
@@ -1,10 +1,13 @@
 
+/usr/local/bin/gnuplot
+set hidden3d
+
 splot [-5:5][-5:5][0:12] "plot/airy_complex_double_new.txt" index 0 with lines title "|Ai(z)|^{1/6}"
-splot [-20:5][-5:5][-2:2] "plot/airy_complex_double_new.txt" index 1 with lines title "Re[Ai(z)]"
-splot [-20:5][-5:5][-2:2] "plot/airy_complex_double_new.txt" index 2 with lines title "Im[Ai(z)]"
+splot [-20:5][-5:5][-20:20] "plot/airy_complex_double_new.txt" index 1 with lines title "Re[Ai(z)]"
+splot [-20:5][-5:5][-20:20] "plot/airy_complex_double_new.txt" index 2 with lines title "Im[Ai(z)]"
 splot [-20:5][-5:5][-3.2:+3.2] "plot/airy_complex_double_new.txt" index 3 with lines title "Arg[Ai(z)]"
 splot [-5:5][-5:5][0:12] "plot/airy_complex_double_new.txt" index 4 with lines title "|Bi(z)|^{1/6}"
 splot [-20:5][-5:5][-2:2] "plot/airy_complex_double_new.txt" index 5 with lines title "Re[Bi(z)]"
 splot [-20:5][-5:5][-2:2] "plot/airy_complex_double_new.txt" index 6 with lines title "Im[Bi(z)]"
 splot [-20:5][-5:5][-3.2:+3.2] "plot/airy_complex_double_new.txt" index 7 with lines title "Arg[Bi(z)]"
-//splot [-20:5][-5:5][-2:2] "plot/airy_complex_double.txt" index 8 with lines title "Wronski"
+#splot [-20:5][-5:5][-2:2] "plot/airy_complex_double_new.txt" index 8 with lines title "Wronski"
