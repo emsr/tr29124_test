7f51005628616f6805fcc0a96bfdf4f18aa4d5e1
emsr <3dw4rd@verizon.net>
2016-08-26 14:20:12 -0400

    Add roots and words and fixed bugs in bits/notsospecfun.h

---
+++ test_notsospecfun.cpp
@@ -0,0 +1,11 @@
+/*
+$HOME/bin/bin/g++ -std=gnu++17 -g -Wall -Wextra -o test_notsospecfun test_notsospecfun.cpp
+LD_LIBRARY_PATH=$HOME/bin/lib64:$LD_LIBRARY_PATH ./test_notsospecfun > test_notsospecfun.new
+*/
+
+#include "bits/notsospecfun.h"
+
+int
+main()
+{
+}
