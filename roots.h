17124557e1dec718167794e54cd48455ce8fb3ca
emsr <3dw4rd@verizon.net>
2016-09-07 17:44:13 -0400

    Add header and stuff to roots.*

--- roots.h
+++ roots.h
@@ -1,5 +1,35 @@
-#ifndef ROOTS_H
-#define ROOTS_H 1
+// Math extensions -*- C++ -*-
+
+// Copyright (C) 2016 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// Under Section 7 of GPL version 3, you are granted additional
+// permissions described in the GCC Runtime Library Exception, version
+// 3.1, as published by the Free Software Foundation.
+
+// You should have received a copy of the GNU General Public License and
+// a copy of the GCC Runtime Library Exception along with this program;
+// see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
+// <http://www.gnu.org/licenses/>.
+
+/** @file ext/roots.h
+ *  This file is a GNU extension to the Standard C++ Library.
+ */
+
+#ifndef _EXT_ROOTS_H
+#define _EXT_ROOTS_H 1
+
+#pragma GCC system_header
 
 #include <vector>
 //#include <utility> // For pair?
@@ -69,4 +99,4 @@ namespace __gnu_cxx
 
 #include "roots.tcc"
 
-#endif // ROOTS_H
+#endif // _EXT_ROOTS_H
