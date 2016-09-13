cc9988940e77badc1e65d28d085f60f4362e3141
emsr <3dw4rd@verizon.net>
2016-09-13 12:49:10 -0400

    Actually start to test Rational/

--- test_rational.cpp
+++ test_rational.cpp
@@ -1,13 +1,20 @@
-// $HOME/bin/bin/g++ -o test_rational test_rational.cpp
+// $HOME/bin/bin/g++ -g -I. -o test_rational test_rational.cpp
 
 // LD_LIBRARY_PATH=$HOME/bin/lib64:$LD_LIBRARY_PATH ./test_rational
 
 #include "rational.h"
-#include "polynomial.h"
+#include <ext/polynomial.h>
 
 int
 main()
 {
-  __gnu_cxx::_Polynomial<__gnu_cxx::_Rational<int>> rpoly{{1,2}, {3, 4}, {5, 6}, {7, 8}};
+  using Rat = __gnu_cxx::_Rational<int>;
+
+  std::cout << Rat(1, 2) + Rat(1, 3) << '\n';
+  std::cout << Rat(1, 2) - Rat(1, 3) << '\n';
+  std::cout << Rat(1, 2) * Rat(1, 3) << '\n';
+  std::cout << Rat(1, 2) / Rat(1, 3) << '\n';
+
+  __gnu_cxx::_Polynomial<Rat> rpoly{{1,2}, {3, 4}, {5, 6}, {7, 8}};
   std::cout << rpoly << '\n';
 }
