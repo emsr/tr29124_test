ec28ad40f32e488b856f9957a9db69e2ab05bd2e
emsr <3dw4rd@verizon.net>
2016-09-22 00:35:32 -0400

    Worked through heuman_lambda problems with test_heuman_lambda.cpp. Double checked jacobi_zeta. Arranged special cases for these in bits/sf_ellint.tcc Adjusted .gitignore

---
+++ test_jacobi_zeta.cpp
@@ -0,0 +1,42 @@
+/*
+$HOME/bin/bin/g++ -std=gnu++17 -I. -o test_jacobi_zeta test_jacobi_zeta.cpp wrap_boost.cpp
+./test_jacobi_zeta > test_jacobi_zeta.txt
+
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <bits/specfun.h>
+#include "wrap_boost.h"
+
+int
+main()
+{
+  double _S_pi_2 = 1.5707963267948966192313216916397514L;
+
+  std::cout << '\n';
+  for (auto k : {//-1.0, -0.99, -0.9, -0.8, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1,
+		 0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.99, 1.0})
+    {
+      std::cout << '\n';
+      for (auto phi : {-_S_pi_2, -0.99 * _S_pi_2, -0.9 * _S_pi_2, -0.8 * _S_pi_2, -0.6 * _S_pi_2, -0.4 * _S_pi_2, -0.2 * _S_pi_2,
+		       0.0, 0.2 * _S_pi_2, 0.4 * _S_pi_2, 0.6 * _S_pi_2, 0.8 * _S_pi_2, 0.9 * _S_pi_2, 0.99 * _S_pi_2, _S_pi_2})
+	{
+	  try
+	    {
+	      auto Lam_boost = beast::jacobi_zeta(k, phi);
+	      auto Lam_gnu = __gnu_cxx::jacobi_zeta(k, phi);
+	      std::cout << ' ' << std::setw(6) << k
+			<< ' ' << std::setw(12) << phi
+			<< ' ' << std::setw(12) << Lam_gnu
+			<< ' ' << std::setw(12) << Lam_boost
+			<< ' ' << std::setw(12) << std::abs(Lam_gnu - Lam_boost)
+			<< '\n';
+	    }
+	  catch(...)
+	    {
+	      std::cerr << "\nexception\n";
+	    }
+	}
+    }
+}
