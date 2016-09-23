968f60ad3feec9a5c85df670eb0835256d5f2ba5
emsr <3dw4rd@verizon.net>
2016-09-22 22:23:49 -0400

    Update gamma output from linux. Update .gitignore Pencil in __float128 plot commands in plot_gamma.sh

--- plot_gamma.sh
+++ plot_gamma.sh
@@ -17,6 +17,11 @@ splot [-20:5][-5:5][-8:20] "plot/gamma_lanczos_long_double.txt" index 1 with lin
 splot [-20:5][-5:5][0:40] "plot/gamma_lanczos_long_double.txt" index 2 with lines title "|logGamma(z)|"
 splot [-20:5][-5:5][-180:180] "plot/gamma_lanczos_long_double.txt" index 3 with lines title "Arg(logGamma(z))"
 
+#splot [-20:5][-5:5][-8:20] "plot/gamma_lanczos__float128.txt" index 0 with lines title "Re(logGamma(z))"
+#splot [-20:5][-5:5][-8:20] "plot/gamma_lanczos__float128.txt" index 1 with lines title "Im(logGamma(z))"
+#splot [-20:5][-5:5][0:40] "plot/gamma_lanczos__float128.txt" index 2 with lines title "|logGamma(z)|"
+#splot [-20:5][-5:5][-180:180] "plot/gamma_lanczos__float128.txt" index 3 with lines title "Arg(logGamma(z))"
+
 
 
 splot [-20:5][-5:5][-20:20] "plot/gamma_spouge_float.txt" index 0 with lines title "Re(logGamma(z))"
@@ -33,3 +38,8 @@ splot [-20:5][-5:5][-20:20] "plot/gamma_spouge_long_double.txt" index 0 with lin
 splot [-20:5][-5:5][-20:20] "plot/gamma_spouge_long_double.txt" index 1 with lines title "Im(logGamma(z))"
 splot [-20:5][-5:5][0:40] "plot/gamma_spouge_long_double.txt" index 2 with lines title "|logGamma(z)|"
 splot [-20:5][-5:5][-180:180] "plot/gamma_spouge_long_double.txt" index 3 with lines title "Arg(logGamma(z))"
+
+#splot [-20:5][-5:5][-20:20] "plot/gamma_spouge__float128.txt" index 0 with lines title "Re(logGamma(z))"
+#splot [-20:5][-5:5][-20:20] "plot/gamma_spouge__float128.txt" index 1 with lines title "Im(logGamma(z))"
+#splot [-20:5][-5:5][0:40] "plot/gamma_spouge__float128.txt" index 2 with lines title "|logGamma(z)|"
+#splot [-20:5][-5:5][-180:180] "plot/gamma_spouge__float128.txt" index 3 with lines title "Arg(logGamma(z))"
