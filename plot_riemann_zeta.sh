6b6c9da5c16cbbcde490c609ac71772ac54ee263
emsr <3dw4rd@verizon.net>
2016-09-26 15:16:04 -0400

    Plot all the precisions of riemann_zeta. Add headers to the test. Add two calls to examine the incorrect jump along the imag azis!

--- plot_riemann_zeta.sh
+++ plot_riemann_zeta.sh
@@ -1,4 +1,4 @@
-
+m
 /usr/local/bin/gnuplot
 set hidden3d
 
@@ -6,3 +6,13 @@ splot [-20:5][-5:5][-20:20] "plot/riemann_zeta_float.txt" index 0 with lines tit
 splot [-20:5][-5:5][-20:20] "plot/riemann_zeta_float.txt" index 1 with lines title "Im(zeta(s))"
 splot [-20:5][-5:5][0:80] "plot/riemann_zeta_float.txt" index 2 with lines title "|zeta(s)|"
 splot [-20:5][-5:5][-180:180] "plot/riemann_zeta_float.txt" index 3 with lines title "Arg(zeta(s))"
+
+splot [-20:5][-5:5][-20:20] "plot/riemann_zeta_double.txt" index 0 with lines title "Re(zeta(s))"
+splot [-20:5][-5:5][-20:20] "plot/riemann_zeta_double.txt" index 1 with lines title "Im(zeta(s))"
+splot [-20:5][-5:5][0:80] "plot/riemann_zeta_double.txt" index 2 with lines title "|zeta(s)|"
+splot [-20:5][-5:5][-180:180] "plot/riemann_zeta_double.txt" index 3 with lines title "Arg(zeta(s))"
+
+splot [-20:5][-5:5][-20:20] "plot/riemann_zeta_long_double.txt" index 0 with lines title "Re(zeta(s))"
+splot [-20:5][-5:5][-20:20] "plot/riemann_zeta_long_double.txt" index 1 with lines title "Im(zeta(s))"
+splot [-20:5][-5:5][0:80] "plot/riemann_zeta_long_double.txt" index 2 with lines title "|zeta(s)|"
+splot [-20:5][-5:5][-180:180] "plot/riemann_zeta_long_double.txt" index 3 with lines title "Arg(zeta(s))"
