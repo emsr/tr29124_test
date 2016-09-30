831b2bb3ea0941f49fc4fe996dd89887c7d90b90
emsr <3dw4rd@verizon.net>
2016-09-30 14:04:18 -0400

    Get the parab cylinder function toy to at least compile.

---
+++ test_parab_cyl.cpp
@@ -0,0 +1,17 @@
+/*
+$HOME/bin/bin/g++ -std=gnu++17 -I. -o test_parab_cyl test_parab_cyl.cpp
+
+./test_parab_cyl > test_parab_cyl.txt
+
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <bits/numeric_limits.h>
+#include "sf_parab_cyl.tcc"
+
+int
+main()
+{
+}
