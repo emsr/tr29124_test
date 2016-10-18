0d5efb8a6c70a500f05ea0ebacf174e886d08f84
emsr <3dw4rd@verizon.net>
2016-10-18 16:57:10 -0400

    Don't use quoted includes for ext, bits

--- test_notsospecfun.cpp
+++ test_notsospecfun.cpp
@@ -1,9 +1,9 @@
 /*
-$HOME/bin/bin/g++ -std=gnu++17 -g -Wall -Wextra -o test_notsospecfun test_notsospecfun.cpp
+$HOME/bin/bin/g++ -std=gnu++17 -g -Wall -Wextra -I -o test_notsospecfun test_notsospecfun.cpp
 LD_LIBRARY_PATH=$HOME/bin/lib64:$LD_LIBRARY_PATH ./test_notsospecfun > test_notsospecfun.new
 */
 
-#include "bits/notsospecfun.h"
+#include <bits/notsospecfun.h>
 
 int
 main()
