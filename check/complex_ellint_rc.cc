ff745c586ea03990c24a4cf6f7f7843257dc769b
emsr <3dw4rd@verizon.net>
2016-10-19 18:24:24 -0400

    Add the complex checks back!

---
+++ check/complex_ellint_rc.cc
@@ -0,0 +1,56 @@
+// { dg-require-c-std "" }
+// { dg-add-options ieee }
+// { dg-options "-D__STDCPP_WANT_MATH_SPEC_FUNCS__" }
+
+// Copyright (C) 2016 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// ellint_rc
+
+#include <cmath>
+#include <complex>
+#include <testsuite_hooks.h>
+
+void
+test01()
+{
+  using cmplx = std::complex<double>;
+  using __gnu_cxx::ellint_rc;
+
+  auto rc1 = ellint_rc(0.0, 0.25);
+  auto rc2 = ellint_rc(2.25, 2.0);  //  ln 2 = 0.69314718055995
+  auto rc3 = ellint_rc(cmplx(0.0, 0.0), cmplx(0.0, 1.0));
+  //auto rc4 = ellint_rc(cmplx(0.0, -1.0), cmplx(0.0, 1.0));
+  auto rc5 = ellint_rc(0.25, -2.0);
+  auto rc6 = ellint_rc(cmplx(0.0, 1.0), cmplx(-1.0, 0.0));
+
+  bool test [[gnu::unused]] = true;
+  double eps = 1.0e-12;
+  VERIFY(std::abs(rc1 - 3.1415926535898) < eps);
+  VERIFY(std::abs(rc2 - 0.69314718055995) < eps);
+  VERIFY(std::abs(rc3 - cmplx(1.1107207345396, -1.1107207345396)) < eps);
+  //VERIFY(std::abs(rc4 - cmplx(1.2260849569072, -0.34471136988768)) < eps);
+  VERIFY(std::abs(rc5 - 0.23104906018665) < eps);
+  VERIFY(std::abs(rc6 - cmplx(0.77778596920447, 0.19832484993429)) < eps);
+}
+
+int
+main()
+{
+  test01();
+  return 0;
+}
---
+++ check/complex_ellint_rj.cc
@@ -0,0 +1,66 @@
+// { dg-require-c-std "" }
+// { dg-add-options ieee }
+// { dg-options "-D__STDCPP_WANT_MATH_SPEC_FUNCS__" }
+
+// Copyright (C) 2016 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// ellint_rj
+
+#include <cmath>
+#include <complex>
+#include <testsuite_hooks.h>
+
+void
+test01()
+{
+  using cmplx = std::complex<double>;
+  using __gnu_cxx::ellint_rj;
+
+  auto rj1 = ellint_rj(0.0, 1.0, 2.0, 3.0);
+  auto rj2 = ellint_rj(2.0, 3.0, 4.0, 5.0);
+  auto rj3 = ellint_rj(cmplx(2.0,0.0), cmplx(3.0,0.0),
+		       cmplx(4.0,0.0), cmplx(-1.0,1.0));
+  auto rj4 = ellint_rj(cmplx(0.0,1.0), cmplx(0.0,-1.0),
+		       cmplx(0.0,0.0), cmplx(2.0,0.0));
+  auto rj5 = ellint_rj(cmplx(-1.0,1.0), cmplx(-1.0,-1.0),
+				  cmplx(1.0,0.0), cmplx(2.0,0.0));
+  auto rj6 = ellint_rj(cmplx(0.0,1.0), cmplx(0.0,-1.0),
+		       cmplx(0.0,0.0), cmplx(1.0,-1.0));
+  auto rj7 = ellint_rj(cmplx(-1.0,1.0), cmplx(-1.0,-1.0),
+		       cmplx(1.0,0.0), cmplx(-3.0,1.0));
+  auto rj8 = ellint_rj(cmplx(-1.0,1.0), cmplx(-2.0,-1.0),
+		       cmplx(0.0,-1.0), cmplx(-1.0,1.0));
+
+  bool test [[gnu::unused]] = true;
+  double eps = 1.0e-12;
+  VERIFY(std::abs(rj1 - 0.77688623778582) < eps);
+  VERIFY(std::abs(rj2 - 0.14297579667157) < eps);
+  VERIFY(std::abs(rj3 - cmplx(0.13613945827771, -0.38207561624427)) < eps);
+  VERIFY(std::abs(rj4 - 1.6490011662711) < eps);
+  VERIFY(std::abs(rj5 - 0.94148358841220) < eps);
+  VERIFY(std::abs(rj6 - cmplx(1.8260115229009, +1.2290661908643)) < eps);
+  VERIFY(std::abs(rj7 - cmplx(-0.61127970812028, -1.0684038390007)) < eps);
+  VERIFY(std::abs(rj8 - cmplx(1.8249027393704, -1.2218475784827)) < eps);
+}
+
+int
+main()
+{
+  test01();
+  return 0;
+}
