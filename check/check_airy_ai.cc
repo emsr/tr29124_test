625679350df6c98dd0bfd0f54f6ef478eb0ff115
emsr <3dw4rd@verizon.net>
2016-10-19 14:46:20 -0400

    Try to get a better gamma_recip for bessel. For some reason diff is not showing good results but test is?!? Refactorizations and such.

--- check/check_airy_ai.cc
+++ check/check_airy_ai.cc
@@ -41,9 +41,9 @@
 // Test data.
 // max(|f - f_GSL|): 1.5126788710517758e-15
 // max(|f - f_GSL| / |f_GSL|): 5.7527019513802774e-14
-// mean(f - f_GSL): 1.8135459685330988e-17
-// variance(f - f_GSL): 8.4279317371800330e-36
-// stddev(f - f_GSL): 2.9030900325653066e-18
+// mean(f - f_GSL): -4.8884100947529072e-17
+// variance(f - f_GSL): 6.1234917779371205e-35
+// stddev(f - f_GSL): 7.8252742941938585e-18
 const testcase_airy_ai<double>
 data001[41] =
 {
--- test_gamma_reciprocal.cpp
+++ test_gamma_reciprocal.cpp
@@ -1,6 +1,6 @@
 /*
-  $HOME/bin/bin/g++ -std=gnu++17 -I. -o test_gamma_reciprocal test_gamma_reciprocal.cpp -lquadmath
-  ./test_gamma_reciprocal > test_gamma_reciprocal.txt
+$HOME/bin/bin/g++ -std=gnu++17 -I. -o test_gamma_reciprocal test_gamma_reciprocal.cpp -lquadmath -lmpfr
+./test_gamma_reciprocal > test_gamma_reciprocal.txt
 */
 
 /**
@@ -9,8 +9,6 @@
  */
 
 #include <bits/specfun.h>
-#include <bits/float128.h>
-#include <ext/math_const.h>
 #include <limits>
 #include <iostream>
 #include <fstream>
@@ -18,6 +16,13 @@
 #include <vector>
 #include <string>
 #include <complex>
+
+#include <bits/float128.h>
+#include <bits/complex128.h>
+
+#include <mpreal.h>
+#include <ext/math_const.h>
+#include <ext/math_const_mpreal.h>
 
 // I'm not sure why I need this here and not other places...
 template<>
@@ -40,20 +45,16 @@ template<>
   constexpr std::array<long double, 11>
   std::__detail::_GammaLanczos<long double>::_S_cheby;
 
-namespace std
-{
-namespace __detail
-{
 
   /**
    * 
    */
   template<typename _Tp>
-    std::vector<__num_traits_t<_Tp>>
-    __gamma_reciprocal_coef(std::size_t __n, _Tp __proto = _Tp{})
+    std::vector<std::__detail::__num_traits_t<_Tp>>
+    __gamma_reciprocal_series_coef(std::size_t __n, _Tp __proto = _Tp{})
     {
       using _Val = _Tp;
-      using _Real = __num_traits_t<_Val>;
+      using _Real = std::__detail::__num_traits_t<_Val>;
       const auto _S_gamma_e = __gnu_cxx::__const_gamma_e(std::real(__proto));
       auto __sign = [](std::size_t __i){ return __i & 1u == 1u ? -1 : +1; };
       std::vector<_Real> __c;
@@ -64,71 +65,117 @@ namespace __detail
 	  auto __sum = _Real{0};
 	  for (auto __k = 1u; __k < __j; ++__k)
 	    __sum += __sign(__k) * __c[__k]
-		   * (_Real{1} + __riemann_zeta_m_1(_Real(__j + 1 - __k)));
+		   * (_Real{1} + std::__detail::__riemann_zeta_m_1(_Real(__j + 1 - __k)));
 	  __c.push_back((_S_gamma_e * __c[__j] + __sign(__j) * __sum) / __j);
 	}
       return __c;
     }
 
   /**
-   * 
+   * Return the reciprocal of the Gamma function by series.
+   * The reciprocal of the Gamma function is given by
+   * @f[
+   *   \frac{1}{\Gamma(a)} = \sum_{k=1}^{\infty} c_k a^k
+   * @f]
+   * where the coefficients are defined by recursion:
+   * @f[
+   *   c_{k+1} = \frac{1}{k}\left[\gamma_E c_k
+   *           + (-1)^k\sum_{j=1}^{k-1}(-1)^j\zeta(j+1-k)c_j\right]
+   * @f]
+   * where @f$ c_1 = 1 @f$
    */
   template<typename _Tp>
     _Tp
-    __gamma_reciprocal(_Tp __a)
+    __gamma_reciprocal_series(_Tp __a)
     {
-      std::array<long double, 26>
+      static constexpr std::array<long double, 31>
       _S_c
       {{
-	 0.000000000000000000e+00L,
-	 1.000000000000000000e+00L,
-	 5.772156649015328606e-01L,
-	-6.558780715202538810e-01L,
-	-4.200263503409523551e-02L,
-	 1.665386113822914895e-01L,
-	-4.219773455554433676e-02L,
-	-9.621971527876973561e-03L,
-	 7.218943246663099543e-03L,
-	-1.165167591859065112e-03L,
-	-2.152416741149509656e-04L,
-	 1.280502823881161902e-04L,
-	-2.013485478078824803e-05L,
-	-1.250493482142674951e-06L,
-	 1.133027231981700022e-06L,
-	-2.056338416977613597e-07L,
-	 6.116095104478334562e-09L,
-	 5.002007644476128389e-09L,
-	-1.181274570484747060e-09L,
-	 1.043426711616684967e-10L,
-	 7.782263434328190273e-12L,
-	-3.696805613340574099e-12L,
-	 5.100370285646940727e-13L,
-	-2.058326339174801492e-14L,
-	-5.348122461614610358e-15L,
-	 1.226777080156782648e-15L,
+	 0.0000000000000000000000000000000000000000L,
+	 1.0000000000000000000000000000000000000000L,
+	 0.5772156649015328606065120900824024310422L,
+	-0.6558780715202538810770195151453904812798L,
+	-0.0420026350340952355290039348754298187114L,
+	 0.1665386113822914895017007951021052357178L,
+	-0.0421977345555443367482083012891873913017L,
+	-0.0096219715278769735621149216723481989754L,
+	 0.0072189432466630995423950103404465727099L,
+	-0.0011651675918590651121139710840183886668L,
+	-0.0002152416741149509728157299630536478065L,
+	 0.0001280502823881161861531986263281643234L,
+	-0.0000201348547807882386556893914210218184L,
+	-0.0000012504934821426706573453594738330922L,
+	 0.0000011330272319816958823741296203307449L,
+	-0.0000002056338416977607103450154130020573L,
+	 0.0000000061160951044814158178624986828553L,
+	 0.0000000050020076444692229300556650480600L,
+	-0.0000000011812745704870201445881265654365L,
+	 0.0000000001043426711691100510491540332312L,
+	 0.0000000000077822634399050712540499373114L,
+	-0.0000000000036968056186422057081878158781L,
+	 0.0000000000005100370287454475979015481323L,
+	-0.0000000000000205832605356650678322242954L,
+	-0.0000000000000053481225394230179823700173L,
+	 0.0000000000000012267786282382607901588938L,
+	-0.0000000000000001181259301697458769513765L,
+	 0.0000000000000000011866922547516003325798L,
+	 0.0000000000000000014123806553180317815558L,
+	-0.0000000000000000002298745684435370206592L,
+	 0.0000000000000000000171440632192733743338L,
       }};
+      const auto _S_eps = __gnu_cxx::__epsilon(std::real(__a));
       auto __ak = _Tp{1};
       auto __gam = _Tp{0};
       for (auto __k = 1u; __k < _S_c.size(); ++__k)
-	__gam += _S_c[__k] * (__ak *= __a);
+	{
+	  __ak *= __a;
+	  auto __term = _S_c[__k] * __ak;
+	  __gam += __term;
+	  if (std::abs(__term) < _S_eps)
+	    break;
+	}
       return __gam;
     }
 
-} // namespace __detail
-} // namespace std
+  /**
+   * Return the reciprocal of the Gamma function by infinite product.
+   * The reciprocal of the Gamma function is given by
+   * @f[
+   *   \frac{1}{\Gamma(a)} = ae^{\gamma_E a}\Pi_{k=1}^{\infty}
+   *                     (\left 1+\frac{a}{k}\right)e^{-a/k}
+   * @f]
+   */
+  template<typename _Tp>
+    _Tp
+    __gamma_reciprocal_prod(_Tp __a)
+    {
+      const auto _S_eps = __gnu_cxx::__epsilon(std::real(__a));
+      const auto _S_gamma_e = __gnu_cxx::__const_gamma_e(std::real(__a));
+      const auto _S_max_iter = 1000;
+      auto __gam = __a * std::exp(_S_gamma_e * __a);
+      for (auto __k = 1u; __k < _S_max_iter; ++__k)
+	{
+	  auto __rat = __a / __k;
+	  __gam *= (_Tp{1} + __rat) * std::exp(-__a / __k);
+	  if (std::abs(__rat) < _S_eps)
+	    break;
+	}
+      return __gam;
+    }
 
 template<typename _Tp>
+  void
   test_gamma_reciprocal(_Tp __proto)
   {
     using _Val = _Tp;
     using _Real = std::__detail::__num_traits_t<_Val>;
 
-    std::cout.precision(std::numeric_limits<_Real>::digits10);
+    std::cout.precision(__gnu_cxx::__digits10(__proto));
     std::cout << std::showpoint << std::scientific;
     auto width = 8 + std::cout.precision();
 
     std::size_t __n = 50;
-    auto __c = std::__detail::__gamma_reciprocal_coef<_Real>(__n, __proto);
+    auto __c = __gamma_reciprocal_series_coef<_Real>(__n, __proto);
 
     std::cout << '\n'
 	      << ' ' << std::setw(4) << "k"
@@ -141,19 +188,24 @@ template<typename _Tp>
 
     std::cout << '\n'
 	      << ' ' << std::setw(4) << "a"
-	      << ' ' << std::setw(width) << "1/Gamma(a)"
+	      << ' ' << std::setw(width) << "1/Gamma(a) ser"
+	      << ' ' << std::setw(width) << "1/Gamma(a) prod"
 	      << ' ' << std::setw(width) << "1/std::tgamma(a)"
-	      << ' ' << std::setw(width) << "delta"
+	      << ' ' << std::setw(width) << "delta series"
+	      << ' ' << std::setw(width) << "delta product"
     	      << '\n';
     for (auto __k = 0; __k < 100; ++__k)
       {
-	auto __a = __k * 0.01;
-	auto __gammarg = std::__detail::__gamma_reciprocal(__a);
+	auto __a = __k * _Tp{0.01L};
+	auto __gammargs = __gamma_reciprocal_series(__a);
+	auto __gammargp = __gamma_reciprocal_prod(__a);
 	auto __gammars = 1 / std::tgamma(__a);
 	std::cout << ' ' << std::setw(4) << __a
-	        << ' ' << std::setw(width) << __gammarg
+	        << ' ' << std::setw(width) << __gammargs
+	        << ' ' << std::setw(width) << __gammargp
 	        << ' ' << std::setw(width) << __gammars
-		<< ' ' << std::setw(width) << (__gammarg - __gammars) / __gammars
+		<< ' ' << std::setw(width) << (__gammargs - __gammars) / __gammars
+		<< ' ' << std::setw(width) << (__gammargp - __gammars) / __gammars
     	        << '\n';
       }
   }
@@ -168,4 +220,6 @@ main()
   test_gamma_reciprocal(1.0l);
 
   //test_gamma_reciprocal(1.0q);
+
+  //test_gamma_reciprocal(mpfr::mpreal(1,128));
 }
