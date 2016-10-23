60948b07a76a34f6ec05e03b632db4dd6f17cd18
emsr <3dw4rd@verizon.net>
2016-10-23 13:05:11 -0400

    Use states for the internal Bessel type functions! Add a state for Fock-airy finctions.

--- test_bessel.cpp
+++ test_bessel.cpp
@@ -1,6 +1,6 @@
 /*
-$HOME/bin_tr29124/bin/g++ -std=gnu++1z -Wall -Wextra -o test_bessel test_bessel.cpp -lquadmath
-LD_LIBRARY_PATH=$HOME/bin_tr29124/lib64:$LD_LIBRARY_PATH ./test_bessel > test_bessel.txt
+$HOME/bin_tr29124/bin/g++ -std=gnu++17 -g -Wall -Wextra -I. -o test_bessel test_bessel.cpp -lquadmath
+./test_bessel > test_bessel.txt
 */
 
 #include <iostream>
@@ -10,92 +10,162 @@ LD_LIBRARY_PATH=$HOME/bin_tr29124/lib64:$LD_LIBRARY_PATH ./test_bessel > test_be
 #include <limits>
 #include <vector>
 
-#include <cmath>
-//#include "new_bessel.tcc.bad"
-//#include "new_bessel.tcc"
-
-template <typename Type>
-void
-test_bessel( void )
-{
-  std::cout.precision(std::numeric_limits<Type>::digits10);
-  std::cout.flags(std::ios::showpoint);
-  auto width = 8 + std::cout.precision();
-  constexpr auto _S_pi_2 = __gnu_cxx::__math_constants<Type>::__pi_half;
-
-  std::cyl_neumann(1.0, 0.01);
-
-  std::vector<Type> nu
+template<typename _Tp>
+  void
+  test_cyl_bessel()
   {
-    Type{0},
-    Type{1} / Type{3},
-    Type{1} / Type{2},
-    Type{2} / Type{3},
-    Type{1},
-    Type{2},
-    Type{5},
-    Type{10},
-    Type{20},
-    Type{50},
-    Type{100},
-  };
+    using _Val = _Tp;
+    using _Real = std::__detail::__num_traits_t<_Val>;
+    const auto _S_pi_2 = __gnu_cxx::__const_pi_half<_Real>();
 
-  for (std::size_t n = 0; n < nu.size(); ++n)
+    std::cout.precision(__gnu_cxx::__digits10<_Real>());
+    std::cout << std::showpoint << std::scientific;
+    auto width = 8 + std::cout.precision();
+
+    std::cyl_neumann(1.0, 0.01);
+
+    std::vector<_Tp> nuvec
     {
-      std::cout << "\n  nu = " << nu[n] << std::endl;
-      std::cout << "  " << std::setw(width) << "x";
-      std::cout << "  " << std::setw(width) << "J_nu(x)";
-      std::cout << "  " << std::setw(width) << "N_nu(x)";
-      std::cout << "  " << std::setw(width) << "pi x W{J,N} / 2";
-      std::cout << "  " << std::setw(width) << "I_nu(x)";
-      std::cout << "  " << std::setw(width) << "K_nu(x)";
-      std::cout << "  " << std::setw(width) << "-x W{I,K}";
-      std::cout << std::endl;
-      for (unsigned int i = 0; i <= 1000; ++i)
-        {
-          auto x = Type(i) / Type{100};
-          Type J_nu, N_nu, Jp_nu, Np_nu;
-          try
-            {
-              std::__detail::__cyl_bessel_jn(nu[n], x, J_nu, N_nu, Jp_nu, Np_nu);
-            }
-          catch (std::exception & err)
-            {
-              std::cerr << err.what() << std::endl;
-            }
-          Type I_nu, K_nu, Ip_nu, Kp_nu;
-          try
-            {
-              std::__detail::__cyl_bessel_ik(nu[n], x, I_nu, K_nu, Ip_nu, Kp_nu);
-            }
-          catch (std::exception & err)
-            {
-              std::cerr << err.what() << std::endl;
-            }
-          auto W_JN = _S_pi_2 * x * (J_nu * Np_nu - N_nu * Jp_nu);
-          auto W_IK = -x * (I_nu * Kp_nu - K_nu * Ip_nu);
-          std::cout << "  " << std::setw(width) << x;
-          std::cout << "  " << std::setw(width) << J_nu;
-          std::cout << "  " << std::setw(width) << N_nu;
-          std::cout << "  " << std::setw(width) << W_JN;
-          std::cout << "  " << std::setw(width) << I_nu;
-          std::cout << "  " << std::setw(width) << K_nu;
-          std::cout << "  " << std::setw(width) << W_IK;
-          std::cout << std::endl;
-        }
-    }
+      _Tp{0},
+      _Tp{1} / _Tp{3},
+      _Tp{1} / _Tp{2},
+      _Tp{2} / _Tp{3},
+      _Tp{1},
+      _Tp{2},
+      _Tp{5},
+      _Tp{10},
+      _Tp{20},
+      _Tp{50},
+      _Tp{100},
+    };
 
-  return;
-}
+    for (auto nu : nuvec)
+      {
+	std::cout << "\n  nu = " << nu << '\n';
+	std::cout << ' ' << std::setw(width) << "x";
+	std::cout << ' ' << std::setw(width) << "J_nu(x)";
+	std::cout << ' ' << std::setw(width) << "J'_nu(x)";
+	std::cout << ' ' << std::setw(width) << "N_nu(x)";
+	std::cout << ' ' << std::setw(width) << "N'_nu(x)";
+	std::cout << ' ' << std::setw(width) << "pi x W{J,N} / 2";
+	std::cout << ' ' << std::setw(width) << "I_nu(x)";
+	std::cout << ' ' << std::setw(width) << "I'_nu(x)";
+	std::cout << ' ' << std::setw(width) << "K_nu(x)";
+	std::cout << ' ' << std::setw(width) << "K'_nu(x)";
+	std::cout << ' ' << std::setw(width) << "-x W{I,K}";
+	std::cout << '\n';
+	for (unsigned int i = 0; i <= 1000; ++i)
+          {
+            auto x = _Tp(i) / _Tp{100};
+            std::cout << ' ' << std::setw(width) << x;
+            try
+              {
+        	auto Cyl = std::__detail::__cyl_bessel_jn(nu, x);
+        	std::cout << ' ' << std::setw(width) << Cyl.__J_value;
+        	std::cout << ' ' << std::setw(width) << Cyl.__J_deriv;
+        	std::cout << ' ' << std::setw(width) << Cyl.__N_value;
+        	std::cout << ' ' << std::setw(width) << Cyl.__N_deriv;
+        	std::cout << ' ' << std::setw(width) << _S_pi_2 * x * Cyl.__Wronskian();
+              }
+            catch (std::exception& err)
+              {
+        	std::cerr << err.what() << '\n';
+              }
+            try
+              {
+        	auto Cyl = std::__detail::__cyl_bessel_ik(nu, x);
+        	std::cout << ' ' << std::setw(width) << Cyl.__I_value;
+        	std::cout << ' ' << std::setw(width) << Cyl.__I_deriv;
+        	std::cout << ' ' << std::setw(width) << Cyl.__K_value;
+        	std::cout << ' ' << std::setw(width) << Cyl.__K_deriv;
+        	std::cout << ' ' << std::setw(width) << -x * Cyl.__Wronskian();
+              }
+            catch (std::exception& err)
+              {
+        	std::cerr << err.what() << '\n';
+              }
+            std::cout << '\n';
+          }
+      }
+
+    return;
+  }
+
+template<typename _Tp>
+  void
+  test_std_bessel()
+  {
+    using _Val = _Tp;
+    using _Real = std::__detail::__num_traits_t<_Val>;
+
+    std::cout.precision(__gnu_cxx::__digits10<_Real>());
+    std::cout << std::showpoint << std::scientific;
+    auto width = 8 + std::cout.precision();
+
+    std::cout << '\n';
+
+    std::vector<_Tp> nuvec
+    {
+      _Tp{0},
+      _Tp{1} / _Tp{3},
+      _Tp{1} / _Tp{2},
+      _Tp{2} / _Tp{3},
+      _Tp{1},
+      _Tp{2},
+      _Tp{5},
+      _Tp{10},
+      _Tp{20},
+      _Tp{50},
+      _Tp{100},
+    };
+
+    for (auto nu : nuvec)
+      {
+	std::cout << ' ' << std::setw(width) << "x";
+	std::cout << ' ' << std::setw(width) << "J_nu(x)";
+	std::cout << ' ' << std::setw(width) << "N_nu(x)";
+	std::cout << ' ' << std::setw(width) << "I_nu(x)";
+	std::cout << ' ' << std::setw(width) << "K_nu(x)";
+	for (int i = 0; i <= 100; ++i)
+	  {
+	    auto x = i * _Tp{0.1Q};
+	    std::cout << ' ' << std::setw(width) << x;
+	    try
+	      {
+		auto jx = std::cyl_bessel(nu, x);
+		auto nx = std::cyl_neumann(nu, x);
+		std::cout << ' ' << std::setw(width) << jx;
+		std::cout << ' ' << std::setw(width) << nx;
+	      }
+	    catch (std::exception& err)
+	      {
+		std::cerr << err.what() << '\n';
+		auto ix = std::cyl_bessel_i(nu, x);
+		auto kx = std::cyl_bessel_k(nu, x);
+		std::cout << ' ' << std::setw(width) << ix;
+		std::cout << ' ' << std::setw(width) << kx;
+	      }
+	    std::cout << '\n';
+	  }
+      }
+  }
 
 
 ///
 ///
 ///
-int main(int, char **)
+int
+main()
 {
+  test_cyl_bessel<float>();
+  test_cyl_bessel<double>();
+  test_cyl_bessel<long double>();
+  test_cyl_bessel<__float128>();
 
-  test_bessel<double>();
+  test_std_bessel<float>();
+  test_std_bessel<double>();
+  test_std_bessel<long double>();
+  test_std_bessel<__float128>();
 
   return 0;
 }
