4304071594ad575772084cf473fdd7a87f9fdebb
emsr <3dw4rd@verizon.net>
2016-11-04 11:31:52 -0400

    Restore the plot files that were removed. Restore 'with lines' for linear plots. Move origin_bessel_j to origin_cyl_bessel_j.

--- plot_airy.sh
+++ plot_airy.sh
@@ -1,40 +1,40 @@
 
 /usr/local/bin/gnuplot --persist -e "plot [-20.0:5.0][-1.5:+1.5] \
- 'plot/airy_float.txt' using 1:2 with pm3d title 'Ai', \
-        	    '' using 1:3 with pm3d title 'Ai''', \
-        	    '' using 1:4 with pm3d title 'Bi', \
-        	    '' using 1:5 with pm3d title 'Bi'''"
+ 'plot/airy_float.txt' using 1:2 with lines title 'Ai', \
+        	    '' using 1:3 with lines title 'Ai''', \
+        	    '' using 1:4 with lines title 'Bi', \
+        	    '' using 1:5 with lines title 'Bi'''"
 
 /usr/local/bin/gnuplot --persist -e "plot [-20.0:5.0][-1.5:+1.5] \
- 'plot/airy_double.txt' using 1:2 with pm3d title 'Ai', \
-        	     '' using 1:3 with pm3d title 'Ai''', \
-        	     '' using 1:4 with pm3d title 'Bi', \
-        	     '' using 1:5 with pm3d title 'Bi'''"
+ 'plot/airy_double.txt' using 1:2 with lines title 'Ai', \
+        	     '' using 1:3 with lines title 'Ai''', \
+        	     '' using 1:4 with lines title 'Bi', \
+        	     '' using 1:5 with lines title 'Bi'''"
 
 /usr/local/bin/gnuplot --persist -e "plot [-20.0:5.0][-1.0:+1.0] \
- 'plot/airy_long_double.txt' using 1:2 with pm3d title 'Ai', \
-                	  '' using 1:4 with pm3d title 'Bi'"
+ 'plot/airy_long_double.txt' using 1:2 with lines title 'Ai', \
+                	  '' using 1:4 with lines title 'Bi'"
 
 /usr/local/bin/gnuplot --persist -e "plot [-20.0:5.0][-1.5:+1.5] \
- 'plot/airy_long_double.txt' using 1:2 with pm3d title 'Ai', \
-                	  '' using 1:3 with pm3d title 'Ai'''"
+ 'plot/airy_long_double.txt' using 1:2 with lines title 'Ai', \
+                	  '' using 1:3 with lines title 'Ai'''"
 
 /usr/local/bin/gnuplot --persist -e "plot [-20.0:5.0][-1.5:+1.5] \
- 'plot/airy_long_double.txt' using 1:4 with pm3d title 'Bi', \
-                	  '' using 1:5 with pm3d title 'Bi'''"
+ 'plot/airy_long_double.txt' using 1:4 with lines title 'Bi', \
+                	  '' using 1:5 with lines title 'Bi'''"
 
 
 /usr/local/bin/gnuplot --persist -e "plot [-20.0:5.0][-1.0:+1.0] \
- 'plot/scorer_double.txt' using 1:2 with pm3d title 'Gi', \
-                	  '' using 1:4 with pm3d title 'Hi'"
+ 'plot/scorer_double.txt' using 1:2 with lines title 'Gi', \
+                	  '' using 1:4 with lines title 'Hi'"
 
 /usr/local/bin/gnuplot --persist -e "plot [-20.0:5.0][-1.5:+1.5] \
- 'plot/scorer_double.txt' using 1:2 with pm3d title 'Gi', \
-                	  '' using 1:3 with pm3d title 'Gi'''"
+ 'plot/scorer_double.txt' using 1:2 with lines title 'Gi', \
+                	  '' using 1:3 with lines title 'Gi'''"
 
 /usr/local/bin/gnuplot --persist -e "plot [-20.0:5.0][-1.5:+1.5] \
- 'plot/scorer_double.txt' using 1:4 with pm3d title 'Hi', \
-                	  '' using 1:5 with pm3d title 'Hi'''"
+ 'plot/scorer_double.txt' using 1:4 with lines title 'Hi', \
+                	  '' using 1:5 with lines title 'Hi'''"
 
 
 /usr/local/bin/gnuplot --persist -e "splot [-20.0:5.0][-5.0:5.0][-1.5:+1.5] \
@@ -50,47 +50,47 @@ set palette model RGB defined (0.0 "blue", 0.3 "green", 0.7 "yellow", 1 "red")
 set colorbox
 set pm3d at sb corners2color geomean
 
-plot [-5.0:2.0][-1.5:+1.5] "plot/fgh_double.txt" using 1:2 with pm3d title "fai", "" using 1:3 with pm3d title "gai", "" using 1:4 with pm3d title "hai"
-plot [-5.0:2.0][-2.5:+2.5] "plot/fgh_double.txt" using 1:5 with pm3d title "fai'", "" using 1:6 with pm3d title "gai'", "" using 1:7 with pm3d title "hai'"
-plot [-5.0:2.0][-2.5:+2.5] "plot/fgh_double.txt" using 1:($2+$3+$4) with pm3d title "Hi"
-plot [-5.0:2.0][-2.5:+2.5] "plot/fgh_double.txt" using 1:($5+$6+$7) with pm3d title "Hi'"
+plot [-5.0:2.0][-1.5:+1.5] "plot/fgh_double.txt" using 1:2 with lines title "fai", "" using 1:3 with lines title "gai", "" using 1:4 with lines title "hai"
+plot [-5.0:2.0][-2.5:+2.5] "plot/fgh_double.txt" using 1:5 with lines title "fai'", "" using 1:6 with lines title "gai'", "" using 1:7 with lines title "hai'"
+plot [-5.0:2.0][-2.5:+2.5] "plot/fgh_double.txt" using 1:($2+$3+$4) with lines title "Hi"
+plot [-5.0:2.0][-2.5:+2.5] "plot/fgh_double.txt" using 1:($5+$6+$7) with lines title "Hi'"
 
-plot [-20.0:5.0][-1.0:+1.0] "plot/scorer_double.txt" using 1:2 with pm3d title "Gi", "" using 1:4 with pm3d title "Hi"
-plot [-20.0:5.0][-1.5:+1.5] "plot/scorer_double.txt" using 1:2 with pm3d title "Gi", "" using 1:3 with pm3d title "Gi'"
-plot [-20.0:5.0][-1.5:+1.5] "plot/scorer_double.txt" using 1:4 with pm3d title "Hi", "" using 1:5 with pm3d title "Hi'"
+plot [-20.0:5.0][-1.0:+1.0] "plot/scorer_double.txt" using 1:2 with lines title "Gi", "" using 1:4 with lines title "Hi"
+plot [-20.0:5.0][-1.5:+1.5] "plot/scorer_double.txt" using 1:2 with lines title "Gi", "" using 1:3 with lines title "Gi'"
+plot [-20.0:5.0][-1.5:+1.5] "plot/scorer_double.txt" using 1:4 with lines title "Hi", "" using 1:5 with lines title "Hi'"
 
-plot [-20.0:5.0][-1.0:+1.0] "plot/scorer_float.txt" using 1:2 with pm3d title "Gi", "" using 1:3 with pm3d title "Gi'", "" using 1:4 with pm3d title "Hi", "" using 1:5 with pm3d title "Hi'"
-plot [-20.0:5.0][-1.0:+1.0] "plot/scorer_double.txt" using 1:2 with pm3d title "Gi", "" using 1:3 with pm3d title "Gi'", "" using 1:4 with pm3d title "Hi", "" using 1:5 with pm3d title "Hi'"
-plot [-20.0:5.0][-1.0:+1.0] "plot/scorer_long_double.txt" using 1:2 with pm3d title "Gi", "" using 1:3 with pm3d title "Gi'", "" using 1:4 with pm3d title "Hi", "" using 1:5 with pm3d title "Hi'"
+plot [-20.0:5.0][-1.0:+1.0] "plot/scorer_float.txt" using 1:2 with lines title "Gi", "" using 1:3 with lines title "Gi'", "" using 1:4 with lines title "Hi", "" using 1:5 with lines title "Hi'"
+plot [-20.0:5.0][-1.0:+1.0] "plot/scorer_double.txt" using 1:2 with lines title "Gi", "" using 1:3 with lines title "Gi'", "" using 1:4 with lines title "Hi", "" using 1:5 with lines title "Hi'"
+plot [-20.0:5.0][-1.0:+1.0] "plot/scorer_long_double.txt" using 1:2 with lines title "Gi", "" using 1:3 with lines title "Gi'", "" using 1:4 with lines title "Hi", "" using 1:5 with lines title "Hi'"
 
-plot [-20.0:5.0][-1.0:+1.0] "plot/airy_float.txt" using 1:2 with pm3d title "Ai(x)", "" using 1:4 with pm3d title "Bi(x)"
-plot [-20.0:5.0][-1.5:+1.5] "plot/airy_float.txt" using 1:2 with pm3d title "Ai(x)", "" using 1:3 with pm3d title "Ai'(x)"
-plot [-20.0:5.0][-1.5:+1.5] "plot/airy_float.txt" using 1:4 with pm3d title "Bi(x)", "" using 1:5 with pm3d title "Bi'(x)"
+plot [-20.0:5.0][-1.0:+1.0] "plot/airy_float.txt" using 1:2 with lines title "Ai(x)", "" using 1:4 with lines title "Bi(x)"
+plot [-20.0:5.0][-1.5:+1.5] "plot/airy_float.txt" using 1:2 with lines title "Ai(x)", "" using 1:3 with lines title "Ai'(x)"
+plot [-20.0:5.0][-1.5:+1.5] "plot/airy_float.txt" using 1:4 with lines title "Bi(x)", "" using 1:5 with lines title "Bi'(x)"
 
-plot [-20.0:5.0][-1.0:+1.0] "plot/airy_double.txt" using 1:2 with pm3d title "Ai(x)", "" using 1:4 with pm3d title "Bi(x)"
-plot [-20.0:5.0][-1.5:+1.5] "plot/airy_double.txt" using 1:2 with pm3d title "Ai(x)", "" using 1:3 with pm3d title "Ai'(x)"
-plot [-20.0:5.0][-1.5:+1.5] "plot/airy_double.txt" using 1:4 with pm3d title "Bi(x)", "" using 1:5 with pm3d title "Bi'(x)"
+plot [-20.0:5.0][-1.0:+1.0] "plot/airy_double.txt" using 1:2 with lines title "Ai(x)", "" using 1:4 with lines title "Bi(x)"
+plot [-20.0:5.0][-1.5:+1.5] "plot/airy_double.txt" using 1:2 with lines title "Ai(x)", "" using 1:3 with lines title "Ai'(x)"
+plot [-20.0:5.0][-1.5:+1.5] "plot/airy_double.txt" using 1:4 with lines title "Bi(x)", "" using 1:5 with lines title "Bi'(x)"
 
-plot [-20.0:5.0][-1.0:+1.0] "plot/airy_long_double.txt" using 1:2 with pm3d title "Ai(x)", "" using 1:4 with pm3d title "Bi(x)"
-plot [-20.0:5.0][-1.5:+1.5] "plot/airy_long_double.txt" using 1:2 with pm3d title "Ai(x)", "" using 1:3 with pm3d title "Ai'(x)"
-plot [-20.0:5.0][-1.5:+1.5] "plot/airy_long_double.txt" using 1:4 with pm3d title "Bi(x)", "" using 1:5 with pm3d title "Bi'(x)"
+plot [-20.0:5.0][-1.0:+1.0] "plot/airy_long_double.txt" using 1:2 with lines title "Ai(x)", "" using 1:4 with lines title "Bi(x)"
+plot [-20.0:5.0][-1.5:+1.5] "plot/airy_long_double.txt" using 1:2 with lines title "Ai(x)", "" using 1:3 with lines title "Ai'(x)"
+plot [-20.0:5.0][-1.5:+1.5] "plot/airy_long_double.txt" using 1:4 with lines title "Bi(x)", "" using 1:5 with lines title "Bi'(x)"
 
-plot [0.0:30.0][-0.5:+6.0] "plot/struve_float.txt" using 1:2 with pm3d title "H_0", "" using 1:4 with pm3d title "H_1", "" using 1:6 with pm3d title "H_2", "" using 1:8 with pm3d title "H_3"
-plot [0.0:5.0][-0.5:+4.5] "plot/struve_float.txt" using 1:3 with pm3d title "L_0", "" using 1:5 with pm3d title "L_1", "" using 1:7 with pm3d title "L_2", "" using 1:9 with pm3d title "L_3"
+plot [0.0:30.0][-0.5:+6.0] "plot/struve_float.txt" using 1:2 with lines title "H_0", "" using 1:4 with lines title "H_1", "" using 1:6 with lines title "H_2", "" using 1:8 with lines title "H_3"
+plot [0.0:5.0][-0.5:+4.5] "plot/struve_float.txt" using 1:3 with lines title "L_0", "" using 1:5 with lines title "L_1", "" using 1:7 with lines title "L_2", "" using 1:9 with lines title "L_3"
 
-plot [0.0:30.0][-0.5:+6.0] "plot/struve_double.txt" using 1:2 with pm3d title "H_0", "" using 1:4 with pm3d title "H_1", "" using 1:6 with pm3d title "H_2", "" using 1:8 with pm3d title "H_3"
-plot [0.0:5.0][-0.5:+4.5] "plot/struve_double.txt" using 1:3 with pm3d title "L_0", "" using 1:5 with pm3d title "L_1", "" using 1:7 with pm3d title "L_2", "" using 1:9 with pm3d title "L_3"
+plot [0.0:30.0][-0.5:+6.0] "plot/struve_double.txt" using 1:2 with lines title "H_0", "" using 1:4 with lines title "H_1", "" using 1:6 with lines title "H_2", "" using 1:8 with lines title "H_3"
+plot [0.0:5.0][-0.5:+4.5] "plot/struve_double.txt" using 1:3 with lines title "L_0", "" using 1:5 with lines title "L_1", "" using 1:7 with lines title "L_2", "" using 1:9 with lines title "L_3"
 
-plot [0.0:30.0][-0.5:+6.0] "plot/struve_long_double.txt" using 1:2 with pm3d title "H_0", "" using 1:4 with pm3d title "H_1", "" using 1:6 with pm3d title "H_2", "" using 1:8 with pm3d title "H_3"
-plot [0.0:5.0][-0.5:+4.5] "plot/struve_long_double.txt" using 1:3 with pm3d title "L_0", "" using 1:5 with pm3d title "L_1", "" using 1:7 with pm3d title "L_2", "" using 1:9 with pm3d title "L_3"
+plot [0.0:30.0][-0.5:+6.0] "plot/struve_long_double.txt" using 1:2 with lines title "H_0", "" using 1:4 with lines title "H_1", "" using 1:6 with lines title "H_2", "" using 1:8 with lines title "H_3"
+plot [0.0:5.0][-0.5:+4.5] "plot/struve_long_double.txt" using 1:3 with lines title "L_0", "" using 1:5 with lines title "L_1", "" using 1:7 with lines title "L_2", "" using 1:9 with lines title "L_3"
 
-plot [0.0:40.0][-1.5:1.5] "plot/kelvin_float.txt" using 1:2 with pm3d title "ber(x)", "" using 1:3 with pm3d title "bei(x)", "" using 1:4 with pm3d title "ker(x)", "" using 1:5 with pm3d title "kei(x)"
-plot [0.0:40.0][-1.5:1.5] "plot/kelvin_double.txt" using 1:2 with pm3d title "ber(x)", "" using 1:3 with pm3d title "bei(x)", "" using 1:4 with pm3d title "ker(x)", "" using 1:5 with pm3d title "kei(x)"
-plot [0.0:40.0][-1.5:1.5] "plot/kelvin_long_double.txt" using 1:2 with pm3d title "ber(x)", "" using 1:3 with pm3d title "bei(x)", "" using 1:4 with pm3d title "ker(x)", "" using 1:5 with pm3d title "kei(x)"
+plot [0.0:40.0][-1.5:1.5] "plot/kelvin_float.txt" using 1:2 with lines title "ber(x)", "" using 1:3 with lines title "bei(x)", "" using 1:4 with lines title "ker(x)", "" using 1:5 with lines title "kei(x)"
+plot [0.0:40.0][-1.5:1.5] "plot/kelvin_double.txt" using 1:2 with lines title "ber(x)", "" using 1:3 with lines title "bei(x)", "" using 1:4 with lines title "ker(x)", "" using 1:5 with lines title "kei(x)"
+plot [0.0:40.0][-1.5:1.5] "plot/kelvin_long_double.txt" using 1:2 with lines title "ber(x)", "" using 1:3 with lines title "bei(x)", "" using 1:4 with lines title "ker(x)", "" using 1:5 with lines title "kei(x)"
 
-plot [0.0:40.0][-1.5:1.5] "plot/kelvin_order_float.txt" using 1:2 with pm3d title "ber(x)", "" using 1:3 with pm3d title "bei(x)", "" using 1:4 with pm3d title "ker(x)", "" using 1:5 with pm3d title "kei(x)"
-plot [0.0:40.0][-1.5:1.5] "plot/kelvin_order_double.txt" using 1:2 with pm3d title "ber(x)", "" using 1:3 with pm3d title "bei(x)", "" using 1:4 with pm3d title "ker(x)", "" using 1:5 with pm3d title "kei(x)"
-plot [0.0:40.0][-1.5:1.5] "plot/kelvin_order_long_double.txt" using 1:2 with pm3d title "ber(x)", "" using 1:3 with pm3d title "bei(x)", "" using 1:4 with pm3d title "ker(x)", "" using 1:5 with pm3d title "kei(x)"
+plot [0.0:40.0][-1.5:1.5] "plot/kelvin_order_float.txt" using 1:2 with lines title "ber(x)", "" using 1:3 with lines title "bei(x)", "" using 1:4 with lines title "ker(x)", "" using 1:5 with lines title "kei(x)"
+plot [0.0:40.0][-1.5:1.5] "plot/kelvin_order_double.txt" using 1:2 with lines title "ber(x)", "" using 1:3 with lines title "bei(x)", "" using 1:4 with lines title "ker(x)", "" using 1:5 with lines title "kei(x)"
+plot [0.0:40.0][-1.5:1.5] "plot/kelvin_order_long_double.txt" using 1:2 with lines title "ber(x)", "" using 1:3 with lines title "bei(x)", "" using 1:4 with lines title "ker(x)", "" using 1:5 with lines title "kei(x)"
 
 /usr/local/bin/gnuplot
 set xzeroaxis
