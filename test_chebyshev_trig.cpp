59e89c51748d138e411036bf7d23c575b59ec3a1
emsr <3dw4rd@verizon.net>
2016-11-07 14:11:25 -0500

    Add zero finders for the chebyshev testers

--- test_chebyshev_trig.cpp
+++ test_chebyshev_trig.cpp
@@ -17,7 +17,8 @@ g++ -std=gnu++14 -o test_chebyshev_trig test_chebyshev_trig.cpp -lquadmath
 #include <bits/float128_io.h>
 
 /**
- * Return the Chebyshev polynomial of the first kind by trigonometric identity:
+ * Return the Chebyshev polynomial of the first kind
+ * of order @f$ n @f$, @f$ T_n(x) @f$ by trigonometric identity:
  * @f[
  *   T_n(x) = \cos(n\theta)
  * @f]
@@ -32,7 +33,27 @@ template<typename _Tp>
   }
 
 /**
- * Return the Chebyshev polynomial of the second kind by trigonometric identity:
+ * Return a vector of zeros of the Chebyshev function of the first kind
+ * of order @f$ n @f$, @f$ T_n(x) @f$.
+ * The zeros are given by:
+ * @f[
+ *   x_k = \cos\left(\frac{(k+\frac{1}{2})\pi}{n+1}\right),
+ *     k \elem {1, ..., n}
+ * @f]
+ */
+template<typename _Tp>
+  std::vector<_Tp>
+  __chebyshev_t_zeros(unsigned int __n)
+  {
+    std::vector<_Tp> __zero;
+    for (unsigned int __k = 0; __k < __n; ++__k)
+      __zero.push_back(__gnu_cxx::cos_pi(_Tp(__k + 0.5L) / _Tp(__n)));
+    return __zero;
+  }
+
+/**
+ * Return the Chebyshev polynomial of the second kind
+ * of order @f$ n @f$, @f$ U_n(x) @f$ by trigonometric identity:
  * @f[
  *   U_n(x) = \frac{\sin((n + 1)\theta)}{\sin(\theta)}
  * @f]
@@ -56,7 +77,26 @@ template<typename _Tp>
   }
 
 /**
- * Return the Chebyshev polynomial of the third kind by trigonometric identity:
+ * Return a vector of zeros of the Chebyshev function of the second kind
+ * of order @f$ n @f$, @f$ U_n(x) @f$.
+ * The zeros are given by:
+ * @f[
+ *   x_k = \cos\left(\frac{k\pi}{n+1}\right), k \elem {1, ..., n}
+ * @f]
+ */
+template<typename _Tp>
+  std::vector<_Tp>
+  __chebyshev_u_zeros(unsigned int __n)
+  {
+    std::vector<_Tp> __zero;
+    for (unsigned int __k = 1; __k <= __n; ++__k)
+      __zero.push_back(__gnu_cxx::cos_pi(_Tp(__k) / _Tp(__n + 1)));
+    return __zero;
+  }
+
+/**
+ * Return the Chebyshev polynomial of the third kind
+ * of order @f$ n @f$, @f$ V_n(x) @f$ by trigonometric identity:
  * @f[
  *   V_n(x) = \frac{\cos((n + \frac{1}{2})\theta)}
  *                 {cos(\frac{1}{2}\theta)}
@@ -79,7 +119,27 @@ template<typename _Tp>
   }
 
 /**
- * Return the Chebyshev polynomial of the fourth kind by trigonometric identity:
+ * Return a vector of zeros of the Chebyshev function of the third kind
+ * of order @f$ n @f$, @f$ V_n(x) @f$.
+ * The zeros are given by:
+ * @f[
+ *   x_k = \cos\left(\frac{\left(k+\frac{1}{2}\right)\pi}{n+\frac{1}{2}}\right),
+ *       k \elem {1, ..., n}
+ * @f]
+ */
+template<typename _Tp>
+  std::vector<_Tp>
+  __chebyshev_v_zeros(unsigned int __n)
+  {
+    std::vector<_Tp> __zero;
+    for (unsigned int __k = 0; __k < __n; ++__k)
+      __zero.push_back(__gnu_cxx::cos_pi(_Tp(__k + 0.5L) / _Tp(__n + 0.5L)));
+    return __zero;
+  }
+
+/**
+ * Return the Chebyshev polynomial of the fourth kind
+ * of order @f$ n @f$, @f$ W_n(x) @f$ by trigonometric identity:
  * @f[
  *   W_n(x) = \frac{\sin((n + \frac{1}{2})\theta)}
  *                 {\sin(\frac{1}{2}\theta)}
@@ -99,6 +159,25 @@ template<typename _Tp>
 	return std::sin(_Tp(__n + 0.5L) * __theta)
 	     / std::sin(_Tp{0.5L} * __theta);
       }
+  }
+
+/**
+ * Return a vector of zeros of the Chebyshev function of the fourth kind
+ * of order @f$ n @f$, @f$ W_n(x) @f$.
+ * The zeros are given by:
+ * @f[
+ *   x_k = \cos\left(\frac{k\pi}{n+\frac{1}{2}}\right),
+ *       k \elem {1, ..., n}
+ * @f]
+ */
+template<typename _Tp>
+  std::vector<_Tp>
+  __chebyshev_w_zeros(unsigned int __n)
+  {
+    std::vector<_Tp> __zero;
+    for (unsigned int __k = 1; __k <= __n; ++__k)
+      __zero.push_back(__gnu_cxx::cos_pi(_Tp(__k) / _Tp(__n + 0.5L)));
+    return __zero;
   }
 
 template<typename Tp>
@@ -138,6 +217,70 @@ template<typename Tp>
 		      << ' ' << std::setw(width) << Ut - Ug
 		      << ' ' << std::setw(width) << Vt - Vg
 		      << ' ' << std::setw(width) << Wt - Wg
+		      << '\n';
+	  }
+      }
+
+    std::cout << "\n\nZeros of Chebyshev T_n(x)\n";
+    for (auto n : index)
+      {
+	std::cout << "\n n = " << std::setw(width) << n << '\n';
+	auto tz = __chebyshev_t_zeros<Tp>(n);
+	for (auto z : tz)
+	  {
+	    auto Tt = __chebyshev_t_trig(n, z);
+	    auto Tg = __gnu_cxx::chebyshev_t(n, z);
+	    std::cout << ' ' << std::setw(width) << z
+		      << ' ' << std::setw(width) << Tt
+		      << ' ' << std::setw(width) << Tg
+		      << '\n';
+	  }
+      }
+
+    std::cout << "\n\nZeros of Chebyshev U_n(x)\n";
+    for (auto n : index)
+      {
+	std::cout << "\n n = " << std::setw(width) << n << '\n';
+	auto uz = __chebyshev_u_zeros<Tp>(n);
+	for (auto z : uz)
+	  {
+	    auto Ut = __chebyshev_u_trig(n, z);
+	    auto Ug = __gnu_cxx::chebyshev_u(n, z);
+	    std::cout << ' ' << std::setw(width) << z
+		      << ' ' << std::setw(width) << Ut
+		      << ' ' << std::setw(width) << Ug
+		      << '\n';
+	  }
+      }
+
+    std::cout << "\n\nZeros of Chebyshev V_n(x)\n";
+    for (auto n : index)
+      {
+	std::cout << "\n n = " << std::setw(width) << n << '\n';
+	auto vz = __chebyshev_v_zeros<Tp>(n);
+	for (auto z : vz)
+	  {
+	    auto Vt = __chebyshev_v_trig(n, z);
+	    auto Vg = __gnu_cxx::chebyshev_v(n, z);
+	    std::cout << ' ' << std::setw(width) << z
+		      << ' ' << std::setw(width) << Vt
+		      << ' ' << std::setw(width) << Vg
+		      << '\n';
+	  }
+      }
+
+    std::cout << "\n\nZeros of Chebyshev W_n(x)\n";
+    for (auto n : index)
+      {
+	std::cout << "\n n = " << std::setw(width) << n << '\n';
+	auto wz = __chebyshev_w_zeros<Tp>(n);
+	for (auto z : wz)
+	  {
+	    auto Wt = __chebyshev_w_trig(n, z);
+	    auto Wg = __gnu_cxx::chebyshev_w(n, z);
+	    std::cout << ' ' << std::setw(width) << z
+		      << ' ' << std::setw(width) << Wt
+		      << ' ' << std::setw(width) << Wg
 		      << '\n';
 	  }
       }
