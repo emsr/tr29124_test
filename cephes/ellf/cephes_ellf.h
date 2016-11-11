7af236d8c6d4781977e0d2cdbd3e58d739708841
emsr <3dw4rd@verizon.net>
2016-11-11 17:49:06 -0500

    Build out more of the CEPHES library for testing.

--- cephes/ellf/cephes_ellf.h
+++ cephes/ellf/cephes_ellf.h
@@ -1,3 +1,9 @@
+#ifndef CEPHES_ELLF
+#define CEPHES_ELLF 1
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 double ellik(double phi, double m);
 double ellpk(double x);
@@ -6,3 +12,9 @@ double ellie(double phi, double m);
 double ellpe(double x);
 
 int ellpj(double u, double m, double *sn, double *cn, double *dn, double *ph);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // CEPHES_ELLF
