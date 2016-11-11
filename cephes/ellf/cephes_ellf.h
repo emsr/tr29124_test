2dd33694f309879a5d7f4244493f7e5d8a21119d
emsr <3dw4rd@verizon.net>
2016-11-11 14:16:18 -0500

    Make more cephes libs available.

--- cephes/ellf/cephes_ellf.h
+++ cephes/ellf/cephes_ellf.h
@@ -1,6 +1,8 @@
 
 double ellik(double phi, double m);
-double ellpk(double x)
+double ellpk(double x);
 
 double ellie(double phi, double m);
 double ellpe(double x);
+
+int ellpj(double u, double m, double *sn, double *cn, double *dn, double *ph);
