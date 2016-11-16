d10591e4a1d29c0f989c33db9bfe8c7c4c51860e
emsr <3dw4rd@verizon.net>
2016-11-16 15:51:01 -0500

    Tidy up LentzContinuedFraction. Add a new LentzContinuedFraction and make a tester for it.

---
+++ test_steed_continued_fraction.cpp
@@ -0,0 +1,131 @@
+/*
+$HOME/bin_tr29124/bin/g++ -std=gnu++17 -I. -o test_steed_continued_fraction test_steed_continued_fraction.cpp -lquadmath
+./test_steed_continued_fraction > test_steed_continued_fraction.txt
+
+$HOME/bin/bin/g++ -std=gnu++17 -I. -o test_steed_continued_fraction test_steed_continued_fraction.cpp -lquadmath
+./test_steed_continued_fraction > test_steed_continued_fraction.txt
+*/
+
+#include <ext/cmath>
+#include <complex>
+#include <iostream>
+
+#include "SteedContinuedFraction.tcc"
+
+template<typename _Tp>
+  void
+  test_steed_continued_fraction(_Tp proto = _Tp{})
+  {
+    std::cout.precision(__gnu_cxx::__digits10(proto));
+    std::cout << std::showpoint << std::scientific;
+    auto width = 8 + std::cout.precision();
+
+    const auto _S_pi_2 = __gnu_cxx::__const_pi_half(proto);
+    using _Cmplx = std::complex<_Tp>;
+
+    auto a_trigint
+      = [](std::size_t i, _Tp)
+	-> _Cmplx
+	{
+	  if (i == 1)
+	    return _Tp(1);
+	  else
+	    return -_Tp(i - 1) * _Tp(i - 1);
+	};
+    using _AFun = decltype(a_trigint);
+
+    auto b_trigint
+      = [](std::size_t i, _Tp __x)
+	{
+	  if (i == 0)
+	    return _Cmplx{0};
+	  else
+	    return _Cmplx{_Tp(2 * i - 1), __x};
+	};
+    using _BFun = decltype(b_trigint);
+
+    auto w_trigint
+      = [](std::size_t, _Tp)
+	{ return _Cmplx{}; };
+    using _TailFun = decltype(w_trigint);
+
+    _SteedContinuedFraction<_Tp, _AFun, _BFun, _TailFun>
+      SiCi(a_trigint, b_trigint, w_trigint);
+    auto t = 1.2;
+    auto y = SiCi(t);
+    y *= std::polar(_Tp{1}, -t);
+    y += _Cmplx{0, _S_pi_2};
+    std::cout << '\n';
+    std::cout << "SiCi = " << y << '\n';
+    std::cout << '\n';
+    std::cout << "Si = " << std::imag(y) << '\n';
+    std::cout << "Ci = " << -std::real(y) << '\n';
+    std::cout << '\n';
+    std::cout << "Si = " << __gnu_cxx::sinint(1.2) << '\n';
+    std::cout << "Ci = " << __gnu_cxx::cosint(1.2) << '\n';
+  }
+
+template<typename _Tp>
+  void
+  test_steed_hypint(_Tp proto = _Tp{})
+  {
+    using _Val = _Tp;
+    using _Real = std::__detail::__num_traits_t<_Val>;
+    using _Cmplx = std::complex<_Real>;
+
+    std::cout.precision(__gnu_cxx::__digits10(std::real(proto)));
+    std::cout << std::showpoint << std::scientific;
+    auto width = 8 + std::cout.precision();
+
+    const auto _S_pi_2 = __gnu_cxx::__const_pi_half(proto);
+    const auto _S_i = _Cmplx{0, 1};
+
+    auto a_hypint
+      = [](std::size_t i, _Tp)
+	-> _Cmplx
+	{
+	  if (i == 1)
+	    return _Tp(1);
+	  else
+	    return -_Tp(i - 1) * _Tp(i - 1);
+	};
+    using _AFun = decltype(a_hypint);
+
+    auto b_hypint
+      = [_S_i](std::size_t i, _Tp __x)
+	{
+	  if (i == 0)
+	    return _Cmplx{0};
+	  else
+	    return _Cmplx(_Tp(2 * i - 1), __x);
+	};
+    using _BFun = decltype(b_hypint);
+
+    auto w_hypint
+      = [](std::size_t, _Tp)
+	{ return _Cmplx{}; };
+    using _TailFun = decltype(w_hypint);
+
+    _SteedContinuedFraction<_Tp, _AFun, _BFun, _TailFun>
+      ShiChi(a_hypint, b_hypint, w_hypint);
+    auto t = 1.2;
+    auto y = ShiChi(t);
+    y *= std::polar(_Tp{1}, -t);
+    y += _Cmplx{0, _S_pi_2};
+    std::cout << '\n';
+    std::cout << "ShiChi = " << y << '\n';
+    std::cout << '\n';
+    std::cout << "Shi = " << std::imag(y) << '\n';
+    std::cout << "Chi = " << -std::real(y) << '\n';
+    std::cout << '\n';
+    std::cout << "Shi = " << __gnu_cxx::sinhint(1.2) << '\n';
+    std::cout << "Chi = " << __gnu_cxx::coshint(1.2) << '\n';
+  }
+
+
+int
+main()
+{
+  test_steed_continued_fraction(1.0);
+  test_steed_hypint(1.0);
+}
