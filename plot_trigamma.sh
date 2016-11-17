1b57a14940657793556525f3d9b6e5a4cd7e8874
emsr <3dw4rd@verizon.net>
2016-11-17 13:10:16 -0500

    Get trigamma and tetragamma to plot separately.

--- plot_trigamma.sh
+++ plot_trigamma.sh
@@ -5,6 +5,6 @@ set xzeroaxis
 set yzeroaxis
 set grid
 
-plot [0.0:10.0][0.0:1.0] "test_trigamma.txt" index 0 using 1:2 with lines title "phi_1(x)"
+plot [0.0:20.0][0.0:1.0] "test_trigamma.txt" index 0 using 1:2 with lines title "phi_1(x)"
 
-plot [0.0:10.0][0.0:1.0] "test_trigamma.txt" index 1 using 1:2 with lines title "phi_2(x)"
+plot [0.0:20.0][-0.5:0.0] "test_trigamma.txt" index 1 using 1:2 with lines title "phi_2(x)"
