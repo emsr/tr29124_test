df0d1bca2e549f181f2cda78075f027ebedf0c2e
emsr <3dw4rd@verizon.net>
2016-12-02 16:29:51 -0500

    Use enhanced text for symbols and bold, etc.

--- plot_anger_weber.sh
+++ plot_anger_weber.sh
@@ -1,5 +1,6 @@
 
 gnuplot
+set termoption enhanced
 set xzeroaxis
 set yzeroaxis
 set grid
@@ -9,23 +10,23 @@ plot [-8.0:8.0][-1.0:1.0] "test_anger_weber.txt" index 0 using 1:3 with lines ti
 
 
 plot [-8.0:8.0][-1.0:1.0] \
-"test_anger_weber.txt" index 0 using 1:2 with lines title "J_0(x)", \
-                    "" index 0 using 1:3 with lines title "H_0(x)", \
-                    "" index 1 using 1:2 with lines title "J_0.5(x)", \
-                    "" index 1 using 1:3 with lines title "H_0.5(x)", \
-                    "" index 2 using 1:2 with lines title "J_1(x)", \
-                    "" index 2 using 1:3 with lines title "H_1(x)", \
-                    "" index 3 using 1:2 with lines title "J_1.5(x)", \
-                    "" index 3 using 1:3 with lines title "H_1.5(x)"
+"test_anger_weber.txt" index 0 using 1:2 with lines title "{/:Bold J}_0(x)", \
+                    "" index 0 using 1:3 with lines title "{/:Bold H}_0(x)", \
+                    "" index 1 using 1:2 with lines title "{/:Bold J}_{1/2}(x)", \
+                    "" index 1 using 1:3 with lines title "{/:Bold H}_{1/2}(x)", \
+                    "" index 2 using 1:2 with lines title "{/:Bold J}_1(x)", \
+                    "" index 2 using 1:3 with lines title "{/:Bold H}_1(x)", \
+                    "" index 3 using 1:2 with lines title "{/:Bold J}_{3/2}(x)", \
+                    "" index 3 using 1:3 with lines title "{/:Bold H}_{3/2}(x)"
 
 plot [-8.0:8.0][-1.0:1.0] \
-"test_anger_weber.txt" index 0 using 1:2 with lines title "J_0(x)", \
-                    "" index 1 using 1:2 with lines title "J_0.5(x)", \
-                    "" index 2 using 1:2 with lines title "J_1(x)", \
-                    "" index 3 using 1:2 with lines title "J_1.5(x)"
+"test_anger_weber.txt" index 0 using 1:2 with lines title "{/:Bold J}_0(x)", \
+                    "" index 1 using 1:2 with lines title "{/:Bold J}_{1/2}(x)", \
+                    "" index 2 using 1:2 with lines title "{/:Bold J}_1(x)", \
+                    "" index 3 using 1:2 with lines title "{/:Bold J}_{3/2}(x)"
 
 plot [-8.0:8.0][-1.0:1.0] \
-"test_anger_weber.txt" index 0 using 1:3 with lines title "H_0(x)", \
-                    "" index 1 using 1:3 with lines title "H_0.5(x)", \
-                    "" index 2 using 1:3 with lines title "H_1(x)", \
-                    "" index 3 using 1:3 with lines title "H_1.5(x)"
+"test_anger_weber.txt" index 0 using 1:3 with lines title "{/:Bold H}_0(x)", \
+                    "" index 1 using 1:3 with lines title "{/:Bold H}_{1/2}(x)", \
+                    "" index 2 using 1:3 with lines title "{/:Bold H}_1(x)", \
+                    "" index 3 using 1:3 with lines title "{/:Bold H}_{3/2}(x)"
--- plot_bessel.sh
+++ plot_bessel.sh
@@ -1,6 +1,8 @@
 
 gnuplot
 
+set termoption enhanced
+
 set hidden3d
 
 set colorbox
@@ -8,11 +10,11 @@ set pm3d at bs corners2color geomean
 
 set palette model RGB defined (0.0 "blue", 0.3 "green", 0.7 "yellow", 1 "red")
 
-splot [0:100][0:100][-1:1] "plot/cyl_bessel_j_float.txt" index 0 with pm3d title "J_\nu(x)"
-splot [0:100][0:100][-1:1] "plot/cyl_bessel_j_double.txt" index 0 with pm3d title "J_\nu(x)"
+splot [0:100][0:100][-1:1] "plot/cyl_bessel_j_float.txt" index 0 with pm3d title "J_{/Symbol n}(x)"
+splot [0:100][0:100][-1:1] "plot/cyl_bessel_j_double.txt" index 0 with pm3d title "J_{/Symbol n}(x)"
 
 set palette model RGB defined (-1.0 "black", -0.2 "blue", 0.0 "green", 0.2 "yellow", 1.0 "red")
 
-splot [0:100][0:100][-10:1] "plot/cyl_neumann_float.txt" index 0 with pm3d title "N_\nu(x)"
-splot [0:100][0:100][-10:1] "plot/cyl_neumann_double.txt" index 0 with pm3d title "N_\nu(x)"
+splot [0:100][0:100][-10:1] "plot/cyl_neumann_float.txt" index 0 with pm3d title "N_{/Symbol n}(x)"
+splot [0:100][0:100][-10:1] "plot/cyl_neumann_double.txt" index 0 with pm3d title "N_{/Symbol n}(x)"
 
