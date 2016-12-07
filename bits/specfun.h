25a7e1e56105942a0163df7940a58272249d31b1
emsr <3dw4rd@verizon.net>
2016-12-07 09:35:37 -0500

    Checked in the diffd for hyperg after changingthe sums to add the last term, etc. Worked on distributions and testing.

--- bits/specfun.h
+++ bits/specfun.h
@@ -2816,6 +2816,39 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
       return std::__detail::__airy<__type>(__x).__Bi_value;
     }
 
+  // Log Gamma function for real argument.
+
+  /**
+   * Return the logarithm of the gamma function for
+   * <tt> float </tt> argument.
+   *
+   * @see lgamma for details.
+   */
+  inline float
+  lgammaf(float __a)
+  { return std::__detail::__log_gamma<float>(__a); }
+
+  /**
+   * Return the logarithm of the gamma function for
+   * <tt> long double </tt> argument.
+   *
+   * @see lgamma for details.
+   */
+  inline long double
+  lgammal(long double __a)
+  { return std::__detail::__log_gamma<long double>(__a); }
+
+  /**
+   * Return the logarithm of the gamma function for real argument.
+   */
+  template<typename _Ta>
+    inline __gnu_cxx::__promote_fp_t<_Ta>
+    lgamma(_Ta __a)
+    {
+      using __type = __gnu_cxx::__promote_fp_t<_Ta>;
+      return std::__detail::__log_gamma<__type>(__a);
+    }
+
   // Log Gamma function for complex argument.
 
   /**
@@ -2847,6 +2880,38 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
     {
       using __type = std::complex<__gnu_cxx::__promote_fp_t<_Ta>>;
       return std::__detail::__log_gamma<__type>(__a);
+    }
+
+  // Gamma function for real argument.
+
+  /**
+   * Return the gamma function for <tt> float </tt> argument.
+   *
+   * @see lgamma for details.
+   */
+  inline float
+  tgammaf(float __a)
+  { return std::__detail::__gamma<float>(__a); }
+
+  /**
+   * Return the gamma function for <tt> long double </tt>
+   * argument.
+   *
+   * @see lgamma for details.
+   */
+  inline long double
+  tgammal(long double __a)
+  { return std::__detail::__gamma<long double>(__a); }
+
+  /**
+   * Return the gamma function for real argument.
+   */
+  template<typename _Ta>
+    inline __gnu_cxx::__promote_fp_t<_Ta>
+    tgamma(_Ta __a)
+    {
+      using __type = __gnu_cxx::__promote_fp_t<_Ta>;
+      return std::__detail::__gamma<__type>(__a);
     }
 
   // Gamma function for complex argument.
@@ -5689,7 +5754,7 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
    * @f]
    */
   template<typename _Tp>
-    inline __gnu_cxx::__sincos_t<_Tp>
+    inline __gnu_cxx::__sincos_t<__gnu_cxx::__promote_fp_t<_Tp>>
     sincos(_Tp __x)
     {
       using __type = __gnu_cxx::__promote_fp_t<_Tp>;
@@ -5723,11 +5788,341 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
    * @f]
    */
   template<typename _Tp>
-    inline __gnu_cxx::__sincos_t<_Tp>
+    inline __gnu_cxx::__sincos_t<__gnu_cxx::__promote_fp_t<_Tp>>
     sincos_pi(_Tp __x)
     {
       using __type = __gnu_cxx::__promote_fp_t<_Tp>;
       return std::__detail::__sincos_pi<__type>(__x);
+    }
+
+  /**
+   * @brief Return the gamma propability distribution function.
+   *
+   * The formula for the gamma probability density function is:
+   * @f[
+   *    \Gamma(x|\alpha,\beta) = \frac{1}{\beta\Gamma(\alpha)}
+   *                             (x/\beta)^{\alpha - 1} e^{-x/\beta}
+   * @f]
+   */
+  template<typename _Ta, typename _Tb, typename _Tp>
+    inline __gnu_cxx::__promote_fp_t<_Ta, _Tb, _Tp>
+    gamma_pdf(_Ta __alpha, _Tb __beta, _Tp __x)
+    {
+      using __type = __gnu_cxx::__promote_fp_t<_Ta, _Tb, _Tp>;
+      return std::__detail::__gamma_pdf<__type>(__alpha, __beta, __x);
+    }
+
+  /**
+   * @brief Return the gamma cumulative propability distribution function.
+   *
+   * The formula for the gamma probability density function is:
+   * @f[
+   *    \Gamma(x|\alpha,\beta) = \frac{1}{\beta\Gamma(\alpha)}
+   *                             (x/\beta)^{\alpha - 1} e^{-x/\beta}
+   * @f]
+   */
+  template<typename _Ta, typename _Tb, typename _Tp>
+    inline __gnu_cxx::__promote_fp_t<_Ta, _Tb, _Tp>
+    gamma_cdf(_Ta __alpha, _Tb __beta, _Tp __x)
+    {
+      using __type = __gnu_cxx::__promote_fp_t<_Ta, _Tb, _Tp>;
+      return std::__detail::__gamma_cdf<__type>(__alpha, __beta, __x);
+    }
+
+  /**
+   * @brief Return the normal probability density function.
+   *
+   * The formula for the normal probability density function is
+   * @f[
+   *   f(x|\mu,\sigma) = \frac{e^{(x-\mu)^2/2\sigma^2}}{\sigma\sqrt{2\pi}}
+   * @f]
+   */
+  template<typename _Tmu, typename _Tsig, typename _Tp>
+    inline __gnu_cxx::__promote_fp_t<_Tmu, _Tsig, _Tp>
+    normal_pdf(_Tmu __mu, _Tsig __sigma, _Tp __x)
+    {
+      using __type = __gnu_cxx::__promote_fp_t<_Tmu, _Tsig, _Tp>;
+      return std::__detail::__normal_pdf<__type>(__mu, __sigma, __x);
+    }
+
+  /**
+   * @brief Return the normal cumulative probability density function.
+   *
+   * The formula for the normal cumulative probability density function is
+   * @f[
+   *     F(x|\mu,\sigma)
+   *        = \frac{1}{2}\left[ 1-erf(\frac{x-\mu}{\sqrt{2}\sigma}) \right]
+   * @f]
+   */
+  template<typename _Tmu, typename _Tsig, typename _Tp>
+    inline __gnu_cxx::__promote_fp_t<_Tmu, _Tsig, _Tp>
+    normal_cdf(_Tmu __mu, _Tsig __sigma, _Tp __x)
+    {
+      using __type = __gnu_cxx::__promote_fp_t<_Tmu, _Tsig, _Tp>;
+      return std::__detail::__normal_cdf<__type>(__mu, __sigma, __x);
+    }
+
+  /**
+   * @brief Return the lognormal probability density function.
+   *
+   * The formula for the lognormal probability density function is
+   * @f[
+   *   f(x|\mu,\sigma) = \frac{e^{(\ln{x}-\mu)^2/2\sigma^2}}{\sigma\sqrt{2\pi}}
+   * @f]
+   */
+  template<typename _Tmu, typename _Tsig, typename _Tp>
+    inline __gnu_cxx::__promote_fp_t<_Tmu, _Tsig, _Tp>
+    lognormal_pdf(_Tmu __mu, _Tsig __sigma, _Tp __x)
+    {
+      using __type = __gnu_cxx::__promote_fp_t<_Tmu, _Tsig, _Tp>;
+      return std::__detail::__lognormal_pdf<__type>(__mu, __sigma, __x);
+    }
+
+  /**
+   * @brief Return the lognormal cumulative probability density function.
+   *
+   * The formula for the lognormal cumulative probability density function is
+   * @f[
+   *   F(x|\mu,\sigma)
+   *     = \frac{1}{2}\left[ 1-erf(\frac{\ln{x}-\mu}{\sqrt{2}\sigma}) \right]
+   * @f]
+   */
+  template<typename _Tmu, typename _Tsig, typename _Tp>
+    inline __gnu_cxx::__promote_fp_t<_Tmu, _Tsig, _Tp>
+    lognormal_cdf(_Tmu __mu, _Tsig __sigma, _Tp __x)
+    {
+      using __type = __gnu_cxx::__promote_fp_t<_Tmu, _Tsig, _Tp>;
+      return std::__detail::__lognormal_cdf<__type>(__mu, __sigma, __x);
+    }
+
+  /**
+   * @brief Return the exponential probability density function.
+   *
+   * The formula for the exponential probability density function is
+   * @f[
+   *   f(x|\lambda) = \lambda e^{-\lambda x} \mbox{ for } x >= 0
+   * @f]
+   */
+  template<typename _Tlam, typename _Tp>
+    inline __gnu_cxx::__promote_fp_t<_Tlam, _Tp>
+    exponential_pdf(_Tlam __lambda, _Tp __x)
+    {
+      using __type = __gnu_cxx::__promote_fp_t<_Tlam, _Tp>;
+      return std::__detail::__exponential_pdf<__type>(__lambda, __x);
+    }
+
+  /**
+   * @brief Return the exponential cumulative probability density function.
+   *
+   * The formula for the exponential cumulative probability density function is
+   * @f[
+   *   F(x|\lambda) = 1 - e^{-\lambda x} \mbox{ for } x >= 0
+   * @f]
+   */
+  template<typename _Tlam, typename _Tp>
+    inline __gnu_cxx::__promote_fp_t<_Tlam, _Tp>
+    exponential_cdf(_Tlam __lambda, _Tp __x)
+    {
+      using __type = __gnu_cxx::__promote_fp_t<_Tlam, _Tp>;
+      return std::__detail::__exponential_cdf<__type>(__lambda, __x);
+    }
+
+  /**
+   * @brief Return the Weibull probability density function.
+   *
+   * The formula for the Weibull probability density function is
+   * @f[
+   *   f(x | a, b) = \frac{a}{b}
+   *                 \left(\frac{x}{b} \right)^{a-1}
+   *                 \exp{-\left(\frac{x}{b}\right)^a}
+   *                 \mbox{ for } x >= 0
+   * @f]
+   */
+  template<typename _Ta, typename _Tb, typename _Tp>
+    inline __gnu_cxx::__promote_fp_t<_Ta, _Tb, _Tp>
+    weibull_pdf(_Ta __a, _Tb __b, _Tp __x)
+    {
+      using __type = __gnu_cxx::__promote_fp_t<_Ta, _Tb, _Tp>;
+      return std::__detail::__weibull_pdf<__type>(__a, __b, __x);
+    }
+
+  /**
+   * @brief Return the Weibull cumulative probability density function.
+   *
+   * The formula for the Weibull cumulative probability density function is
+   * @f[
+   *   F(x|\lambda) = 1 - e^{-(x / b)^a} \mbox{ for } x >= 0
+   * @f]
+   */
+  template<typename _Ta, typename _Tb, typename _Tp>
+    inline __gnu_cxx::__promote_fp_t<_Ta, _Tb, _Tp>
+    weibull_cdf(_Ta __a, _Tb __b, _Tp __x)
+    {
+      using __type = __gnu_cxx::__promote_fp_t<_Ta, _Tb, _Tp>;
+      return std::__detail::__weibull_cdf<__type>(__a, __b, __x);
+    }
+
+  /**
+   * @brief  Return the Students T probability function.
+   *
+   * The students T propability function is related to the incomplete beta function:
+   * @f[
+   *   A(t|\nu) = 1 - I_{\frac{\nu}{\nu + t^2}}(\frac{\nu}{2}, \frac{1}{2})
+   *   A(t|\nu) = 
+   * @f]
+   *
+   * @param __t 
+   * @param __nu 
+   */
+  template<typename _Tt, typename _Tp>
+    __gnu_cxx::__promote_fp_t<_Tp>
+    student_t_cdf(_Tt __t, unsigned int __nu)
+    {
+      using __type = __gnu_cxx::__promote_fp_t<_Tp>;
+      return std::__detail::__student_t_cdf<__type>(__t, __nu);
+    }
+
+  /**
+   * @brief  Return the complement of the Students T probability function.
+   *
+   * The complement of the students T propability function is:
+   * @f[
+   *   A_c(t|\nu) = I_{\frac{\nu}{\nu + t^2}}(\frac{\nu}{2}, \frac{1}{2})
+   * 		  = 1 - A(t|\nu)
+   * @f]
+   *
+   * @param __t 
+   * @param __nu 
+   */
+  template<typename _Tt, typename _Tp>
+    __gnu_cxx::__promote_fp_t<_Tp>
+    student_t_pdf(_Tt __t, unsigned int __nu)
+    {
+      using __type = __gnu_cxx::__promote_fp_t<_Tp>;
+      return std::__detail::__student_t_pdf<__type>(__t, __nu);
+    }
+
+  /**
+   * @brief  Return the F-distribution propability function.
+   * This returns the probability that the observed chi-square for a correct model
+   * exceeds the value @f$ \chi^2 @f$.
+   *
+   * The f-distribution propability function is related to the incomplete beta function:
+   * @f[
+   *   Q(F|\nu_1, \nu_2) = I_{\frac{\nu_2}{\nu_2 + \nu_1 F}}
+   * 			     (\frac{\nu_2}{2}, \frac{\nu_1}{2})
+   * @f]
+   *
+   * @param __nu1 The number of degrees of freedom of sample 1
+   * @param __nu2 The number of degrees of freedom of sample 2
+   * @param __F The F statistic
+   */
+  template<typename _Tp>
+    __gnu_cxx::__promote_fp_t<_Tp>
+    fisher_f_cdf(_Tp __F, unsigned int __nu1, unsigned int __nu2)
+    {
+      using __type = __gnu_cxx::__promote_fp_t<_Tp>;
+      return std::__detail::__fisher_f_cdf<__type>(__F, __nu1, __nu2);
+    }
+
+  /**
+   * @brief  Return the F-distribution propability function.
+   * This returns the probability that the observed chi-square for a correct model
+   * exceeds the value @f$ \chi^2 @f$.
+   *
+   * The f-distribution propability function is related to the incomplete beta function:
+   * @f[
+   *   P(F|\nu_1, \nu_2) = 1 - I_{\frac{\nu_2}{\nu_2 + \nu_1 F}}
+   * 			     (\frac{\nu_2}{2}, \frac{\nu_1}{2})
+   * 			 = 1 - Q(F|\nu_1, \nu_2)
+   * @f]
+   *
+   * @param __F 
+   * @param __nu1 
+   * @param __nu2 
+   */
+  template<typename _Tp>
+    __gnu_cxx::__promote_fp_t<_Tp>
+    fisher_f_pdf(_Tp __F, unsigned int __nu1, unsigned int __nu2)
+    {
+      using __type = __gnu_cxx::__promote_fp_t<_Tp>;
+      return std::__detail::__fisher_f_pdf<__type>(__F, __nu1, __nu2);
+    }
+
+  /**
+   * @brief  Return the binomial probability mass function.
+   *
+   * The binomial cumulative distribution function is related
+   * to the incomplete beta function:
+   * @f[
+   *   f(k|n,p) = \binom{n}{k}p^k(1-p)^{n-k}
+   * @f]
+   *
+   * @param __p 
+   * @param __n 
+   * @param __k 
+   */
+  template<typename _Tp>
+    __gnu_cxx::__promote_fp_t<_Tp>
+    binomial_pdf(_Tp __p, unsigned int __n, unsigned int __k)
+    {
+      using __type = __gnu_cxx::__promote_fp_t<_Tp>;
+      return std::__detail::__binomial_pdf<__type>(__p, __n, __k);
+    }
+
+  /**
+   * @brief  Return the binomial cumulative distribution function.
+   *
+   * The binomial cumulative distribution function is related
+   * to the incomplete beta function:
+   * @f[
+   *   P(k|n,p) = I_p(k, n-k+1)
+   * @f]
+   *
+   * @param __p 
+   * @param __n 
+   * @param __k 
+   */
+  template<typename _Tp>
+    __gnu_cxx::__promote_fp_t<_Tp>
+    binomial_cdf(_Tp __p, unsigned int __n, unsigned int __k)
+    {
+      using __type = __gnu_cxx::__promote_fp_t<_Tp>;
+      return std::__detail::__binomial_cdf<__type>(__p, __n, __k);
+    }
+
+  /**
+   * @brief  Return the logistic probability density function.
+   *
+   * The formula for the logistic probability density function is
+   * @f[
+   *     f(x| a, b) = \frac{e^{(x - a)/b}}{b[1 + e^{(x - a)/b}]^2}
+   * @f]
+   * where @f$b > 0@f$.
+   */
+  template<typename _Ta, typename _Tb, typename _Tp>
+    inline __gnu_cxx::__promote_fp_t<_Ta, _Tb, _Tp>
+    logistic_pdf(_Ta __a, _Tb __b, _Tp __x)
+    {
+      using __type = __gnu_cxx::__promote_fp_t<_Ta, _Tb, _Tp>;
+      return std::__detail::__logistic_pdf<__type>(__a, __b, __x);
+    }
+
+  /**
+   * @brief  Return the logistic cumulative distribution function.
+   *
+   * The formula for the logistic probability function is
+   * @f[
+   *     P(x| a, b) = \frac{e^{(x - a)/b}}{1 + e^{(x - a)/b}}
+   * @f]
+   * where @f$b > 0@f$.
+   */
+  template<typename _Ta, typename _Tb, typename _Tp>
+    inline __gnu_cxx::__promote_fp_t<_Ta, _Tb, _Tp>
+    logistic_cdf(_Ta __a, _Tb __b, _Tp __x)
+    {
+      using __type = __gnu_cxx::__promote_fp_t<_Ta, _Tb, _Tp>;
+      return std::__detail::__logistic_cdf<__type>(__a, __b, __x);
     }
 
 #endif // __cplusplus >= 201103L
