70e24078d7da7cd048d9faa625b8daa912b1878d
emsr <3dw4rd@verizon.net>
2016-12-12 12:59:20 -0500

    Work on Anger-Weber

--- test_anger_weber.cpp
+++ test_anger_weber.cpp
@@ -9,58 +9,119 @@ $HOME/bin/bin/g++ -std=gnu++17 -g -Wall -Wextra -Wno-psabi -I. -o test_anger_web
 #include <ext/cmath>
 
   template<typename _Tp>
-    struct _AngerWeberState
+    struct __anger_weber_t
     {
       _Tp __nu;
       _Tp __z;
-      _Tp __Jbold;
-      _Tp __Ebold;
+      _Tp __J_value;
+      _Tp __E_value;
     };
 
   template<typename _Tp>
-    _AngerWeberState<_Tp>
+    __anger_weber_t<_Tp>
     __anger_weber_sum(_Tp __nu, _Tp __z)
     {
       //using _Val = _Tp;
       //using _Real = std::__detail::__num_traits_t<_Val>;
       const auto _S_eps = __gnu_cxx::__epsilon(__z);
 
-      const auto __z2 = __z / _Tp{2};
-      auto _GamArg11 = _Tp{1} + __nu / _Tp{2};
-      auto _GamArg12 = _Tp{1} - __nu / _Tp{2};
-      auto _GamArg21 = _Tp{3} / _Tp{2} + __nu / _Tp{2};
-      auto _GamArg22 = _Tp{3} / _Tp{2} - __nu / _Tp{2};
-      auto _Gam11 = std::tgamma(_GamArg11);
-      auto _Gam12 = std::tgamma(_GamArg12);
-      auto _Gam21 = std::tgamma(_GamArg21);
-      auto _Gam22 = std::tgamma(_GamArg22);
-      auto __term1 = _Tp{1} / (_Gam11 * _Gam12);
-      auto _S1 = __term1;
-      auto __term2 = __z2 / (_Gam21 * _Gam22);
-      auto _S2 = __term2;
-      for (auto __k = 1u; __k < 10000u; ++__k)
+      auto __nuint = __gnu_cxx::__fp_is_integer(__nu);
+
+      if (__nu < _Tp{0})
 	{
-	  __term1 *= -__z2 / _GamArg11 * __z2 / _GamArg12;
-	  _S1 += __term1;
-	  _GamArg11 += _Tp{1};
-	  _GamArg12 += _Tp{1};
-
-	  __term2 *= -__z2 / _GamArg21 * __z2 / _GamArg22;
-	  _S2 += __term2;
-	  _GamArg21 += _Tp{1};
-	  _GamArg22 += _Tp{1};
-
-	  if (std::abs(__term1) < _S_eps * std::abs(_S1)
-	   && std::abs(__term2) < _S_eps * std::abs(_S2))
-	    break;
+	  auto __JE = __anger_weber_sum(-__nu, -__z);
+	  __JE.__E_value = -__JE.__E_value;
+	  return __JE;
 	}
-      //auto [__sin, __cos] = __sincos_pi(__nu / _Tp{2});
-      auto __ph = std::__detail::__sincos_pi(__nu / _Tp{2});
-      return _AngerWeberState<_Tp>{__nu, __z,
-				   __ph.__cos_value * _S1
-				 + __ph.__sin_value * _S2,
-				   __ph.__sin_value * _S1
-				 - __ph.__cos_value * _S2};
+      else if (__nuint && __nuint() > 1)
+	{
+	  auto __n = __nuint();
+	  if (__n & 1)
+	    {
+	      const auto __z2 = __z / _Tp{2};
+	      auto _GamArg11 = _Tp{1} + __nu / _Tp{2};
+	      auto _GamArg12 = _Tp{1} - __nu / _Tp{2};
+	      auto _Gam11 = std::tgamma(_GamArg11);
+	      auto _Gam12 = std::tgamma(_GamArg12);
+	      auto __term1 = _Tp{1} / (_Gam11 * _Gam12);
+	      auto _S1 = __term1;
+	      for (auto __k = 1u; __k < 10000u; ++__k)
+		{
+		  __term1 *= -__z2 / _GamArg11 * __z2 / _GamArg12;
+		  _S1 += __term1;
+		  _GamArg11 += _Tp{1};
+		  _GamArg12 += _Tp{1};
+
+		  if (std::abs(__term1) < _S_eps * std::abs(_S1))
+		    break;
+		}
+	      return __anger_weber_t<_Tp>{__nu, __z,
+					  _Tp{0},
+					  -((__n / 2) & 1 ? -1 : +1) * _S1};
+	    }
+	  else
+	    {
+	      const auto __z2 = __z / _Tp{2};
+	      auto _GamArg21 = _Tp{3} / _Tp{2} + __nu / _Tp{2};
+	      auto _GamArg22 = _Tp{3} / _Tp{2} - __nu / _Tp{2};
+	      auto _Gam21 = std::tgamma(_GamArg21);
+	      auto _Gam22 = std::tgamma(_GamArg22);
+	      auto __term2 = __z2 / (_Gam21 * _Gam22);
+	      auto _S2 = __term2;
+	      for (auto __k = 1u; __k < 10000u; ++__k)
+		{
+		  __term2 *= -__z2 / _GamArg21 * __z2 / _GamArg22;
+		  _S2 += __term2;
+		  _GamArg21 += _Tp{1};
+		  _GamArg22 += _Tp{1};
+
+		  if (std::abs(__term2) < _S_eps * std::abs(_S2))
+		    break;
+		}
+	      return __anger_weber_t<_Tp>{__nu, __z,
+					  _Tp{0},
+					 -((__n / 2) & 1 ? -1 : +1) * _S2};
+	    }
+	}
+      else
+	{
+	  const auto __z2 = __z / _Tp{2};
+	  auto _GamArg11 = _Tp{1} + __nu / _Tp{2};
+	  auto _GamArg12 = _Tp{1} - __nu / _Tp{2};
+	  auto _GamArg21 = _Tp{3} / _Tp{2} + __nu / _Tp{2};
+	  auto _GamArg22 = _Tp{3} / _Tp{2} - __nu / _Tp{2};
+	  auto _Gam11 = std::tgamma(_GamArg11);
+	  auto _Gam12 = std::tgamma(_GamArg12);
+	  auto _Gam21 = std::tgamma(_GamArg21);
+	  auto _Gam22 = std::tgamma(_GamArg22);
+	  auto __term1 = _Tp{1} / (_Gam11 * _Gam12);
+	  auto _S1 = __term1;
+	  auto __term2 = __z2 / (_Gam21 * _Gam22);
+	  auto _S2 = __term2;
+	  for (auto __k = 1u; __k < 10000u; ++__k)
+	    {
+	      __term1 *= -__z2 / _GamArg11 * __z2 / _GamArg12;
+	      _S1 += __term1;
+	      _GamArg11 += _Tp{1};
+	      _GamArg12 += _Tp{1};
+
+	      __term2 *= -__z2 / _GamArg21 * __z2 / _GamArg22;
+	      _S2 += __term2;
+	      _GamArg21 += _Tp{1};
+	      _GamArg22 += _Tp{1};
+
+	      if (std::abs(__term1) < _S_eps * std::abs(_S1)
+	       && std::abs(__term2) < _S_eps * std::abs(_S2))
+		break;
+	    }
+	  //auto [__sin, __cos] = __sincos_pi(__nu / _Tp{2});
+	  auto __ph = std::__detail::__sincos_pi(__nu / _Tp{2});
+	  return __anger_weber_t<_Tp>{__nu, __z,
+				      __ph.__cos_value * _S1
+				    + __ph.__sin_value * _S2,
+				      __ph.__sin_value * _S1
+				    - __ph.__cos_value * _S2};
+	}
     }
 
   /**
@@ -70,7 +131,7 @@ $HOME/bin/bin/g++ -std=gnu++17 -g -Wall -Wextra -Wno-psabi -I. -o test_anger_web
    * @see http://dlmf.nist.gov/11.11#i
    */
   template<typename _Tp>
-    _AngerWeberState<_Tp>
+    __anger_weber_t<_Tp>
     __anger_weber_asymp_arg(_Tp __nu, _Tp __z)
     {
       const auto _S_eps = __gnu_cxx::__epsilon(std::real(__z));
@@ -95,15 +156,14 @@ $HOME/bin/bin/g++ -std=gnu++17 -g -Wall -Wextra -Wno-psabi -I. -o test_anger_web
 
       auto __ph = std::__detail::__sincos_pi(__nu / _Tp{2});
       auto _Bess = __cyl_bessel(__nu, __z);
-      return _AngerWeberState<_Tp>{__nu, __z,
-				   _Bess._J_value
-				     + __ph.__sin_value
-					* (__Fsum + __nu * __Gsum / __z)
-					/ _S_pi / __z,
-				  -_Bess._N_value
-				     - (_Tp{1} + __ph.__cos_value) * __Fsum
+      return __anger_weber_t<_Tp>{__nu, __z,
+				  _Bess._J_value
+				    + __ph.__sin_value
+				* (__Fsum + __nu * __Gsum / __z) / _S_pi / __z,
+				 -_Bess._N_value
+				    - (_Tp{1} + __ph.__cos_value) * __Fsum
 					/ _S_pi / __z
-				     - (_Tp{1} - __ph.__cos_value) * __Gsum
+				    - (_Tp{1} - __ph.__cos_value) * __Gsum
 					* __nu / _S_pi / __z / __z};
     }
 
@@ -114,7 +174,7 @@ $HOME/bin/bin/g++ -std=gnu++17 -g -Wall -Wextra -Wno-psabi -I. -o test_anger_web
    * @see http://dlmf.nist.gov/11.11#ii
    */
   template<typename _Tp>
-    _AngerWeberState<_Tp>
+    __anger_weber_t<_Tp>
     __anger_weber_asymp_order(_Tp __nu, _Tp __z)
     {
       const auto _S_pi = __gnu_cxx::__const_pi(std::real(__z));
@@ -122,10 +182,10 @@ $HOME/bin/bin/g++ -std=gnu++17 -g -Wall -Wextra -Wno-psabi -I. -o test_anger_web
       const auto __sinnpd2 = __gnu_cxx::sin_pi(__nu / _Tp{2});
       const auto __cosnpd2 = __gnu_cxx::cos_pi(__nu / _Tp{2});
       const auto __nufact = __nu * __z / (__nu * __nu - _Tp{1});
-      return _AngerWeberState<_Tp>{__nu, __z,
-				   __sinnp * (_Tp{1} - __nufact) / __nu / _S_pi,
-				   _Tp{2} * (__sinnpd2 + __nufact * __cosnpd2)
-					  / __nu / _S_pi};
+      return __anger_weber_t<_Tp>{__nu, __z,
+				  __sinnp * (_Tp{1} - __nufact) / __nu / _S_pi,
+				  _Tp{2} * (__sinnpd2 + __nufact * __cosnpd2)
+					 / __nu / _S_pi};
     }
 
   /**
@@ -135,14 +195,14 @@ $HOME/bin/bin/g++ -std=gnu++17 -g -Wall -Wextra -Wno-psabi -I. -o test_anger_web
    * @see http://dlmf.nist.gov/11.11#iii
    */
   template<typename _Tp>
-    _AngerWeberState<_Tp>
+    __anger_weber_t<_Tp>
     __anger_weber_asymp_uniform(_Tp __nu, _Tp __z)
     {
     }
 
   /* Use the reciprocal gamma function... Fails.  WTF. */
   template<typename _Tp>
-    _AngerWeberState<_Tp>
+    __anger_weber_t<_Tp>
     __anger_weber_sum_recip(_Tp __nu, _Tp __z)
     {
       //using _Val = _Tp;
@@ -180,11 +240,44 @@ $HOME/bin/bin/g++ -std=gnu++17 -g -Wall -Wextra -Wno-psabi -I. -o test_anger_web
 	}
       //auto [__sin, __cos] = __sincos_pi(__nu / _Tp{2});
       auto __ph = std::__detail::__sincos_pi(__nu / _Tp{2});
-      return _AngerWeberState<_Tp>{__nu, __z,
-				   __ph.__cos_value * _S1
-				 + __ph.__sin_value * _S2,
-				   __ph.__sin_value * _S1
-				 - __ph.__cos_value * _S2};
+      return __anger_weber_t<_Tp>{__nu, __z,
+				  __ph.__cos_value * _S1
+				+ __ph.__sin_value * _S2,
+				  __ph.__sin_value * _S1
+				- __ph.__cos_value * _S2};
+    }
+
+  /**
+   * Compute the Anger @f$ {\boldmath J}_\nu(z) @f$
+   * and Weber @f$ {\boldmath E}_\nu(z) @f$ functions
+   * for order @f$ \nu @f$ and agument @f$ z @f$.
+   *
+   * @see http://dlmf.nist.gov/11.11#ii
+   */
+  template<typename _Tp>
+    __anger_weber_t<_Tp>
+    __assoc_anger_weber_asymp(_Tp __nu, _Tp __z)
+    {
+      auto _Bessel = __cyl_bessel(__nu, __z);
+      auto _Weber = __anger_weber(__nu, __z);
+    }
+
+  /**
+   * Compute the associated Anger and Weber function @f$ A_\nu(z) @f$
+   * for order @f$ \nu @f$ and agument @f$ z @f$.
+   * We use the relationship:
+   * @f[
+   *   \boldmath{J}_\nu(z) = J_\nu(z)
+   * @f]
+   *
+   * @see http://dlmf.nist.gov/11.11#ii
+   */
+  template<typename _Tp>
+    __anger_weber_t<_Tp>
+    __assoc_anger_weber(_Tp __nu, _Tp __z)
+    {
+      auto _Bessel = __cyl_bessel(__nu, __z);
+      auto _Weber = __anger_weber(__nu, __z);
     }
 
 
@@ -196,7 +289,10 @@ template<typename _Tp>
     auto width = std::cout.precision() + 8;
     std::cout << std::showpoint << std::scientific;
 
-    for (auto nu : {_Tp{0}, _Tp{0.5Q}, _Tp{1}, _Tp{1.5Q}, _Tp{2}, _Tp{5}})
+    for (auto nu : {_Tp{0}, _Tp{0.5Q}, _Tp{1}, _Tp{1.5Q},
+ _Tp{1.999Q},
+ _Tp{2.999Q},
+ _Tp{2}, _Tp{3}, _Tp{5}})
       {
 	std::cout << "\n\n nu = " << std::setw(4) << nu << '\n';
 	std::cout << ' ' << std::setw(4) << "z"
@@ -212,8 +308,8 @@ template<typename _Tp>
 	    auto z = _Tp{0.1Q} * k;
 	    auto AW = __anger_weber_sum(nu, z);
 	    std::cout << ' ' << std::setw(4) << z
-		      << ' ' << std::setw(width) << AW.__Jbold
-		      << ' ' << std::setw(width) << AW.__Ebold
+		      << ' ' << std::setw(width) << AW.__J_value
+		      << ' ' << std::setw(width) << AW.__E_value
 		      << '\n';
 	  }
       }
