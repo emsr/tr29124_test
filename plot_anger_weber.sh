4a2b25fb0b93fa4cbea1ff0a576640f4ddea9a48
emsr <3dw4rd@verizon.net>
2016-12-12 23:15:30 -0500

    I think I got Anger-Weber for large integer orders.

--- plot_anger_weber.sh
+++ plot_anger_weber.sh
@@ -26,7 +26,21 @@ plot [-8.0:8.0][-1.0:1.0] \
                     "" index 3 using 1:2 with lines title "{/:Bold J}_{3/2}(x)"
 
 plot [-8.0:8.0][-1.0:1.0] \
+"test_anger_weber.txt" index 0 using 1:2 with lines title "{/:Bold J}_0(x)", \
+                    "" index 1 using 1:2 with lines title "{/:Bold J}_{1/2}(x)", \
+                    "" index 2 using 1:2 with lines title "{/:Bold J}_1(x)", \
+                    "" index 3 using 1:2 with lines title "{/:Bold J}_{3/2}(x)", \
+                    "" index 4 using 1:2 with lines title "{/:Bold J}_{1.999}(x)", \
+                    "" index 5 using 1:2 with lines title "{/:Bold J}_{2}(x)", \
+                    "" index 6 using 1:2 with lines title "{/:Bold J}_{2.999}(x)", \
+                    "" index 7 using 1:2 with lines title "{/:Bold J}_{3}(x)"
+
+plot [-8.0:8.0][-1.0:1.0] \
 "test_anger_weber.txt" index 0 using 1:3 with lines title "{/:Bold H}_0(x)", \
                     "" index 1 using 1:3 with lines title "{/:Bold H}_{1/2}(x)", \
                     "" index 2 using 1:3 with lines title "{/:Bold H}_1(x)", \
-                    "" index 3 using 1:3 with lines title "{/:Bold H}_{3/2}(x)"
+                    "" index 3 using 1:3 with lines title "{/:Bold H}_{3/2}(x)", \
+                    "" index 4 using 1:3 with lines title "{/:Bold H}_{1.999}(x)", \
+                    "" index 5 using 1:3 with lines title "{/:Bold H}_{2}(x)", \
+                    "" index 6 using 1:3 with lines title "{/:Bold H}_{2.999}(x)", \
+                    "" index 7 using 1:3 with lines title "{/:Bold H}_{3}(x)"
