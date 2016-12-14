d154f1963c4a0a7d1fdce564311351b0e361e5e0
emsr <3dw4rd@verizon.net>
2016-12-14 11:17:10 -0500

    Finally got integer order Anger-Weber functions correct.

--- plot_anger_weber.sh
+++ plot_anger_weber.sh
@@ -11,13 +11,13 @@ plot [-8.0:8.0][-1.0:1.0] "test_anger_weber.txt" index 0 using 1:3 with lines ti
 
 plot [-8.0:8.0][-1.0:1.0] \
 "test_anger_weber.txt" index 0 using 1:2 with lines title "{/:Bold J}_0(x)", \
-                    "" index 0 using 1:3 with lines title "{/:Bold H}_0(x)", \
+                    "" index 0 using 1:3 with lines title "{/:Bold E}_0(x)", \
                     "" index 1 using 1:2 with lines title "{/:Bold J}_{1/2}(x)", \
-                    "" index 1 using 1:3 with lines title "{/:Bold H}_{1/2}(x)", \
+                    "" index 1 using 1:3 with lines title "{/:Bold E}_{1/2}(x)", \
                     "" index 2 using 1:2 with lines title "{/:Bold J}_1(x)", \
-                    "" index 2 using 1:3 with lines title "{/:Bold H}_1(x)", \
+                    "" index 2 using 1:3 with lines title "{/:Bold E}_1(x)", \
                     "" index 3 using 1:2 with lines title "{/:Bold J}_{3/2}(x)", \
-                    "" index 3 using 1:3 with lines title "{/:Bold H}_{3/2}(x)"
+                    "" index 3 using 1:3 with lines title "{/:Bold E}_{3/2}(x)"
 
 plot [-8.0:8.0][-1.0:1.0] \
 "test_anger_weber.txt" index 0 using 1:2 with lines title "{/:Bold J}_0(x)", \
@@ -30,17 +30,19 @@ plot [-8.0:8.0][-1.0:1.0] \
                     "" index 1 using 1:2 with lines title "{/:Bold J}_{1/2}(x)", \
                     "" index 2 using 1:2 with lines title "{/:Bold J}_1(x)", \
                     "" index 3 using 1:2 with lines title "{/:Bold J}_{3/2}(x)", \
-                    "" index 4 using 1:2 with lines title "{/:Bold J}_{1.999}(x)", \
+                    "" index 4 using 1:2 with lines title "{/:Bold J}_{2-}(x)", \
                     "" index 5 using 1:2 with lines title "{/:Bold J}_{2}(x)", \
-                    "" index 6 using 1:2 with lines title "{/:Bold J}_{2.999}(x)", \
-                    "" index 7 using 1:2 with lines title "{/:Bold J}_{3}(x)"
+                    "" index 6 using 1:2 with lines title "{/:Bold J}_{3-}(x)", \
+                    "" index 7 using 1:2 with lines title "{/:Bold J}_{3}(x)", \
+                    "" index 8 using 1:2 with lines title "{/:Bold J}_{5}(x)"
 
 plot [-8.0:8.0][-1.0:1.0] \
-"test_anger_weber.txt" index 0 using 1:3 with lines title "{/:Bold H}_0(x)", \
-                    "" index 1 using 1:3 with lines title "{/:Bold H}_{1/2}(x)", \
-                    "" index 2 using 1:3 with lines title "{/:Bold H}_1(x)", \
-                    "" index 3 using 1:3 with lines title "{/:Bold H}_{3/2}(x)", \
-                    "" index 4 using 1:3 with lines title "{/:Bold H}_{1.999}(x)", \
-                    "" index 5 using 1:3 with lines title "{/:Bold H}_{2}(x)", \
-                    "" index 6 using 1:3 with lines title "{/:Bold H}_{2.999}(x)", \
-                    "" index 7 using 1:3 with lines title "{/:Bold H}_{3}(x)"
+"test_anger_weber.txt" index 0 using 1:3 with lines title "{/:Bold E}_0(x)", \
+                    "" index 1 using 1:3 with lines title "{/:Bold E}_{1/2}(x)", \
+                    "" index 2 using 1:3 with lines title "{/:Bold E}_1(x)", \
+                    "" index 3 using 1:3 with lines title "{/:Bold E}_{3/2}(x)", \
+                    "" index 4 using 1:3 with lines title "{/:Bold E}_{1.999}(x)", \
+                    "" index 5 using 1:3 with lines title "{/:Bold E}_{2}(x)", \
+                    "" index 6 using 1:3 with lines title "{/:Bold E}_{2.999}(x)", \
+                    "" index 7 using 1:3 with lines title "{/:Bold E}_{3}(x)", \
+                    "" index 8 using 1:3 with lines title "{/:Bold E}_{5}(x)"
