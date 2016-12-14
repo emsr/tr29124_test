a619af1ba766f6541ddcd847b39fbea8437487e0
emsr <3dw4rd@verizon.net>
2016-12-14 14:13:08 -0500

    Last representable integer: __max_integer.

--- bits/numeric_limits_float128.h
+++ bits/numeric_limits_float128.h
@@ -216,6 +216,11 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
 
   template<>
     inline __float128
+    __max_integer(__float128) _GLIBCXX_USE_NOEXCEPT
+    { return std::ldexp(1, __digits(__float128{}));
+
+  template<>
+    inline __float128
     __sqrt_max<__float128>(__float128) _GLIBCXX_USE_NOEXCEPT
     { return std::sqrt(__max(__float128{})); }
 
