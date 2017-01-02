95c7a06736ce316a47ec6cb7c85e8191e3af94a5
emsr <3dw4rd@verizon.net>
2017-01-02 15:42:09 -0500

    Cycle Copyright date.

---
+++ quadrature/integration_workspace_new.h
@@ -0,0 +1,253 @@
+// -*- C++ -*-
+// Integration utilities for the C++ library testsuite.
+//
+// Copyright (C) 2011-2017 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+//
+// Ported from GSL by Jason Dick
+// Originally written by Brian Gaugh
+//
+// Implements the integration_workspace class which stores temporary data
+// for performing integrals
+// Based upon gsl-2.3/integration/workspace.c
+
+#ifndef INTEGRATION_WORKSPACE_H
+#define INTEGRATION_WORKSPACE_H 1
+
+#include <vector>
+#include <limits>
+#include <cmath>
+
+namespace __gnu_test
+{
+
+  template<typename _Tp>
+    class integration_workspace
+    {
+    public:
+
+      struct interval
+      {
+	_Tp _M_lower_lim;
+	_Tp _M_upper_lim;
+	_Tp _M_result;
+	_Tp _M_abs_error;
+	std::size_t _M_level;
+      }
+
+      integration_workspace(std::size_t __cap)
+      : _M_capacity(__cap),
+	_M_size(0),
+	_M_nrmax(0),
+	_M_current_index(0),
+	_M_maximum_level(0),
+	_M_interval(__cap)
+      { }
+
+      void
+      set_initial_limits(_Tp a0, _Tp b0)
+      {
+	if (this->_M_capacity > 0)
+	  {
+	    this->_M_size = 0;
+	    this->_M_nrmax = 0;
+	    this->_M_current_index = 0;
+	    this->_M_interval[0]._M_lower_lim = a0;
+	    this->_M_interval[0]._M_upper_lim = b0;
+	    this->_M_interval[0]._M_result = 0.0;
+	    this->_M_interval[0]._M_abs_error = 0.0;
+	    this->_M_interval[0]._M_level = 0;
+	    this->_M_maximum_level = 0;
+	  }
+      }
+
+      bool
+      interval_order(const interval& __ivl, const interval& __ivr)
+      { return __ivl._M_abs_error < __ivr._M_abs_error; }
+
+      void
+      set_initial_results(_Tp result, _Tp error)
+      {
+	if (this->_M_capacity > 0)
+	  {
+	    this->_M_size = 1;
+	    this->_M_interval[0]._M_result = result;
+	    this->_M_interval[0]._M_abs_error = error;
+	  }
+      }
+
+      void
+      set_initial(_Tp __a0, _Tp __b0, _Tp __result0, _Tp __error0)
+      {
+	this->_M_interval[0]._M_lower_lim = __a0;
+	this->_M_interval[0]._M_upper_lim = __b0;
+	this->_M_interval[0]._M_result = __result0;
+	this->_M_interval[0]._M_abs_error = __error0;
+	this->_M_interval[0]._M_level = 0;
+	this->_M_size = 1;
+      }
+
+      //void sort_error();
+      //void sort_results();
+
+      void append(_Tp __a, _Tp __b, _Tp __area, _Tp __error);
+
+      void update(_Tp __a1, _Tp __b1, _Tp __area1, _Tp __error1,
+		  _Tp __a2, _Tp __b2, _Tp __area2, _Tp __error2);
+
+      interval
+      retrieve() const
+      {
+	const auto __ii = this->_M_current_index;
+	return this->_M_interval[__ii];
+      }
+
+      std::size_t
+      size() const
+      { return this->_M_size; }
+
+      std::size_t
+      capacity() const
+      { return this->_M_capacity; }
+
+      void
+      clear()
+      { this->_M_interval.clear(); }
+
+      std::size_t
+      current_index() const
+      { return this->_M_current_index; }
+/*
+      _Tp
+      lower_lim(std::size_t __ii) const
+      { return this->_M_interval[__ii]._M_lower_lim; }
+
+      _Tp
+      upper_lim(std::size_t __ii) const
+      { return this->_M_interval[__ii]._M_upper_lim; }
+
+      _Tp
+      result(std::size_t __ii) const
+      { return this->_M_interval[__ii]._M_result; }
+
+      _Tp
+      abs_error(std::size_t __ii) const
+      { return this->_M_interval[__ii]._M_abs_error; }
+*/
+      _Tp
+      set_abs_error(std::size_t __ii, _Tp __abserr)
+      { return this->_M_interval[__ii]._M_abs_error = __abserr; }
+
+      size_t
+      level(std::size_t ii) const
+      { return this->_M_interval[ii]._M_level; }
+
+      void
+      set_level(std::size_t __ii, std::size_t __lvl)
+      { this->_M_interval[__ii]._M_level = __lvl; }
+
+      std::size_t
+      current_level() const
+      { return this->_M_level[this->_M_current_index]; }
+
+      std::size_t
+      max_level() const
+      { return this->_M_maximum_level; }
+
+      bool
+      large_interval() const
+      {
+	if (this->_M_level[this->_M_current_index] < this->_M_maximum_level)
+	  return true;
+	else
+	  return false;
+      }
+/*
+      bool
+      increase_nrmax()
+      {
+	int __id = this->_M_nrmax;
+	int __jupbnd;
+
+	std::size_t __last = this->_M_size - 1;
+
+	if (__last > (1 + this->_M_capacity / 2))
+	  __jupbnd = this->_M_capacity + 1 - __last;
+	else
+	  __jupbnd = __last;
+
+	for (int __k = __id; __k <= __jupbnd; ++__k)
+	  {
+	    std::size_t __i_max = this->_M_order[this->_M_nrmax];
+
+	    this->_M_current_index = __i_max;
+
+	    if (this->_M_interval[__i_max]._M_level < this->_M_maximum_level)
+	      return true;
+
+	    ++this->_M_nrmax;
+	  }
+	return false;
+      }
+      void
+      reset_nrmax()
+      {
+	this->_M_nrmax = 0;
+	this->_M_current_index = this->_M_interval[0]._M_order;
+      }
+
+      void
+      set_nrmax(std::size_t nrmax)
+      { this->_M_nrmax = nrmax; }
+*/
+
+      _Tp
+      sum_results() const
+      {
+	auto __result_sum = _Tp{0};
+	for (std::size_t __kk = 0; __kk < this->_M_size; ++__kk)
+	  __result_sum += this->_M_interval[__kk]._M_result;
+
+	return __result_sum;
+      }
+
+      static bool
+      subinterval_too_small(_Tp __a1, _Tp __a2, _Tp __b2)
+      {
+	const _Tp __e = std::numeric_limits<_Tp>::epsilon();
+	const _Tp __u = std::numeric_limits<_Tp>::min();
+
+	_Tp __tmp = (1 + 100 * __e) * (std::abs(__a2) + 1000 * __u);
+
+	return std::abs(__a1) <= __tmp && std::abs(__b2) <= __tmp;
+      }
+
+    private:
+
+      std::size_t _M_capacity;
+      std::size_t _M_size;
+      std::size_t _M_nrmax;
+      std::size_t _M_current_index;
+      std::size_t _M_maximum_level;
+      std::vector<interval> _M_interval;
+    };
+
+} // namespace __gnu_test
+
+#include "integration_workspace.tcc"
+
+#endif // INTEGRATION_WORKSPACE_H
