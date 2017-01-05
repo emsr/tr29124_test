56ce01d2a93d7729fa15c71130db99bb3694e902
emsr <3dw4rd@verizon.net>
2017-01-05 11:12:19 -0500

    New Gauss-Laguerre and tester.

---
+++ quadrature/gauss_laguerre_integrate.h
@@ -0,0 +1,53 @@
+// -*- C++ -*-
+// Integration utilities for the C++ library testsuite.
+//
+// Copyright (C) 2011-2016 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#ifndef GAUSS_LAGUERRE_INTEGRATE_H
+#define GAUSS_LAGUERRE_INTEGRATE_H 1
+
+#include <vector>
+
+namespace __gnu_test
+{
+
+template<typename _Tp, typename _FuncTp>
+  _Tp
+  gauss_laguerre_integrate(const _FuncTp& __func,
+			   unsigned int __n, _Tp __alpha)
+  {
+    if(__n == 0)
+      std::__throw_domain_error("gauss_laguerre_integrate: "
+    				"laguerre order must be greater than 0");
+    else
+     {
+	auto __rule = std::__detail::__laguerre_zeros(__n, __alpha);
+	auto __sum = _Tp{0};
+	for (const auto& __pt : __rule)
+	  {
+	    auto __x = __pt.__zero;
+	    auto __w = __pt.__weight;
+	    __sum += __w * __func(__x);
+	  }
+	return __sum;
+      }
+  }
+
+} // namespace __gnu_test
+
+#endif // GAUSS_LAGUERRE_INTEGRATE_H
---
+++ quadrature/test_gauss_laguerre.cpp
@@ -0,0 +1,52 @@
+/*
+$HOME/bin/bin/g++ -std=gnu++17 -g -Wall -Wextra -I.. -o test_gauss_laguerre test_gauss_laguerre.cpp
+*/
+
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+
+#include "gauss_laguerre_integrate.h"
+
+template<typename _Tp>
+  struct my_f
+  {
+    _Tp a;
+    _Tp b;
+    _Tp c;
+    _Tp d;
+    _Tp e;
+
+    _Tp
+    operator()(_Tp x)
+    { return (((a * x + b) * x + c ) * x + d ) * x + e; }
+  };
+
+template<typename _Tp>
+  struct my_fc
+  {
+    _Tp a;
+    _Tp b;
+    _Tp c;
+
+    _Tp
+    operator()(_Tp x)
+    { return a * std::cos(b * x + c); }
+  };
+
+void
+test_laguerre_quad()
+{
+  my_f<double> params{ 0.5, 0.0, 0.0, -1.0, 1.3 };
+  my_fc<double> cparams{ 1.0, 1.0, 0.0 };
+
+  //pfoo2 = __gnu_test::gauss_laguerre_prob_integrate<double>(params, 3);
+  //pfooc = __gnu_test::gauss_laguerre_prob_integrate<double>(cparams, 29);
+
+  auto xfoo2 = __gnu_test::gauss_laguerre_integrate<double>(params, 3, 0.0);
+  std::cout << "xfoo2 = " << xfoo2 << '\n';
+  std::cout << "delta = " << xfoo2 - 0.0L << '\n';
+  auto xfooc = __gnu_test::gauss_laguerre_integrate<double>(cparams, 29, 0.0);
+  std::cout << "xfooc = " << xfooc << '\n';
+  std::cout << "delta = " << xfooc - 0.0L << '\n';
+}
