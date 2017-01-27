808d24b37ab2a5d0731dd5ca3d7c7a48b63f67d1
emsr <3dw4rd@verizon.net>
2017-01-27 12:08:04 -0500

    Add new gamma raio methods.

--- test_gamma_ratio.cpp
+++ test_gamma_ratio.cpp
@@ -41,7 +41,8 @@ $HOME/bin_tr29124/bin/g++ -std=gnu++17 -g -Wall -Wextra -Wno-psabi -I. -o test_g
 			  int _S_M = 20, __buhring_mode mode = automatic)
     {
       const auto _S_eps = 10 * __gnu_cxx::__epsilon(__a);
-      if (mode == equation2p7 || (mode == automatic && std::real(1 - __c - __n) < _Tp{0}))
+      if (mode == equation2p7
+	|| (mode == automatic && std::real(1 - __c - __n) < _Tp{0}))
 	{
 	  //__gnu_cxx::_WenigerDeltaSum<__gnu_cxx::_BasicSum<_Tp>> __sum;
 	  __gnu_cxx::_BasicSum<_Tp> __sum;
@@ -55,9 +56,9 @@ $HOME/bin_tr29124/bin/g++ -std=gnu++17 -g -Wall -Wextra -Wno-psabi -I. -o test_g
 	      auto __prev = __term;
 	      __term *= __ca / __m * __cb / __cabn;
 	      __sum += __term;
-	      if (mode == automatic && std::abs(__term) < _S_eps * __sum())
-		break;
-	      if (mode == automatic && std::abs(__term) > std::abs(__prev))
+	      if (mode == automatic
+		&& (std::abs(__term) < _S_eps * __sum()
+		 || std::abs(__term) > std::abs(__prev)))
 		break;
 	      ++__ca;
 	      ++__cb;
@@ -65,7 +66,8 @@ $HOME/bin_tr29124/bin/g++ -std=gnu++17 -g -Wall -Wextra -Wno-psabi -I. -o test_g
 	    }
 	  return __sum();
 	}
-      else if (mode == equation3p1 || (mode == automatic && std::real(1 + __c - __a - __b - __n) < _Tp{0}))
+      else if (mode == equation3p1
+	|| (mode == automatic && std::real(1 + __c - __a - __b - __n) < _Tp{0}))
 	{
 	  //__gnu_cxx::_WenigerDeltaSum<__gnu_cxx::_BasicSum<_Tp>> __sum;
 	  __gnu_cxx::_BasicSum<_Tp> __sum;
@@ -79,7 +81,9 @@ $HOME/bin_tr29124/bin/g++ -std=gnu++17 -g -Wall -Wextra -Wno-psabi -I. -o test_g
 	      auto __prev = __term;
 	      __term *= __ac / __m * __bc / __cn;
 	      __sum += __term;
-	      if (mode == automatic && std::abs(__term) < _S_eps * __sum())
+	      if (mode == automatic
+		&& (std::abs(__term) < _S_eps * __sum()
+		 || std::abs(__term) > std::abs(__prev)))
 		break;
 	      if (mode == automatic && std::abs(__term) > std::abs(__prev))
 		break;
@@ -90,7 +94,80 @@ $HOME/bin_tr29124/bin/g++ -std=gnu++17 -g -Wall -Wextra -Wno-psabi -I. -o test_g
 	  return __sum();
 	}
       else
-	return __gnu_cxx::__infinity(__a);
+	{
+	  // Average 2.7 and 3.1 sums.
+	  __gnu_cxx::_BasicSum<_Tp> __sum2p7;
+	  auto __term2p7 = _Tp{1};
+	  __sum2p7 += _Tp{__term2p7};
+	  auto __ca = __c - __a;
+	  auto __cb = __c - __b;
+	  auto __cabn = _Tp{1} + __c - __a - __b - __n;
+
+	  __gnu_cxx::_BasicSum<_Tp> __sum3p1;
+	  auto __term3p1 = _Tp{1};
+	  __sum3p1 += _Tp{__term3p1};
+	  auto __ac = __a - __c;
+	  auto __bc = __b - __c;
+	  auto __cn = _Tp{1} - __c - __n;
+
+	  bool __conv2p7 = false;
+	  bool __conv3p1 = false;
+	  for (int __m = 1; __m <= _S_M; ++__m)
+	    {
+	      auto __prev2p7 = __term2p7;
+	      __term2p7 *= __ca / __m * __cb / __cabn;
+	      __sum2p7 += __term2p7;
+	      ++__ca;
+	      ++__cb;
+	      ++__cabn;
+	      if (std::abs(__term2p7) < _S_eps * __sum2p7()
+		|| std::abs(__term2p7) > std::abs(__prev2p7))
+		__conv2p7 = true;
+
+	      auto __prev3p1 = __term3p1;
+	      __term3p1 *= __ac / __m * __bc / __cn;
+	      __sum3p1 += __term3p1;
+	      ++__ac;
+	      ++__bc;
+	      ++__cn;
+	      if (std::abs(__term3p1) < _S_eps * __sum3p1()
+		|| std::abs(__term3p1) > std::abs(__prev3p1))
+		__conv3p1 = true;
+
+	      if (__conv2p7 && __conv3p1)
+		break;
+	    }
+	  return (__sum2p7() + __sum3p1()) / _Tp{2}
+		* __gnu_cxx::sin_pi(__c + __n) * __gnu_cxx::sin_pi(__a + __b - __c + __n)
+		/ __gnu_cxx::sin_pi(__a + __n) * __gnu_cxx::sin_pi(__b + __n);
+	}
+    }
+
+  /**
+   * Juggle gamma special cases - from hyperg.
+   */
+  template<typename _Tp>
+    _Tp
+    __hyperg_reflect(_Tp __a, _Tp __b, _Tp __c)
+    {
+      const auto __d = __c - __a - __b;
+      const auto __intd = __gnu_cxx::__fp_is_integer(__d);
+      auto __F1 = _Tp{0};
+      if (__intd)
+	{
+	  if (__intd() <= 0)
+	    {
+	      __F1 = _Tp{0};
+	    }
+	  else
+	    {
+	      __F1 = _Tp{0};
+	    }
+	}
+      else
+	{
+	  __F1 = _Tp{0};
+	}
     }
 
 /**
@@ -135,9 +212,38 @@ template<typename _Tp>
     //using _Val = _Tp;
     //using _Real = std::__detail::__num_traits_t<_Val>;
 
+    std::vector<_Tp> parm{_Tp{0.25}, _Tp{0.5}, _Tp{1}, _Tp{2}, _Tp{5}};
+
     std::cout.precision(__gnu_cxx::__digits10(proto));
     std::cout << std::showpoint << std::scientific;
     auto width = 8 + std::cout.precision();
+
+    std::cout << '\n'
+	      << ' ' << std::setw(width) << "a"
+	      << ' ' << std::setw(width) << "b"
+	      << ' ' << std::setw(width) << "c"
+	      << ' ' << std::setw(width) << "z"
+	      << ' ' << std::setw(width) << "hyperg0"
+	      << ' ' << std::setw(width) << "hyperg"
+	      << '\n';
+    int i_min = -200;
+    for (auto a : parm)
+      for (auto b : parm)
+	for (auto c : parm)
+	  for (int i = i_min; i <= +200; ++i)
+	    {
+	      auto n = _Tp{0.1Q} * i;
+	      //auto gamrat0 = __gamma_ratio_log(a, b, c, z);
+	      auto gamrat = __gamma_ratio_buhring(n, a, b, c);
+	      std::cout << ' ' << std::setw(width) << n
+			<< ' ' << std::setw(width) << a
+			<< ' ' << std::setw(width) << b
+			<< ' ' << std::setw(width) << c
+			//<< ' ' << std::setw(width) << gamrat0
+			<< ' ' << std::setw(width) << gamrat
+			//<< ' ' << std::setw(width) << gamrat - gamrat0
+			<< '\n';
+	    }
   }
 
 template<typename _Tp>
