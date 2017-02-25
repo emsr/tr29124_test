a0d52fa2eec4d3b2398693a10b563d9f64a73c4a
emsr <3dw4rd@verizon.net>
2017-02-24 22:22:41 -0500

    Comment out unused return value.

--- wrap_lerch.cpp
+++ wrap_lerch.cpp
@@ -12,7 +12,8 @@ lerch(double z, double s, double a)
   const double acc = 10.0 * std::numeric_limits<double>::epsilon();
   double phi = 0.0;
   int iter = 0;
-  auto ok = lerchphi(&z, &s, &a, &acc, &phi, &iter);
+  /*auto ok = */
+  lerchphi(&z, &s, &a, &acc, &phi, &iter);
 
   return phi;
 }
