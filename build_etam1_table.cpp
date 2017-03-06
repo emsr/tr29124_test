cefa0507e7c307e410aa2f7335be8bedcef8b4e4
emsr <3dw4rd@verizon.net>
2017-03-06 17:26:39 -0500

    eta-1 table

---
+++ build_etam1_table.cpp
@@ -0,0 +1,23 @@
+/*
+$HOME/bin_tr29124/bin/g++ -std=gnu++17 -g -Wall -Wextra -Wno-psabi -I. -o build_eta_table build_eta_table.cpp -lquadmath -lmpfr
+LD_LIBRARY_PATH=$HOME/bin_tr29124/lib64:$LD_LIBRARY_PATH ./build_eta_table > build_eta_table.txt
+
+$HOME/bin/bin/g++ -std=gnu++17 -g -Wall -Wextra -Wno-psabi -I. -o build_eta_table build_eta_table.cpp -lquadmath -lmpfr
+*/
+
+#include <mpreal.h>
+#include "bits/numeric_limits_mpreal.h"
+
+int
+main()
+{
+  mpfr::mpreal p(0, 128);
+  std::cout.precision(__gnu_cxx::__max_digits10(p));
+  auto w = 8 + std::cout.precision();
+
+  for (int i = 0; i <= 120; ++i)
+    {
+      auto x = mpfr::mpreal(i, 128);
+      std::cout << "  " << std::setw(w) << (1 - mpfr::pow(2, 1 - x)) * mpfr::zeta(x) - 1 << '\n';
+    }
+}
