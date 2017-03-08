46cd09f15cd80d4c60ce76ecc0e1e898647e4245
emsr <3dw4rd@verizon.net>
2017-03-08 00:52:01 -0500

    Add an AGM toy.  Ship it soon.

---
+++ test_agm.cpp
@@ -0,0 +1,58 @@
+/*
+$HOME/bin_tr29124/bin/g++ -std=gnu++17 -g -Wall -Wextra -I. -o test_agm test_agm.cpp -lquadmath -Lwrappers/debug -lwrap_boost -lwrap_gsl
+LD_LIBRARY_PATH=wrappers/debug:$LD_LIBRARY_PATH ./test_agm > test_agm.txt
+
+$HOME/bin/bin/g++ -std=gnu++17 -g -Wall -Wextra -I. -o test_agm test_agm.cpp -lquadmath -Lwrappers/debug -lwrap_boost -lwrap_gsl
+PATH=wrappers/debug:$PATH ./test_agm > test_agm.txt
+*/
+
+#include <wrap_boost.h>
+#include <wrap_gsl.h>
+#include <bits/numeric_limits.h>
+
+  template<typename _Tp>
+    _Tp
+    __agm(_Tp __x, _Tp __y)
+    {
+      if (__x == _Tp{0} || __y == _Tp{0})
+	return _Tp{0};
+      else
+        {
+	  const auto _S_eps = __gnu_cxx::__epsilon(__x) / _Tp{2};
+	  while (true)
+  	    {
+	      __y = std::sqrt(__y * std::exchange(__x, (__x + __y) / _Tp{2}));
+	      if (std::abs(__x - __y) < _S_eps * (__x + __y))
+		break;
+	    }
+	  return (__x + __y) / _Tp{2};
+	}
+    }
+
+template<typename _Tp>
+  void
+  test_agm(_Tp __proto = _Tp{})
+  {
+    std::cout.precision(__gnu_cxx::__digits10(__proto));
+    auto w = 8 + std::cout.precision();
+
+    for (int i = 0; i < 100; ++i)
+      {
+	auto x = i * _Tp{1};
+	for (int j = 0; j < 100; ++j)
+	  {
+	    auto y = j * _Tp{1};
+	    auto agm = __agm(x, y);
+	    std::cout << ' ' << x
+		      << ' ' << y
+		      << ' ' << std::setw(w) << agm << '\n';
+	  }
+      }
+  }
+
+int
+main()
+{
+  test_agm(1.0);
+}
+
