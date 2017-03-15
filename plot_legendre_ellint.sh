d7b948f0b93a134058d5d5b838f2f035e4887f65
emsr <3dw4rd@verizon.net>
2017-03-15 18:54:40 -0400

    Tweak new plotters.

--- plot_legendre_ellint.sh
+++ plot_legendre_ellint.sh
@@ -9,12 +9,32 @@ set grid
 
 set title "Complete elliptic function of the first kind K(k)"
 set xlabel "x"
-plot [0.0:1.0][0.0:4.0] \
+plot [0.0:1.0][1.5:3.5] \
                     "test_legendre_ellint.txt" index 0 using 1:2 with lines title "K(k)", \
                     "" index 0 using 1:3 with lines title "K(k) GSL"
 
-set title "Complete elliptic function of the first kind K(k)"
+set title "Incomplete elliptic function of the first kind F(k,{/Symbol f})"
+set xlabel "{/Symbol f}"
+plot [0.0:1.6][0.0:3.5] \
+                    "test_legendre_ellint.txt" index 1 using 1:2 with lines title "F(0.0,{/Symbol f})", \
+                    "" index 1 using 1:5 with lines title "F(0.5,{/Symbol f})", \
+                    "" index 1 using 1:8 with lines title "F(0.75,{/Symbol f})", \
+                    "" index 1 using 1:11 with lines title "F(0.9,{/Symbol f})", \
+                    "" index 1 using 1:14 with lines title "F(0.95,{/Symbol f})", \
+                    "" index 1 using 1:17 with lines title "F(0.99,{/Symbol f})"
+
+set title "Complete elliptic function of the second kind E(k)"
 set xlabel "x"
-plot [0.0:1.0][0.0:4.0] \
-                    "test_legendre_ellint.txt" index 1 using 1:2 with lines title "K(k)", \
-                    "" index 1 using 1:3 with lines title "K(k) GSL"
+plot [0.0:1.0][1.0:1.6] \
+                    "test_legendre_ellint.txt" index 2 using 1:2 with lines title "E(k)", \
+                    "" index 2 using 2:3 with lines title "E(k) GSL"
+
+set title "Incomplete elliptic function of the second kind E(k,{/Symbol f})"
+set xlabel "{/Symbol f}"
+plot [0.0:1.6][0.0:1.6] \
+                    "test_legendre_ellint.txt" index 3 using 1:2 with lines title "E(0.0,{/Symbol f})", \
+                    "" index 3 using 1:5 with lines title "E(0.5,{/Symbol f})", \
+                    "" index 3 using 1:8 with lines title "E(0.75,{/Symbol f})", \
+                    "" index 3 using 1:11 with lines title "E(0.9,{/Symbol f})", \
+                    "" index 3 using 1:14 with lines title "E(0.95,{/Symbol f})", \
+                    "" index 3 using 1:17 with lines title "E(0.99,{/Symbol f})"
--- plot_psi.sh
+++ plot_psi.sh
@@ -16,7 +16,7 @@ plot [-10.0:10.0][-10.0:10.0] \
 # The harmonic numbers don't plot :-(
 set title "Digamma {/Symbol y}(x)"
 set xlabel "x"
-plot [0.0:50.0][0.0:10.0] \
+plot [0.0:50.0][0.0:4.0] \
                     "test_psi.txt" index 1 using 1:2 with lines title "{/Symbol y}(x)", \
                     "" index 1 using 1:3 with lines title "{/Symbol y}(x) GSL", \
                     "" index 1 using 1:4 with lines title "H_n"
