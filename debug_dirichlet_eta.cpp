fdff8c2cb3b61e89f74d15d0d6079825d6526072
emsr <3dw4rd@verizon.net>
2017-03-22 00:45:19 -0400

    Get dirichlet eta between 0 and 1 figured out.

---
+++ debug_dirichlet_eta.cpp
@@ -0,0 +1,22 @@
+/*
+$HOME/bin_specfun/bin/g++ -std=gnu++17 -g -Wall -Wextra -Wno-psabi -I. -Izeta -o debug_dirichlet_eta debug_dirichlet_eta.cpp -lquadmath -Lwrappers/debug -lwrap_cephes
+LD_LIBRARY_PATH=wrappers/debug:$LD_LIBRARY_PATH ./debug_dirichlet_eta > debug_dirichlet_eta.txt
+
+LD_LIBRARY_PATH=wrappers/debug:$LD_LIBRARY_PATH $HOME/bin_specfun/bin/g++ -std=gnu++17 -g -Wall -Wextra -Wno-psabi -I. -o debug_dirichlet_eta debug_dirichlet_eta.cpp -lquadmath -Lwrappers/debug -lwrap_cephes
+
+$HOME/bin/bin/g++ -std=gnu++17 -g -Wall -Wextra -Wno-psabi -I. -Izeta -o debug_dirichlet_eta debug_dirichlet_eta.cpp -lquadmath -Lwrappers/debug -lwrap_cephes
+PATH=wrappers/debug:$PATH ./debug_dirichlet_eta > debug_dirichlet_eta.txt
+*/
+
+#include <ext/cmath>
+#include <iostream>
+
+int
+main()
+{
+  std::cout.precision(__gnu_cxx::__digits10(1.0));
+  std::cout << std::scientific;
+
+  double s = 0.5;
+  std::cout << __gnu_cxx::dirichlet_eta(s) << '\n';
+}
