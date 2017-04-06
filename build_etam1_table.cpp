baa7951fedf0bf8d75aa9ca0fc77c2414d4f7ddf
emsr <3dw4rd@verizon.net>
2017-04-06 11:43:27 -0400

    Fix build_etam1_table. Add build_zeta_trig_tables.

--- build_etam1_table.cpp
+++ build_etam1_table.cpp
@@ -19,7 +19,7 @@ main()
   for (int i = 0; i <= 120; ++i)
     {
       auto x = mpfr::mpreal(i, prec);
-      auto pow2 = mpfr::pow(mpfr::mpreal(2, prec), 1 - x)
+      auto pow2 = mpfr::pow(mpfr::mpreal(2, prec), 1 - x);
       std::cout << "  " << std::setw(w) << (1 - pow2) * mpfr::zeta(x) - 1 << '\n';
     }
 }
