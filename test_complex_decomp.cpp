dd8fcbb5ecf7d9e21a31ce043d41f2eee17f6d13
emsr <3dw4rd@verizon.net>
2017-04-13 18:12:37 -0400

    Start a paper for complex decomp.

--- test_complex_decomp.cpp
+++ test_complex_decomp.cpp
@@ -18,21 +18,27 @@ template<typename Tp>
     std::complex<Tp> z(1, 2);
 
     auto [x, y] = z;
-    std::cout << z << '\n';
-    std::cout << ' ' << x << ' ' << y << '\n';
+    std::cout << "z = " << z << '\n';
+    std::cout << "z parts:" << ' ' << x << ' ' << y << '\n';
 
     z.real(3);
     z.imag(4);
-    std::cout << z << '\n';
-    std::cout << ' ' << x << ' ' << y << '\n';
+    std::cout << "z = " << z << '\n';
+    std::cout << "z parts:" << ' ' << x << ' ' << y << '\n';
 
     std::complex<Tp> w(1, 2);
+    std::cout << "w = " << w << '\n';
     auto& [p, q] = w;
 
     w.real(3);
     w.imag(4);
-    std::cout << w << '\n';
-    std::cout << ' ' << p << ' ' << q << '\n';
+    std::cout << "w = " << w << '\n';
+    std::cout << "w parts:" << ' ' << p << ' ' << q << '\n';
+
+    p = -5;
+    q = -6;
+    std::cout << "w = " << w << '\n';
+    std::cout << "w parts:" << ' ' << p << ' ' << q << '\n';
 
     return;
   }
