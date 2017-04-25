aff11223825b3f99e1000f00b02282901d261325
emsr <3dw4rd@verizon.net>
2017-04-25 17:34:06 -0400

    Refine power mean with input checks.

--- test_power_mean.cpp
+++ test_power_mean.cpp
@@ -10,6 +10,7 @@ $HOME/bin/bin/g++ -std=c++17 -g -Wall -Wextra -Wno-psabi -I. -o test_power_mean 
 #include <cmath>
 #include <iostream>
 #include <iomanip>
+#include <limits>
 
 /**
  * 
@@ -19,14 +20,16 @@ template<typename _Real, typename _Iter>
   __impl_power_mean_0(_Iter __begin, _Iter __end)
   {
     std::size_t __n = 0;
-    _Real __prod = _Real(1);
+    auto __prod = _Real{1};
     while (__begin != __end)
       {
 	++__n;
+	if (*__begin < _Real{0})
+	  std::__throw_domain_error("entries must be non-negative");
 	__prod *= *__begin;
 	++__begin;
       }
-    return std::pow(__prod, _Real(1) / _Real(__n));
+    return std::pow(__prod, _Real{1} / _Real(__n));
   }
 
 /**
@@ -37,21 +40,23 @@ template<typename _Real, typename _Iter>
   __impl_power_mean(_Real __p, _Iter __begin, _Iter __end)
   {
     if (__begin == __end)
-      return _Real(0);
+      return _Real{0};
 
     using _Tp = std::decay_t<decltype(*__begin)>;
-    if (__p == _Real(0))
+    if (__p == _Real{0})
       return __impl_power_mean_0<_Tp>(__begin, __end);
 
     std::size_t __n = 0;
-    _Real __sum = _Real(0);
+    auto __sum = _Real{0};
     while (__begin != __end)
       {
 	++__n;
+	if (*__begin < _Real{0})
+	  std::__throw_domain_error("entries must be non-negative");
 	__sum += std::pow(*__begin, __p);
 	++__begin;
       }
-    return std::pow(__sum / _Real(__n), _Real(1) / _Real(__p));
+    return std::pow(__sum / _Real(__n), _Real{1} / _Real(__p));
   }
 
 /**
@@ -61,16 +66,18 @@ template<typename _Real, typename _Iter, typename _IterW>
   _Real
   __impl_power_mean_0(_Iter __vbegin, _Iter __vend, _IterW __wbegin)
   {
-    _Real __w = _Real(0);
-    _Real __prod = _Real(1);
+    auto __w = _Real{0};
+    auto __prod = _Real{1};
     while (__vbegin != __vend)
       {
+	if (*__vbegin < _Real{0})
+	  std::__throw_domain_error("entries must be non-negative");
 	__w += *__wbegin;
 	__prod *= std::pow(*__vbegin, *__wbegin);
 	++__vbegin;
 	++__wbegin;
       }
-    return std::pow(__prod, _Real(1) / __w);
+    return std::pow(__prod, _Real{1} / __w);
   }
 
 /**
@@ -81,22 +88,24 @@ template<typename _Real, typename _Iter, typename _IterW>
   __impl_power_mean(_Real __p, _Iter __vbegin, _Iter __vend, _IterW __wbegin)
   {
     if (__vbegin == __vend)
-      return _Real(0);
+      return _Real{0};
 
     using _Tp = decltype(*__vbegin * *__wbegin);
-    if (__p == _Real(0))
+    if (__p == _Real{0})
       return __impl_power_mean_0<_Tp>(__vbegin, __vend, __wbegin);
 
-    _Real __w = _Real(0);
-    _Real __sum = _Real(0);
+    auto __w = _Real{0};
+    auto __sum = _Real{0};
     while (__vbegin != __vend)
       {
+	if (*__vbegin < _Real{0})
+	  std::__throw_domain_error("entries must be non-negative");
 	__w += *__wbegin;
 	__sum += *__wbegin * std::pow(*__vbegin, __p);
 	++__vbegin;
 	++__wbegin;
       }
-    return std::pow(__sum / __w, _Real(1) / _Real(__p));
+    return std::pow(__sum / __w, _Real{1} / _Real(__p));
   }
 
 /**
@@ -119,6 +128,8 @@ template<typename _Real>
 			std::initializer_list<_Real> __weight)
   {
     using _Tp = decltype(*__val.begin() * *__weight.begin());
+    if (__weight.size() != __val.size())
+      std::__throw_domain_error("number of weights not equal to number of values.");
     return __impl_power_mean<_Tp>(__p, __val.begin(), __val.end(),
 				  __weight.begin());
   }
@@ -133,7 +144,7 @@ template<typename _Tp>
 
     _Tp a = 1.5;
     _Tp b = 6.7;
-    for (_Tp p = 0.0; p <= 10.0; p += 0.015625)
+    for (_Tp p = -10.0; p <= 10.0; p += 0.015625)
       std::cout << ' ' << std::setw(w) << p
 		<< ' ' << std::setw(w) << power_mean(p, {a, b})
 		<< '\n';
