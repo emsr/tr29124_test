024ce1dcea2bc194c40c9a1fda3d1f363829c198
emsr <3dw4rd@verizon.net>
2017-06-02 00:59:59 -0400

    Start a Jacobi theta tester.test_jacobi_theta.cpp

---
+++ test_jacobi_theta.cpp
@@ -0,0 +1,29 @@
+/*
+$HOME/bin_tr29124/bin/g++ -std=gnu++17 -g -Wall -Wextra -Wno-psabi -I. -o test_jacobi_theta test_jacobi_theta.cpp -lquadmath
+./test_jacobi_theta > test_jacobi_theta.txt
+
+$HOME/bin/bin/g++ -std=gnu++17 -g -Wall -Wextra -Wno-psabi -I. -o test_jacobi_theta test_jacobi_theta.cpp -lquadmath
+./test_jacobi_theta > test_jacobi_theta.txt
+*/
+
+#include <ext/cmath>
+
+template<typename _Tp>
+  void
+  test_jacobi_theta(_Tp proto = _Tp{})
+  {
+    std::cout.precision(__gnu_cxx::__digits10(proto));
+    auto width = std::cout.precision() + 8;
+    std::cout << std::showpoint << std::scientific;
+
+  }
+
+int
+main()
+{
+  auto jt1 [[maybe_unused]] = std::__detail::__jacobi_theta_1_sum(2.0, -8.0);
+  auto jt2 [[maybe_unused]] = std::__detail::__jacobi_theta_2_sum(1.999, -8.0);
+  auto jt3 [[maybe_unused]] = std::__detail::__jacobi_theta_3_sum(3.0, -8.0);
+  auto jt4 [[maybe_unused]] = std::__detail::__jacobi_theta_4_sum(2.999, -8.0);
+  test_jacobi_theta(1.0);
+}
