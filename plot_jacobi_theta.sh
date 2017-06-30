83d487070c92cc6d617ee6cb863845bbb004ba1f
emsr <3dw4rd@verizon.net>
2017-06-30 12:17:10 -0400

    Ignore new binaries.  start plotter for jacobi theta.

---
+++ plot_jacobi_theta.sh
@@ -0,0 +1,10 @@
+
+gnuplot
+
+set termoption enhanced
+
+set hidden3d
+
+set palette model RGB defined (-1 "black", -0.1 "blue", 0 "white", +0.1 "red", 1 "white")
+set colorbox
+set pm3d at bs corners2color geomean
