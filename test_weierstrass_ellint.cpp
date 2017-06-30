3e0a0505b20066ecbdaae19bf3ead008618a6f68
emsr <3dw4rd@verizon.net>
2017-06-29 20:22:11 -0400

    more weierstrass tests. Failing here but might work on another branch.  Tests like DLMF.

--- test_weierstrass_ellint.cpp
+++ test_weierstrass_ellint.cpp
@@ -12,21 +12,103 @@ $HOME/bin/bin/g++ -std=gnu++17 -g -Wall -Wextra -I. -o test_weierstrass_ellint t
 #include <iomanip>
 
   /**
+   * Return the elliptic modular function.
+   * @f[
+   *    \lambda(q) = 16 q \prod_{n=1}^{\infty}
+   *             \left(\frac{(1 + q^{2n})}{(1 + q^{2n-1})}\right)^8
+   * @f]
+   */
+  template<typename _Tp>
+    std::complex<_Tp>
+    __elliptic_modular_prod(const std::complex<_Tp>& __q)
+    {
+      using _Real = std::__detail::__num_traits_t<_Tp>;
+      using _Cmplx = std::complex<_Real>;
+      constexpr std::size_t _S_max_iter = 50;
+      const auto _S_eps = __gnu_cxx::__epsilon(std::abs(__q));
+
+      auto __qqn =  _Cmplx{1};
+      auto __prod = _Cmplx{1};
+      for (std::size_t __i = 1; __i < _S_max_iter; ++__i)
+	{
+	  __qqn *= __q;
+	  auto __fact = _Real{1} / (_Real{1} + __qqn);
+	  __qqn *= __q;
+	  __fact *= (_Real{1} + __qqn);
+	  __fact *= __fact;
+	  __fact *= __fact;
+	  __fact *= __fact;
+	  __prod *= __fact;
+	  if (std::abs(__qqn) < _S_eps)
+	    break;
+	}
+      return _Real{16} * __q * __prod;
+    }
+
+  /**
+   * Return the Dedekind eta function.
+   * @f[
+   *    \eta(q) = q^{1/12} \prod_{n=1}^{\infty}(1 - q^{2n})
+   * @f]
+   */
+  template<typename _Tp>
+    std::complex<_Tp>
+    __dedekind_eta_prod(const std::complex<_Tp>& __q)
+    {
+      using _Real = std::__detail::__num_traits_t<_Tp>;
+      using _Cmplx = std::complex<_Real>;
+      constexpr std::size_t _S_max_iter = 50;
+      const auto _S_eps = __gnu_cxx::__epsilon(std::abs(__q));
+
+      const auto __qq = __q * __q;
+      auto __qqn = _Cmplx{1};
+      auto __prod = _Cmplx{1};
+      for (std::size_t __i = 1; __i < _S_max_iter; ++__i)
+	{
+	  __qqn *= __qq;
+	  __prod *= (_Real{1} - __qqn);
+	  if (std::abs(__qqn) < _S_eps)
+	    break;
+	}
+      return std::pow(__q, _Real{1} / _Real{12}) * __prod;
+    }
+
+  /**
+   * Parallelogram reduction of argument.
+   */
+  template<typename _Tp>
+    std::complex<_Tp>
+    __parallelogram(const std::complex<_Tp>& __tau, const std::complex<_Tp>& __z)
+    {
+      const auto __tau_r = std::real(__tau);
+      const auto __tau_i = std::imag(__tau);
+      const auto __z_r = std::real(__z);
+      const auto __z_i = std::imag(__z);
+      // Solve __z = pi*a*(1,0) + pi*b*(tau_r, tau_i).
+      const auto __b = __zi / __tau_i;
+      const auto __nu = __b - std::floor(__b);
+      const auto __a = (__zr - __b * __tau_r);
+      const auto __mu = __a - std::floor(__a);
+      return std::complex<_Tp>(__mu + __nu * __tau_t, __nu * __tau_i);
+    }
+
+  /**
    * Return the Weierstrass elliptic function.
    */
   template<typename _Tp>
     std::complex<_Tp>
-    weierstrass_p(std::complex<_Tp> __omega1, std::complex<_Tp> __omega3,
-		  std::complex<_Tp> __z)
+    __weierstrass_p(std::complex<_Tp> __omega1, std::complex<_Tp> __omega3,
+		    std::complex<_Tp> __z)
     {
       using _Real = std::__detail::__num_traits_t<_Tp>;
       using _Cmplx = std::complex<_Real>;
+
       const auto _S_pi = __gnu_cxx::__const_pi<_Real>();
+      const auto _S_i = _Cmplx{0, 1};
+
       const auto __tau = __omega3 / __omega1;
-      // Confine z to the fundamental parallelogram defined by pi and tau*pi.
-      const auto __zi = std::fmod(std::imag(__z), _S_pi * std::imag(__tau));
-      const auto __zr = std::fmod(std::real(__z), _S_pi);
-      const auto __q = std::__detail::__polar_pi(_Tp{1}, __tau);
+      const auto __q = std::exp(_S_i * _S_pi * __tau);
+
       const auto __theta2 = std::__detail::__theta_2(__q, _Cmplx{0});
       const auto __theta2p2 = __theta2 * __theta2;
       const auto __theta2p4 = __theta2p2 * __theta2p2;
@@ -35,6 +117,7 @@ $HOME/bin/bin/g++ -std=gnu++17 -g -Wall -Wextra -I. -o test_weierstrass_ellint t
       const auto __theta4p4 = __theta4p2 * __theta4p2;
       const auto __e1 = _S_pi * _S_pi * (__theta2p4 + _Tp{2} * __theta4p4)
 		      / (_Tp{12} * __omega1 * __omega1);
+
       const _Cmplx __arg = _S_pi * __z / _Tp{2} / __omega1;
       const auto __theta3 = std::__detail::__theta_3(__q, _Cmplx{0});
       const auto __theta2a = std::__detail::__theta_2(__q, __arg);
@@ -42,7 +125,8 @@ $HOME/bin/bin/g++ -std=gnu++17 -g -Wall -Wextra -I. -o test_weierstrass_ellint t
       const auto __theta1a = std::__detail::__theta_1(__q, __arg);
       const auto __denom = _Tp{2} * __omega1 * __theta1a;
       const auto __rat = __numer / __denom;
-      return __e1 * __rat * __rat;
+
+      return __e1 + __rat * __rat;
     }
 
 /**/
@@ -50,12 +134,15 @@ template<typename _Tp>
   void
   test_weierstrass_ellint()
   {
-    std::cout.precision(__gnu_cxx::__digits10<_Tp>());
+    using _Real = std::__detail::__num_traits_t<_Tp>;
+    using _Cmplx = std::complex<_Real>;
+
+    std::cout.precision(__gnu_cxx::__digits10<_Real>());
     auto w = std::cout.precision() + 8;
     std::cout << std::showpoint << std::scientific;
 
-    const auto omega1 = _Tp{2};
-    const auto omega3 = _Tp{3};
+    const auto omega1 = _Cmplx{2, 0};
+    const auto omega3 = _Cmplx{0, 3};
     const auto del = _Tp{0.0625};
     for (int ir = -100; ir <= +100; ++ir)
       for (int ii = -100; ii <= +100; ++ii)
@@ -64,7 +151,7 @@ template<typename _Tp>
 	  // The above fails because of C99 complex.
 	  // Which needs to die.
 	  const auto z = std::complex<_Tp>(del * ir, del * ii);
-	  const auto fancyP = weierstrass_p(omega1, omega3, z);
+	  const auto fancyP = __weierstrass_p(omega1, omega3, z);
 	  std::cout << ' ' << std::setw(w) << std::real(z)
 		    << ' ' << std::setw(w) << std::imag(z)
 		    << ' ' << std::setw(w) << std::real(fancyP)
