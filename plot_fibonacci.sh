b3afd3e6055018f86b7d602b377084de3f53cf0c
Ed Smith-Rowland <3dw4rd@verizon.net>
2017-07-20 17:48:25 -0400

    Plot numbers along function. Plot negative numbers.

--- plot_fibonacci.sh
+++ plot_fibonacci.sh
@@ -5,7 +5,8 @@ set termoption enhanced
 set title "Fibonacci function"
 set xlabel "x"
 plot [-10.0:10.0][-60:60] \
-  "test_fibonacci.txt" index 1 using 1:2 with lines title "F_{/Symbol n}"
+  "test_fibonacci.txt" index 0 using 1:2 with points title "F_n", \
+                    "" index 1 using 1:2 with lines title "F_{/Symbol n}"
 
 # 
 set title "Fibonacci polynomials"
@@ -22,7 +23,8 @@ plot [-5.0:5.0][-60:60] \
 set title "Lucas function"
 set xlabel "x"
 plot [-10.0:10.0][-80:120] \
-  "test_fibonacci.txt" index 55 using 1:2 with lines title "L_{/Symbol n}"
+  "test_fibonacci.txt" index 54 using 1:2 with points title "L_n", \
+                    "" index 55 using 1:2 with lines title "L_{/Symbol n}"
 
 # 
 set title "Lucas polynomials"
