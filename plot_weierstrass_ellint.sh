5d8d6ddb1880eef4aafede7fe27c02047bfc192a
emsr <3dw4rd@verizon.net>
2017-07-28 10:16:17 -0400

    You have to plot surfaces/heatmaps in cartesian.

--- plot_weierstrass_ellint.sh
+++ plot_weierstrass_ellint.sh
@@ -3,17 +3,17 @@ gnuplot
 
 set termoption enhanced
 
-set polar
-set grid polar 15
-set angles degrees
-set rrange [0:1]
-set trange [*:*]
-plot  "test_weierstrass_ellint.txt" index 1 using 1:2 title "Re[g_2(q)]"
-plot  "test_weierstrass_ellint.txt" index 1 using 1:3 title "Im[g_2(q)]"
-plot  "test_weierstrass_ellint.txt" index 1 using 1:4 title "|g_2(q)|"
-plot  "test_weierstrass_ellint.txt" index 1 using 1:5 title "Re[g_3(q)]"
-plot  "test_weierstrass_ellint.txt" index 1 using 1:6 title "Im[g_3(q)]"
-plot  "test_weierstrass_ellint.txt" index 1 using 1:7 title "|g_3(q)|"
+#set polar
+#set grid polar 15
+#set angles radians
+#set rrange [0.0:1.0]
+#set trange [0.0:2.0*pi]
+splot [-1.0:1.0][-1.0:1.0][-0.5:0.5] "test_weierstrass_ellint.txt" index 1 using 1:2:3 with pm3d title "Re[g_2(q)]"
+splot [-1.0:1.0][-1.0:1.0][-0.5:0.5] "test_weierstrass_ellint.txt" index 1 using 1:2:4 with pm3d title "Im[g_2(q)]"
+splot [-1.0:1.0][-1.0:1.0][0.0:0.5] "test_weierstrass_ellint.txt" index 1 using 1:2:5 with pm3d title "|g_2(q)|"
+splot [-1.0:1.0][-1.0:1.0][-0.5:0.5] "test_weierstrass_ellint.txt" index 1 using 1:2:6 with pm3d title "Re[g_3(q)]"
+splot [-1.0:1.0][-1.0:1.0][-0.5:0.5] "test_weierstrass_ellint.txt" index 1 using 1:2:7 with pm3d title "Im[g_3(q)]"
+splot [-1.0:1.0][-1.0:1.0][0.0:0.5] "test_weierstrass_ellint.txt" index 1 using 1:2:8 with pm3d title "|g_3(q)|"
 
 
 set hidden3d
