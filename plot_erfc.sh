6330def3bbea859547c1bed6ca00089905d2b9dd
Ed Smith-Rowland <3dw4rd@verizon.net>
2017-08-10 02:49:27 -0400

    erf plot.

--- plot_erfc.sh
+++ plot_erfc.sh
@@ -9,3 +9,13 @@ set xlabel "x"
 plot [-2.0:2.0][-1.0:2.0] \
     "test_erfc.txt" index 0 using 1:2 with lines title "erfc(x)", \
                  "" index 1 using 1:2 with lines title "erf(x)"
+
+set xlabel "x"
+plot [-2.0:5.0][0.0:2.0] \
+  "test_erfc.txt" index 2 using 1:2 with lines title "i^{-1}erfc(x)", \
+               "" index 2 using 1:3 with lines title "i^0erfc(x)", \
+               "" index 2 using 1:4 with lines title "i^1erfc(x)", \
+               "" index 2 using 1:5 with lines title "i^2erfc(x)", \
+               "" index 2 using 1:6 with lines title "i^3erfc(x)", \
+               "" index 2 using 1:7 with lines title "i^4erfc(x)", \
+               "" index 2 using 1:8 with lines title "i^5erfc(x)"
