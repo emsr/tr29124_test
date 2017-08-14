e83aeca9f769d2404c7348619adca859100953ce
emsr <3dw4rd@verizon.net>
2017-08-14 11:21:33 -0400

    Add GSL test functions (not used yet). get test_root_finding to compile.

--- test_root_finding.cpp
+++ test_root_finding.cpp
@@ -13,9 +13,71 @@ $HOME/bin/bin/g++ -g -Wall -Wextra -Wno-psabi -I. -o test_root_finding test_root
 #include <iomanip>
 #include <cmath>
 
-__gnu_cxx::__root_state<double>
-cos_state(double x)
-{ return __gnu_cxx::__root_state<double>{std::cos(x), -std::sin(x)}; }
+template<typename _Tp>
+  _Tp
+  signum(_Tp x)
+  {
+    if (x > _Tp{0})
+      return _Tp{1};
+    else if (x < _Tp{0})
+      return _Tp{-1};
+    else
+      return _Tp{0};
+  }
+
+template<typename _Tp>
+  __gnu_cxx::__root_state<_Tp>
+  func1_state(_Tp x)
+  { return {std::pow(x, _Tp{20}) - 1, _Tp{20} * std::pow(x, _Tp{19})}; }
+
+template<typename _Tp>
+  __gnu_cxx::__root_state<_Tp>
+  func2_state(_Tp x)
+  {
+    return {signum(x) * std::sqrt(std::abs(x)),
+	    _Tp{1} / std::sqrt(std::abs(x))};
+  }
+
+template<typename _Tp>
+  __gnu_cxx::__root_state<_Tp>
+  func3_state(_Tp x)
+  { return {x * x - _Tp{1.0e-8}, _Tp{2} * x}; }
+
+template<typename _Tp>
+  __gnu_cxx::__root_state<_Tp>
+  func4_state(_Tp x)
+  { return {x * std::exp(-x), std::exp(-x) - x * std::exp(-x)}; }
+
+template<typename _Tp>
+  __gnu_cxx::__root_state<_Tp>
+  func5_state(_Tp x)
+  {
+    return {_Tp{1} / (_Tp{1} + std::exp(x)),
+	    -std::exp(x) / std::pow(_Tp{1} + std::exp(x), _Tp{2})};
+  }
+
+template<typename _Tp>
+  __gnu_cxx::__root_state<_Tp>
+  func6_state(_Tp x)
+  {
+    return {std::pow(x - _Tp{1}, _Tp{7}),
+	    _Tp{7} * std::pow(x - _Tp{1}, _Tp{6})};
+  }
+
+template<typename _Tp>
+  __gnu_cxx::__root_state<_Tp>
+  sin_state(_Tp x)
+  { return {std::sin(x), std::cos(x)}; }
+
+template<typename _Tp>
+  __gnu_cxx::__root_state<_Tp>
+  cos_state(_Tp x)
+  { return {std::cos(x), -std::sin(x)}; }
+
+template<typename _Tp>
+  __gnu_cxx::__root_state<_Tp>
+  func7_state(_Tp x)
+  { return {-M_PI * x + M_E, -M_PI}; }
 
 template<typename _Tp>
   void
@@ -50,10 +112,10 @@ template<typename _Tp>
 	    auto x_brent = __gnu_cxx::__root_brent((fun_t)&std::cos, x_lower, x_upper, eps);
 	    std::cout << "cos root (brent)         : x = " << std::setw(width) << x_brent << '\n';
 
-	    auto x_newton = __gnu_cxx::__root_newton(&cos_state, x_lower, x_upper, eps);
+	    auto x_newton = __gnu_cxx::__root_newton(cos_state<_Tp>, x_lower, x_upper, eps);
 	    std::cout << "cos root (newton)        : x = " << std::setw(width) << x_newton << '\n';
 
-	    auto x_safe = __gnu_cxx::__root_safe(&cos_state, x_lower, x_upper, eps);
+	    auto x_safe = __gnu_cxx::__root_safe(cos_state<_Tp>, x_lower, x_upper, eps);
 	    std::cout << "cos root (safe)          : x = " << std::setw(width) << x_safe << '\n';
 	  }
 	else
@@ -86,7 +148,7 @@ template<typename _Tp>
 	std::cout << "cos root brackets:\n";
 	for (auto& br : bracket)
 	  std::cout << "  " << std::setw(width) << br.first << " <= x <= " << std::setw(width) << br.second
-		    << ": x = " << std::setw(width) << __gnu_cxx::__root_newton(&cos_state, br.first, br.second, eps) << '\n';
+		    << ": x = " << std::setw(width) << __gnu_cxx::__root_newton(cos_state<_Tp>, br.first, br.second, eps) << '\n';
 	std::cout << '\n';
       }
     catch(std::exception& err)
