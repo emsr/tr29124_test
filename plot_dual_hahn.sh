d73c2e2b551ade8c2b489f15c0112cc1584b238b
emsr <3dw4rd@verizon.net>
2017-09-19 00:25:49 -0400

    Split off a settings.pgfile and use it in the hahns.

--- plot_dual_hahn.sh
+++ plot_dual_hahn.sh
@@ -1,23 +1,8 @@
 # http://pool-serv1.mathematik.uni-kassel.de/caop/CAOP/plot_dualhahn.html
 
 gnuplot
-set termoption enhanced
-set xzeroaxis
-set yzeroaxis
-set grid
 
-set style line  1 lw 1.2 lc  1
-set style line  2 lw 1.2 lc  2
-set style line  3 lw 1.2 lc  3
-set style line  4 lw 1.2 lc  4
-set style line  5 lw 1.2 lc  5
-set style line  6 lw 1.2 lc  6
-set style line  7 lw 1.2 lc  7
-set style line  8 lw 1.2 lc  8
-set style line  9 lw 1.2 lc  9 dt 2
-set style line 10 lw 1.2 lc 10 dt 2
-set style line 11 lw 1.2 lc 11 dt 2
-set style line 12 lw 1.2 lc 12 dt 2
+load 'settings.gp'
 
 set xlabel "{/Symbol l}(x)"
 plot [0.0:100.0][-2.0:12.0] \
