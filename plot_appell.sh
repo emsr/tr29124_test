1570855d7233e63bab86a4e789d2ab646fa9bcc2
Ed Smith-Rowland <3dw4rd@verizon.net>
2017-09-22 15:21:42 -0400

    Ignore some new test_*. Add test_appell start.

---
+++ plot_appell.sh
@@ -0,0 +1,3 @@
+gnuplot
+
+splot [-1:1][-1:1][-1000:1000] "test_appell.txt" index 0 with pm3d title "F_1(x,y)"
---
+++ test_appell.cpp
@@ -0,0 +1,82 @@
+/*
+$HOME/bin/bin/g++ -std=c++17 -g -Wall -Wextra -o test_appell test_appell.cpp
+./test_appell > test_appell.txt
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <cmath>
+
+template<typename _Tp>
+  _Tp
+  __appell_f1_series(_Tp alpha, _Tp betax, _Tp betay, _Tp gamma, _Tp x, _Tp y)
+  {
+    const _Tp _S_eps = std::numeric_limits<_Tp>::epsilon();
+    const int N = 100;
+    auto f1 = _Tp{0};
+    auto termx = _Tp{1};
+    for (int m = 0; m < N; ++m)
+      {
+	auto termy = _Tp{1};
+	for (int n = 0; n < N; ++n)
+	  {
+	    const auto term = termx * termy;
+	    f1 += term;
+	    if (std::abs(term) < _S_eps * std::abs(f1))
+	      break;
+	    termy *= (alpha + n) * (betay + n) * y / (gamma + n) / (n + 1);
+	  }
+	termx *= (alpha + m) * (betax + m) * x / (gamma + m) / (m + 1);
+      }
+    return f1;
+  }
+
+template<typename _Tp>
+  _Tp
+  __appell_f2_series(_Tp alpha, _Tp beta, _Tp betap, _Tp gamma, _Tp gammap, _Tp x, _Tp y)
+  {
+  }
+
+template<typename _Tp>
+  _Tp
+  __appell_f3_series(_Tp alpha, _Tp alphap, _Tp beta, _Tp betap, _Tp gamma, _Tp x, _Tp y)
+  {
+  }
+
+template<typename _Tp>
+  _Tp
+  __appell_f4_series(_Tp alpha, _Tp beta, _Tp gamma, _Tp gammap, _Tp x, _Tp y)
+  {
+  }
+
+template<typename _Tp>
+  void
+  test_appell(_Tp alpha, _Tp betax, _Tp betay, _Tp gamma)
+  {
+    std::cout.precision(std::numeric_limits<_Tp>::digits10);
+    auto w = 8 + std::cout.precision();
+
+    const auto del = _Tp{0.01};
+    for (int m = -99; m <= 99; ++m)
+      {
+        auto x = m * del;
+	for (int n = -99; n <= 99; ++n)
+	  {
+	    auto y = n * del;
+	    auto f1 = __appell_f1_series(alpha, betax, betay, gamma, x, y);
+	    std::cout << ' ' << std::setw(w) << x
+		      << ' ' << std::setw(w) << y
+		      << ' ' << std::setw(w) << f1
+		      << '\n';
+	  }
+	std::cout << '\n';
+      }
+  }
+
+int
+main()
+{
+  test_appell(1.0, 1.5, -0.5, 0.5);
+}
+
