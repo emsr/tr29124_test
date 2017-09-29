f84136aada5dc6a957fd9d55ccd40eb3f00dbe26
Ed Smith-Rowland <3dw4rd@verizon.net>
2017-09-29 15:13:30 -0400

    Make a plotter for Gudermann.

---
+++ plot_gudermannian.sh
@@ -0,0 +1,11 @@
+
+gnuplot
+
+load 'settings.gp'
+
+set title "Gudermannian gd(x)"
+set xlabel "x"
+set ylabel "{/Symbol f}"
+plot [-3.1:3.1][-1.5:1.5] \
+    "test_gudermannian.txt" index 0 using 1:2 with lines ls 1 title "gd(x) - series", \
+                         "" index 0 using 1:3 with lines ls 2 title "gd(x) - trig"
