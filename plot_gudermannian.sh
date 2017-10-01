43bb870154136b9879cad07f390fa97aaee657fa
emsr <3dw4rd@verizon.net>
2017-09-30 22:02:04 -0400

    Start invgd and test and plot.

--- plot_gudermannian.sh
+++ plot_gudermannian.sh
@@ -8,4 +8,5 @@ set xlabel "x"
 set ylabel "{/Symbol f}"
 plot [-3.1:3.1][-1.5:1.5] \
     "test_gudermannian.txt" index 0 using 1:2 with lines ls 1 title "gd(x) - series", \
-                         "" index 0 using 1:3 with lines ls 2 title "gd(x) - trig"
+                         "" index 0 using 1:3 with lines ls 2 title "gd(x) - trig", \
+                         "" index 0 using 1:5 with lines ls 3 title "invgd(x) - series"
