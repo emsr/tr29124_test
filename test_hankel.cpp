e7827041bf26935f794dc114435eb832cfb0f15a
Ed Smith-Rowland <3dw4rd@verizon.net>
2017-11-03 12:51:45 -0400

    Wrap test_hankel in try-catch

--- test_hankel.cpp
+++ test_hankel.cpp
@@ -31,32 +31,46 @@ main()
   std::complex<double> z, nu;
   std::__detail::_Airy<std::complex<double>> airy_thing;
 
-  nu = 5.0;
-  z = 1.0 - 3.0i;
-
-  // Cool but we nead an eater.
-  auto [zx, nux, h1, h2, h1p, h2p] = std::__detail::__hankel_uniform(nu, z);
-
   std::cout.precision(std::numeric_limits<double>::max_digits10);
   auto width = 6 + std::cout.precision();
 
-  std::cout << '\n';
-  std::cout << " nu     = " << std::setw(width) << nu << '\n';
-  std::cout << " z      = " << std::setw(width) << z << '\n';
-  std::cout << " H1(z)  = " << std::setw(width) << h1 << '\n';
-  std::cout << " H1'(z) = " << std::setw(width) << h1p << '\n';
-  std::cout << " H2(z)  = " << std::setw(width) << h2 << '\n';
-  std::cout << " H2'(z) = " << std::setw(width) << h2p << '\n';
-  std::cout << " J(z)   = " << std::setw(width) << (h1 + h2) / 2.0 << '\n';
-  std::cout << " J'(z)  = " << std::setw(width) << (h1p + h2p) / 2.0 << '\n';
-  std::cout << " Y(z)   = " << std::setw(width) << (h1 - h2) / 2.0i << '\n';
-  std::cout << " Y'(z)  = " << std::setw(width) << (h1p - h2p) / 2.0i << '\n';
+  nu = 5.0;
+  z = 1.0 - 3.0i;
 
-  auto Airy = airy_thing(z);
-  std::cout << '\n';
-  std::cout << " z      = " << std::setw(width) << Airy.__z << '\n';
-  std::cout << " Ai(z)  = " << std::setw(width) << Airy.__Ai_value << '\n';
-  std::cout << " Ai'(z) = " << std::setw(width) << Airy.__Ai_deriv << '\n';
-  std::cout << " Bi(z)  = " << std::setw(width) << Airy.__Bi_value << '\n';
-  std::cout << " Bi'(z) = " << std::setw(width) << Airy.__Bi_deriv << '\n';
+  try
+  {
+    // Cool but we nead an eater.
+    auto [zx, nux, h1, h2, h1p, h2p] = std::__detail::__hankel_uniform(nu, z);
+
+    std::cout << '\n';
+    std::cout << " nu     = " << std::setw(width) << nu << '\n';
+    std::cout << " z      = " << std::setw(width) << z << '\n';
+    std::cout << " H1(z)  = " << std::setw(width) << h1 << '\n';
+    std::cout << " H1'(z) = " << std::setw(width) << h1p << '\n';
+    std::cout << " H2(z)  = " << std::setw(width) << h2 << '\n';
+    std::cout << " H2'(z) = " << std::setw(width) << h2p << '\n';
+    std::cout << " J(z)   = " << std::setw(width) << (h1 + h2) / 2.0 << '\n';
+    std::cout << " J'(z)  = " << std::setw(width) << (h1p + h2p) / 2.0 << '\n';
+    std::cout << " Y(z)   = " << std::setw(width) << (h1 - h2) / 2.0i << '\n';
+    std::cout << " Y'(z)  = " << std::setw(width) << (h1p - h2p) / 2.0i << '\n';
+  }
+  catch (std::exception & err)
+  {
+    std::cerr << "Error in test_hankel (hankel_uniform): " << err.what() << '\n';
+  }
+
+  try
+  {
+    auto Airy = airy_thing(z);
+    std::cout << '\n';
+    std::cout << " z      = " << std::setw(width) << Airy.__z << '\n';
+    std::cout << " Ai(z)  = " << std::setw(width) << Airy.__Ai_value << '\n';
+    std::cout << " Ai'(z) = " << std::setw(width) << Airy.__Ai_deriv << '\n';
+    std::cout << " Bi(z)  = " << std::setw(width) << Airy.__Bi_value << '\n';
+    std::cout << " Bi'(z) = " << std::setw(width) << Airy.__Bi_deriv << '\n';
+  }
+  catch (std::exception & err)
+  {
+    std::cerr << "Error in test_hankel (airy): " << err.what() << '\n';
+  }
 }
