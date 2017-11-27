9d2e452c07d0a0a16665f6a9f092090af5c39db6
Ed Smith-Rowland <3dw4rd@verizon.net>
2017-11-27 17:05:46 -0500

    Make titles better with a space.

--- plot_trigamma.sh
+++ plot_trigamma.sh
@@ -7,8 +7,8 @@ set xzeroaxis
 set yzeroaxis
 set grid
 
-set title "Trigamma function {/Symbol y}^{(1)}(x)"
+set title "Trigamma function  {/Symbol y}^{(1)}(x)"
 plot [0.0:20.0][0.0:1.0] "test_polygamma.txt" index 0 using 1:2 with lines title "{/Symbol y}^{(1)}(x)"
 
-set title "Tetragamma function {/Symbol y}^{(2)}(x)"
+set title "Tetragamma function  {/Symbol y}^{(2)}(x)"
 plot [0.0:20.0][-0.5:0.0] "test_polygamma.txt" index 1 using 1:2 with lines title "{/Symbol y}^{(2)}(x)"
