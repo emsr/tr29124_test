1a6ef23c62c0a89a25d362df2a1584d729ea6bd3
Ed Smith-Rowland <3dw4rd@verizon.net>
2017-12-06 15:21:42 -0500

    The complex literal bug in libstdc++ has been solved!

--- complex_literal_bug.cpp
+++ complex_literal_bug.cpp
@@ -4,18 +4,47 @@
 
 #include <complex>
 
-std::complex<double>
-func(std::complex<double> z)
+template<typename Real>
+  std::complex<Real>
+  func(std::complex<Real> z)
+  {
+    return Real{2} * z;
+  }
+
+void
+test_float()
 {
-  return 2.0 * z;
+  using namespace std::literals::complex_literals;
+  auto w = func(1.0f + 3.0if);
+  w /= 2.0if;
+  auto k = w / 4.0if;
+  auto q = w * 1.5if;
 }
 
-int
-main()
+void
+test_double()
 {
   using namespace std::literals::complex_literals;
   auto w = func(1.0 + 3.0i);
   w /= 2.0i;
   auto k = w / 4.0i;
   auto q = w * 1.5i;
+}
+
+void
+test_long_double()
+{
+  using namespace std::literals::complex_literals;
+  auto w = func(1.0l + 3.0il);
+  w /= 2.0il;
+  auto k = w / 4.0il;
+  auto q = w * 1.5il;
+}
+
+int
+main()
+{
+  test_float();
+  test_double();
+  test_long_double();
 }
