832af0afce35e1b27e47da82fe932e0a56bb4a0b
Ed Smith-Rowland <3dw4rd@verizon.net>
2017-12-14 15:28:04 -0500

    Extend plot range for Debye.

--- plot_debye.sh
+++ plot_debye.sh
@@ -9,7 +9,7 @@ set grid
 
 set title "Debye functions D_n(x)"
 set xlabel "x"
-plot [0.0:20.0][0.0:1.1] "test_debye.txt" index 0 using 1:2 with lines title "Debye_1(x)", \
+plot [-5.0:20.0][0.0:4.5] "test_debye.txt" index 0 using 1:2 with lines title "Debye_1(x)", \
                                        "" index 0 using 1:3 with lines title "Debye_2(x)", \
                                        "" index 0 using 1:4 with lines title "Debye_3(x)", \
                                        "" index 0 using 1:5 with lines title "Debye_4(x)", \
@@ -19,7 +19,7 @@ plot [0.0:20.0][0.0:1.1] "test_debye.txt" index 0 using 1:2 with lines title "De
 
 set title "Debye functions D_n(x) - GSL"
 set xlabel "x"
-plot [0.0:20.0][0.0:1.1] "test_debye.txt" index 1 using 1:2 with lines title "Debye_1(x) GSL", \
+plot [-5.0:20.0][0.0:4.5] "test_debye.txt" index 1 using 1:2 with lines title "Debye_1(x) GSL", \
                                        "" index 1 using 1:3 with lines title "Debye_2(x) GSL", \
                                        "" index 1 using 1:4 with lines title "Debye_3(x) GSL", \
                                        "" index 1 using 1:5 with lines title "Debye_4(x) GSL", \
