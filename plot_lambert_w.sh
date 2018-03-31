236d76f3520eaaef0f9cce7023a160bf1af9130e
emsr <3dw4rd@verizon.net>
2018-03-31 19:53:49 -0400

    Tweak plot_lambert_w for both real branches on one plot.

--- plot_lambert_w.sh
+++ plot_lambert_w.sh
@@ -12,3 +12,9 @@ plot [-0.5:2.8][-1.1:1.1] "test_lambert_w.txt" index 0 using 1:2 with lines titl
 set title "Lambert W function W_{-1}(z)"
 set xlabel "p"
 plot [-0.5:2.8][-5.0:-0.9] "test_lambert_w.txt" index 1 using 1:2 with lines title "W_{-1}(z)"
+
+set title "Lambert W function W(z)"
+set xlabel "p"
+plot [-0.5:2.8][-5.0:1.1] \
+    "test_lambert_w.txt" index 0 using 1:2 with lines title "W_0(z)", \
+                      "" index 1 using 1:2 with lines title "W_{-1}(z)"
