401c7b6b5ae82466466a5e0af999361dbb582df2
Edward M Smith-Rowland <esmith-rowland@alionscience.com>
2018-06-21 17:46:29 -0400

    Make test_abs stuff return absolute values.

--- test_abs.cpp
+++ test_abs.cpp
@@ -7,9 +7,6 @@ $HOME/bin/bin/g++ -std=c++2a -o test_abs test_abs.cpp
 #include <algorithm>
 #include <iostream>
 
-// This returns the thing that has the extremal absolute value
-// not the absolute value of the thing that has the extremal absolute value!
-
 template<typename _Tp>
   constexpr bool
   lessabs(const _Tp& __a, const _Tp& __b)
@@ -18,32 +15,38 @@ template<typename _Tp>
 template<typename _Tp>
   constexpr _Tp
   minabs(const _Tp& __a, const _Tp& __b)
-  { return std::min(__a, __b, lessabs<_Tp>); }
+  { return std::abs(std::min(__a, __b, lessabs<_Tp>)); }
 
 template<typename _Tp>
   constexpr _Tp
   minabs(std::initializer_list<_Tp> __il)
-  { return std::min(__il, lessabs<_Tp>); }
+  { return std::abs(std::min(__il, lessabs<_Tp>)); }
 
 template<typename _Tp>
   constexpr _Tp
   maxabs(const _Tp& __a, const _Tp& __b)
-  { return std::max(__a, __b, lessabs<_Tp>); }
+  { return std::abs(std::max(__a, __b, lessabs<_Tp>)); }
 
 template<typename _Tp>
   constexpr _Tp
   maxabs(std::initializer_list<_Tp> __il)
-  { return std::max(__il, lessabs<_Tp>); }
+  { return std::abs(std::max(__il, lessabs<_Tp>)); }
 
 template<typename _Tp>
   constexpr std::pair<_Tp, _Tp>
   minmaxabs(const _Tp& __a, const _Tp& __b)
-  { return std::minmax(__a, __b, lessabs<_Tp>); }
+  {
+    auto [__c, __d] = std::minmax(__a, __b, lessabs<_Tp>);
+    return {std::abs(__c), std::abs(__d)};
+  }
 
 template<typename _Tp>
   constexpr std::pair<_Tp, _Tp>
   minmaxabs(std::initializer_list<_Tp> __il)
-  { return std::minmax(__il, lessabs<_Tp>); }
+  {
+    auto [__c, __d] = std::minmax(__il, lessabs<_Tp>);
+    return {std::abs(__c), std::abs(__d)};
+  }
 
 int
 main()
