348bc9d5406f707dad8bb565aa0205589e2f1114
Edward M Smith-Rowland <esmith-rowland@alionscience.com>
2018-08-09 18:32:15 -0400

    plot Bernoulli polynomials.

---
+++ plot_bernoulli.sh
@@ -0,0 +1,10 @@
+# http://pool-serv1.mathematik.uni-kassel.de/caop/CAOP/plot_charlier.html
+
+gnuplot
+
+load 'setings.gp'
+
+set title "Bernoulli Polynomial B_n(x)"
+set xlabel "x"
+plot [0.0:9.5][-25.0:16.0] \
+    "test_bernoulli.txt" index 6 using 1:2 with lines title "B_0(x)"
