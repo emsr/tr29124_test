70e47d83c82f97a21f58f8c69478a3db2fdec682
Edward M Smith-Rowland <esmith-rowland@alionscience.com>
2018-08-10 17:14:56 -0400

    Start a test_chebyshev for the normal polynomials and derivatives.

---
+++ plot_chebyshev.sh
@@ -0,0 +1,18 @@
+
+gnuplot
+set termoption enhanced
+set xzeroaxis
+set yzeroaxis
+set grid
+
+set title "Chebyshev polynomials and their derivatives, n = 1"
+set xlabel "x"
+plot [-1.0:1.0][-3.0:3.0] \
+    "test_chebyshev.txt" index 1 using 1:2 with lines title "T(x)", \
+                      "" index 1 using 1:3 with lines title "T'(x)", \
+                      "" index 1 using 1:4 with lines title "U(x)", \
+                      "" index 1 using 1:5 with lines title "U'(x)", \
+                      "" index 1 using 1:6 with lines title "V(x)", \
+                      "" index 1 using 1:7 with lines title "V'(x)", \
+                      "" index 1 using 1:8 with lines title "W(x)", \
+                      "" index 1 using 1:9 with lines title "W'(x)"
--- test_chebyshev.cpp
+++ test_chebyshev.cpp
@@ -1,52 +1,62 @@
 /*
-$HOME/bin_tr29124/bin/g++ -std=gnu++17 -g -Wall -Wextra -Wno-psabi -I. -o test_chebyshev test_chebyshev.cpp -lquadmath
+$HOME/bin_specfun/bin/g++ -std=gnu++17 -g -Wall -Wextra -Wno-psabi -I. -o test_chebyshev test_chebyshev.cpp -lquadmath
 ./test_chebyshev > test_chebyshev.txt
 
 $HOME/bin/bin/g++ -std=gnu++17 -g -Wall -Wextra -Wno-psabi -I. -o test_chebyshev test_chebyshev.cpp -lquadmath
-./test_chebyshev
+./test_chebyshev > test_chebyshev.txt
 */
 
 #include <iostream>
 #include <iomanip>
-
-#include <bits/float128_io.h>
-#include "chebyshev.h"
+#include <vector>
 #include <cmath>
 
+template<typename Tp>
+  void
+  test_chebyshev(Tp proto = Tp{})
+  {
+    std::cout.precision(__gnu_cxx::__digits10(proto));
+    std::cout << std::showpoint << std::scientific;
+    auto width = 8 + std::cout.precision();
+
+    std::vector<unsigned int> index{0, 1, 2, 3, 4, 5, 10, 20, 23, 50, 100};
+
+    for (auto n : index)
+      {
+	std::cout << "\n\n n = " << std::setw(width) << n << '\n';
+	std::cout << ' ' << std::setw(width) << "x"
+		  << ' ' << std::setw(width) << "T"
+		  << ' ' << std::setw(width) << "T'"
+		  << ' ' << std::setw(width) << "U"
+		  << ' ' << std::setw(width) << "U'"
+		  << ' ' << std::setw(width) << "V"
+		  << ' ' << std::setw(width) << "V'"
+		  << ' ' << std::setw(width) << "W"
+		  << ' ' << std::setw(width) << "W'"
+		  << '\n';
+	const auto del = Tp{1} / Tp{100};
+	for (int i = -100; i <= 100; ++i)
+	  {
+	    auto x = del * i;
+	    auto Ts = std::__detail::__chebyshev_t(n, x);
+	    auto Us = std::__detail::__chebyshev_u(n, x);
+	    auto Vs = std::__detail::__chebyshev_v(n, x);
+	    auto Ws = std::__detail::__chebyshev_w(n, x);
+	    std::cout << ' ' << std::setw(width) << x
+		      << ' ' << std::setw(width) << Ts.__T_n
+		      << ' ' << std::setw(width) << Ts.deriv()
+		      << ' ' << std::setw(width) << Us.__U_n
+		      << ' ' << std::setw(width) << Us.deriv()
+		      << ' ' << std::setw(width) << Vs.__V_n
+		      << ' ' << std::setw(width) << Vs.deriv()
+		      << ' ' << std::setw(width) << Ws.__W_n
+		      << ' ' << std::setw(width) << Ws.deriv()
+		      << '\n';
+	  }
+      }
+  }
 int
 main()
 {
-  __gnu_cxx::_Chebyshev<double> c1{-1.0, 1.0,
-   {-1.142022680371168e0, 6.5165112670737e-3,
-     3.087090173086e-4, -3.4706269649e-6,
-     6.9437664e-9, 3.67765e-11, -1.356e-13}};
-  __gnu_cxx::_Chebyshev<double> c2{-1.0, 1.0,
-   { 1.843740587300905e0, -7.86528408447867e-2,
-     1.2719271366546e-3, -4.9717367042e-6,
-    -3.31261198e-8, 2.423096e-10, -1.702e-13, -1.49e-15}};
-
-  //
-  //__gnu_cxx::dilog
-  __gnu_cxx::_Chebyshev<double> cdilog(-4.0, 1.0, 40, [](double x){return x * x;});
-  std::cout << cdilog << '\n';
-  for (int i = 0; i <= 500; ++i)
-    {
-      auto x = -4.0 + i * 0.01;
-      std::cout << "x = " << x
-		<< "  dilog(x) = " << cdilog(x) << '\n';
-    }
-  cdilog.truncate(1.0e-12);
-  std::cout << cdilog << '\n';
-
-  //
-  __gnu_cxx::_Chebyshev<__float128> cquad(-4.0Q, +7.0Q, 40, [](__float128 x){return 3.5Q + x * (13.25Q + x * 6.375Q);});
-  std::cout << cquad << '\n';
-  for (int i = 0; i <= 500; ++i)
-    {
-      auto x = -4.0Q + i * 0.01Q;
-      std::cout << "x = " << x
-		<< "  quad(x) = " << cquad(x) << '\n';
-    }
-  cquad.truncate<float>();
-  std::cout << cquad << '\n';
+  test_chebyshev(1.0);
 }
