e4d99d41f297546fc3bb86c7b2d3bf9fc2f7f32b
Ed Smith-Rowland <3dw4rd@verizon.net>
2018-10-02 11:49:08 -0400

    Give root finding good error categories. Move junk code from lib to tester.

--- test_root_finding.cpp
+++ test_root_finding.cpp
@@ -183,3 +183,114 @@ main()
 {
   test_roots<double>();
 }
+
+/*
+  template<typename _Tp>
+    void
+    __root_laguerre(_Polymomial<std::complex<_Tp>>& __a, std::complex<_Tp>& __x,
+		    int& __its)
+    {
+      // Estimated fractional roundoff error.
+      constexpr _Tp _S_eps = std::numeric_limits<_Tp>::epsilon();
+
+      // Number of fractional values.
+      constexpr int MR = 8;
+      // Fractions used to break a limit cycle.
+      static const _Tp
+      _S_frac[MR + 1]
+      {0.0, 0.5, 0.25, 0.75, 0.13, 0.38, 0.62, 0.88, 1.0};
+
+      // Number of steps taken before trying a new fraction.
+      constexpr int MT = 10;
+
+      constexpr int _S_max_iter = MT * MR;
+
+      Complex d, f;
+      int __m = __a.degree();
+      for (int __iter = 1;__iter <= _S_max_iter; ++__iter)
+	{ // Loop over iterations up to allowed maximum.
+	  __its = __iter;
+	  auto __b = __a[__m];
+	  auto __err = std::abs(__b);
+	  std::complex<_Tp> __d{}, __f{};
+	  auto __abx = std::abs(__x);
+	  for (int __j = __m - 1; __j >= 0; --__j)
+	    {
+	      // Efficient computation of the polynomial and its first two derivatives.
+	      // f stores P''(x)/2.
+	      __f = __x * __f + __d;
+	      __d = __x * __d + __b;
+	      __b = __x * __b + __a[__j];
+	      __err = __abx * __err + std::abs(__b);
+	    }
+	  __err *= ___S_eps;
+	  // Estimate of roundoff error in evaluating polynomial.
+	  if (std::abs(__b) <= __err) // We have the root.
+	    return;
+	  // Use Laguerre's formula.
+	  auto __g = __d / __b;
+	  auto __g2 = __g * __g;
+	  auto __h = __g2 - _Tp{2} * __f / __b;
+	  auto __sq = std::sqrt(_Tp(__m - 1) * (_Tp(__m) * __h - __g2));
+	  auto __gp = __g + __sq;
+	  auto __gm = __g - __sq;
+	  auto __abp = std::abs(__gp);
+	  auto __abm = std::abs(__gm);
+	  if (__abp < __abm)
+	    __gp = __gm;
+	  auto __dx = std::max(__abp, __abm) > _Tp{0}
+		    ? _Tp(__m) / __gp
+		    : std::polar(_Tp{1} + __abx, _Tp(__iter));
+	  auto __x1 = __x - __dx;
+	  if (__x == __x1)
+	    return;
+	  if (__iter % MT != 0)
+	    __x = __x1;
+	  else
+	    __x -= ___S_frac[__iter / MT] * __dx;
+	}
+      std::__throw_runtime_error(__N("__root_laguerre: "
+				     "Maximum number of iterations exceeded"));
+    }
+
+  template<typename _Tp>
+    void
+    qroot(_Polynomial<std::complex<_Tp>>& __p, _Tp& __b, _Tp& __c, _Tp __eps)
+    {
+      using _Poly = _Polynomial<std::complex<_Tp>>;
+      constexpr int _S_max_iter = 20;
+      constexpr auto _S_eps = std::numeric_limits<_Tp>::epsilon();
+      constexpr auto _S_tiny = _Tp{100} * _S_eps;
+      auto __n = __p.order();
+      _Poly __q, __qq, __rem;
+      for (int __iter = 0; __iter < _S_max_iter; ++__iter)
+	{
+	  _Poly __d(__c, __b, _Tp{1});
+
+	  // First division: r, s.
+	  divmod(__p, __d, __q, __rem);
+	  auto __s = __rem[0];
+	  auto __r = __rem[1];
+	  // Second division: partial r, s with respect to c.
+	  divmod(__q, __d, __qq, __rem);
+	  auto __sc = -__rem[0];
+	  auto __rc = -__rem[1];
+	  auto __sb = -__c * __rc;
+	  auto __rb = -__b * __rc + __sc;
+	  // Solve 2x2 equation.
+	  auto __dv = _Tp{1} / (__sb * __rc - __sc * __rb);
+	  auto __delb = ( __r * __sc - __s * __rc) * __dv;
+	  auto __delc = (-__r * __sb + __s * __rb) * __dv;
+	  __b += __delb;
+	  __delc = (-__r * __sb + __s * __rb) * __dv;
+	  __c += __delc;
+	  if ((std::abs(__delb) <= eps * std::abs(__b)
+	      || std::abs(__b) < _S_tiny)
+           && (std::abs(__delc) <= eps * std::abs(__c)
+	      || std::abs(__c) < _S_tiny))
+	    return;
+	}
+      std::__throw_runtime_error(__N("qroot: "
+				     "Maximum number of iterations exceeded"));
+    }
+*/
