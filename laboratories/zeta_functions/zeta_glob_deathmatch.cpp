5f24923f42cdec8e92b25884bc7eaa617cc1f81d
emsr <3dw4rd@verizon.net>
2019-06-07 20:16:03 -0400

    Clean out lab .gitignores (don't ignore binaries in main dirs). Build binaries in local bin dirs.

--- laboratories/zeta_functions/zeta_glob_deathmatch.cpp
+++ laboratories/zeta_functions/zeta_glob_deathmatch.cpp
@@ -11,6 +11,7 @@ $HOME/bin/bin/g++ -std=c++2a -Wall -Wextra -g -I. -o zeta_glob_deathmatch zeta_g
 #include <iostream>
 #include <iomanip>
 
+#include <ext/math_const.h>
 #include <wrap_gsl.h>
 
 #define _GLIBCXX_MATH_NS std
