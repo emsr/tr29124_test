b9e895ef5e63cd51c04967b0d8c1d6ebbf4b253e
emsr <3dw4rd@verizon.net>
2019-06-11 13:22:08 -0400

    Fix infinite recursion in test_sqrt.  Add TODO.md

---
+++ laboratories/elementary_functions/TODO.md
@@ -0,0 +1,2 @@
+1. In test_sqrt put the linear first guess from Cody & Waits.
+2. In test_sqrt try digits/2 for max iteration - can you skip the test?
--- laboratories/elementary_functions/test_sqrt.cpp
+++ laboratories/elementary_functions/test_sqrt.cpp
@@ -3,6 +3,7 @@
  */
 
 #include <iostream>
+#include <iomanip>
 #include <cmath>
 #include <limits>
 
@@ -10,19 +11,25 @@
  * 
  */
 template<typename _Tp>
-  _Tp
+  constexpr _Tp
   __sqrt_recip_newton(_Tp __x)
   {
+    const auto _S_eps = std::numeric_limits<_Tp>::epsilon();
+    const auto _S_digs = std::numeric_limits<_Tp>::digits;
+
     if (__x == _Tp{0})
       return _Tp{0};
 
     auto __f = 0.01;
     auto __ff = __f * __f;
-    while (std::abs(__ff - __x) > std::numeric_limits<_Tp>::epsilon() * __f)
+    for (int i = 0; i < _S_digs; ++i)
       {
 	__f *= (_Tp{3} - __x * __ff) / _Tp{2};
 	__ff = __f * __f;
+	if (std::abs(__ff - __x) < _S_eps * __f)
+	  break;
       }
+
     return __f;
   }
 
@@ -33,11 +40,20 @@ template<typename _Tp>
   void
   test_sqrt()
   {
+    std::cout.precision(std::numeric_limits<_Tp>::max_digits10);
+    const auto w = 6 + std::cout.precision();
+
     for (int i = 0; i <= 1000; ++i)
       {
-	auto x = _Tp{1.0} * i;
+	auto x = _Tp{1.0L} * i;
 	auto sqrtr = x * __sqrt_recip_newton(x);
-	std::cout << ' ' << x << ' ' << sqrtr << '\n';
+	auto sqrt = std::sqrt(x);
+	auto delta = sqrtr - sqrt;
+	std::cout << ' ' << std::setw(w) << x
+		  << ' ' << std::setw(w) << sqrtr
+		  << ' ' << std::setw(w) << sqrt
+		  << ' ' << std::setw(w) << delta
+		  << '\n';
       }
   }
 
