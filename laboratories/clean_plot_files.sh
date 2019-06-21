4f48a68827d6a2821defb0b39fb4052a37af1c87
emsr <3dw4rd@verizon.net>
2019-06-21 16:18:26 -0400

    Build of libstdc++ check_*. Add assorted scripts and output files.

---
+++ laboratories/clean_plot_files.sh
@@ -0,0 +1,39 @@
+rm \
+ airy_functions/airy_complex_float.txt \
+ airy_functions/airy_float.txt \
+ airy_functions/fgh_float.txt \
+ airy_functions/plot_airy_complex_float.txt \
+ airy_functions/scorer_float.txt \
+ bessel_functions/cyl_bessel_j_float.txt \
+ bessel_functions/cyl_neumann_float.txt \
+ bessel_functions/struve_float.txt \
+ gamma_functions/output/test_binet_float.txt \
+ gamma_functions/log_gamma_spouge_float.txt \
+ gamma_functions/log_gamma_lanczos_float.txt
+
+rm \
+ airy_functions/airy_complex_double.txt \
+ airy_functions/airy_complex_long_double.txt \
+ airy_functions/airy_double.txt \
+ airy_functions/airy_long_double.txt \
+ airy_functions/fgh_double.txt \
+ airy_functions/fgh_long_double.txt \
+ airy_functions/plot_airy_complex_double.txt \
+ airy_functions/plot_airy_complex_long_double.txt \
+ airy_functions/scorer_double.txt \
+ airy_functions/scorer_long_double.txt \
+ bessel_functions/cyl_bessel_j_double.txt \
+ bessel_functions/cyl_neumann_double.txt \
+ bessel_functions/kelvin_double.txt \
+ bessel_functions/kelvin_long_double.txt \
+ bessel_functions/kelvin_order_double.txt \
+ bessel_functions/kelvin_order_long_double.txt \
+ beta_functions/beta_double.txt \
+ gamma_functions/log_gamma_lanczos_double.txt \
+ gamma_functions/log_gamma_lanczos_long_double.txt \
+ gamma_functions/log_gamma_spouge_double.txt \
+ gamma_functions/log_gamma_spouge_long_double.txt
+
+rm \
+ gamma_functions/log_gamma_lanczos__float128.txt \
+ gamma_functions/log_gamma_spouge__float128.txt
