bd5000b22c5e6952c1196e9de2a7ea895001cfd0
emsr <3dw4rd@verizon.net>
2019-07-15 18:19:13 -0400

    Remove cmake artifacts from the tree.

---
+++ cmake_clean.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+rm -rf $(find . -name CMakeCache.txt)
+rm -rf $(find . -name CMakeFiles/)
+rm -rf $(find . -name Makefile)
+rm -rf $(find . -name cmake_install.cmake)
+
+rm -rf $(find . -name CTestTestfile.cmake)
+rm -rf $(find . -name Testing/)
+
+rm -rf $(find . -name cmake_install.cmake)
+
+rm -rf $(find . -name CMakeDoxyfile.in)
+rm -rf $(find . -name MakeDoxygenDefaults.cmake)
+
