ae20d0e40c96fbad0bed6551c3c920799ae973cf
emsr <3dw4rd@verizon.net>
2019-07-15 18:20:16 -0400

    Remove dir slashes from cleanup script cmake_clean.sh.

--- cmake_clean.sh
+++ cmake_clean.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
 rm -rf $(find . -name CMakeCache.txt)
-rm -rf $(find . -name CMakeFiles/)
+rm -rf $(find . -name CMakeFiles)
 rm -rf $(find . -name Makefile)
 rm -rf $(find . -name cmake_install.cmake)
 
 rm -rf $(find . -name CTestTestfile.cmake)
-rm -rf $(find . -name Testing/)
+rm -rf $(find . -name Testing)
 
 rm -rf $(find . -name cmake_install.cmake)
 
