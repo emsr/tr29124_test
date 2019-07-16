d75ab5a007530ea3602c62851ae20edb9a8de42b
emsr <3dw4rd@verizon.net>
2019-07-16 12:13:06 -0400

    Remove old comments in test_chebyshev.cpp

--- cxx_chebyshev/test_chebyshev.cpp
+++ cxx_chebyshev/test_chebyshev.cpp
@@ -1,10 +1,3 @@
-/*
-$HOME/bin_tr29124/bin/g++ -std=gnu++2a -g -Wall -Wextra -Wno-psabi -I. -o test_chebyshev test_chebyshev.cpp -lquadmath
-./test_chebyshev > test_chebyshev.txt
-
-$HOME/bin/bin/g++ -std=gnu++2a -g -Wall -Wextra -Wno-psabi -I. -o test_chebyshev test_chebyshev.cpp -lquadmath
-./test_chebyshev
-*/
 
 #include <iostream>
 #include <iomanip>
