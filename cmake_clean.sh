bf1bbb85a9799f9cef238c1d2b089929530c3ba4
emsr <3dw4rd@verizon.net>
2019-07-16 12:10:46 -0400

    Don't delete Makefile everywhere.

--- cmake_clean.sh
+++ cmake_clean.sh
@@ -2,7 +2,8 @@
 
 rm -rf $(find . -name CMakeCache.txt)
 rm -rf $(find . -name CMakeFiles)
-rm -rf $(find . -name Makefile)
+# Can't do this for the whole tree:
+#rm -rf $(find . -name Makefile)
 rm -rf $(find . -name cmake_install.cmake)
 
 rm -rf $(find . -name CTestTestfile.cmake)
