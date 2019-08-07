f0641dca7ac8dd472591769c50ee7ece29172845
emsr <3dw4rd@verizon.net>
2019-08-07 15:34:23 -0400

    Delete cmake-created Makefile.

--- cmake_clean.sh
+++ cmake_clean.sh
@@ -4,6 +4,7 @@ rm -rf $(find . -name CMakeCache.txt)
 rm -rf $(find . -name CMakeFiles)
 # Can't do this for the whole tree:
 #rm -rf $(find . -name Makefile)
+rm -f $(grep -rl "CMAKE generated file" --include=Makefile)
 rm -rf $(find . -name cmake_install.cmake)
 
 rm -rf $(find . -name CTestTestfile.cmake)
@@ -13,4 +14,3 @@ rm -rf $(find . -name cmake_install.cmake)
 
 rm -rf $(find . -name CMakeDoxyfile.in)
 rm -rf $(find . -name MakeDoxygenDefaults.cmake)
-
