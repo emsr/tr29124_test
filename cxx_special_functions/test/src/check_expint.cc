bd3ec0a8b53362e124c94e8f6c30f93d2e5b22c1
Ed Smith-Rowland <esmithrowland@gmail.com>
2022-02-07 17:56:21 -0500

    Fix check_expint.cc to use my dogfood.

--- cxx_special_functions/test/src/check_expint.cc
+++ cxx_special_functions/test/src/check_expint.cc
@@ -140,7 +140,7 @@ template<typename Ret, unsigned int Num>
     unsigned int num_datum = Num;
     for (unsigned int i = 0; i < num_datum; ++i)
       {
-	const Ret f = std::expint(data[i].x);
+	const Ret f = emsr::expint(data[i].x);
 	const bool failure_f = std::isnan(f);
 	if (!failure && failure_f)
 	  failure = true;
